168ab93bc37e7c1247f0deefa0869954963ea568
吕佐康 <lvzk@techsure.com.cn>
2019-11-27 16:04:53 +0800

    add transactional

--- src/main/java/codedriver/framework/tenant/service/MenuServiceImpl.java
+++ src/main/java/codedriver/framework/tenant/service/MenuServiceImpl.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import codedriver.framework.tenant.dao.mapper.MenuMapper;
 import codedriver.framework.tenant.dto.MenuVo;
 
 @Service
+@Transactional
 public class MenuServiceImpl implements MenuService {
 	@Autowired
 	private MenuMapper menuMapper;
