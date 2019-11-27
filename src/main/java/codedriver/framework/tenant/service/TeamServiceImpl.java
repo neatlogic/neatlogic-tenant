22e8f13c888bbb1c844bf41ba87807cad62f6d58
杨洋 <yangy@techsure.com.cn>
2019-11-27 16:14:37 +0800

    --story=1002032 --user=杨洋 角色管理页面接口 https://www.tapd.cn/54247054/s/1035242

--- src/main/java/codedriver/framework/tenant/service/TeamServiceImpl.java
+++ src/main/java/codedriver/framework/tenant/service/TeamServiceImpl.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import codedriver.framework.dto.TeamVo;
 import codedriver.framework.tenant.dao.mapper.TeamMapper;
 
 
 @Service
+@Transactional
 public class TeamServiceImpl implements TeamService{
 
 	@Autowired
