19af526a4bd369a5d1af2bc71a1eab3b3f1f3b32
杨洋 <yangy@techsure.com.cn>
2019-11-27 16:11:16 +0800

    --story=1002032 --user=杨洋 角色管理页面接口 https://www.tapd.cn/54247054/s/1035242

--- src/main/java/codedriver/framework/tenant/service/RoleServiceImpl.java
+++ src/main/java/codedriver/framework/tenant/service/RoleServiceImpl.java
@@ -1,19 +1,16 @@
 package codedriver.framework.tenant.service;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.alibaba.fastjson.JSONArray;
-
+import org.springframework.transaction.annotation.Transactional;
 import codedriver.framework.dto.RoleVo;
-import codedriver.framework.dto.TeamVo;
-import codedriver.framework.dto.UserVo;
+
 import codedriver.framework.tenant.dao.mapper.RoleMapper;
 
 @Service
+@Transactional
 public class RoleServiceImpl implements RoleService{
 
 	@Autowired 
