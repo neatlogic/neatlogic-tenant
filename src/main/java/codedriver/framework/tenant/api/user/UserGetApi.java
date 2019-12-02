3216e3f4b6247c3966382c7c68eb3703a6c049b0
杨洋 <yangy@techsure.com.cn>
2019-12-02 10:15:28 +0800

    --story=1002042 --user=杨洋 用户管理页面接口 https://www.tapd.cn/54247054/s/1035967

--- src/main/java/codedriver/framework/tenant/api/user/UserGetApi.java
+++ src/main/java/codedriver/framework/tenant/api/user/UserGetApi.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSONObject;
 
-import codedriver.framework.api.core.ApiParamType;
+import codedriver.framework.apiparam.core.ApiParamType;
 import codedriver.framework.common.AuthAction;
 import codedriver.framework.dto.UserVo;
 import codedriver.framework.restful.annotation.Description;
