aa4dd6eb039ba92ba88b6d65a3895e562f964e67
chenqiwei <chenqw@techsure.com.cn>
2019-12-08 11:20:52 +0800

    init

--- src/main/java/codedriver/module/tenant/api/role/RoleDeleteApi.java
+++ src/main/java/codedriver/module/tenant/api/role/RoleDeleteApi.java
@@ -9,7 +9,6 @@ import codedriver.framework.apiparam.core.ApiParamType;
 import codedriver.framework.common.AuthAction;
 import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.Input;
-import codedriver.framework.restful.annotation.Output;
 import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.core.ApiComponentBase;
 import codedriver.module.tenant.service.RoleService;
