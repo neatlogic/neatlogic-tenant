e1fe763b55d89b656d387728a6dfa462e4d72bbf
chenqiwei <chenqw@techsure.com.cn>
2019-12-16 18:06:02 +0800

    adjust

--- src/main/java/codedriver/module/tenant/api/user/UserSearchApi.java
+++ src/main/java/codedriver/module/tenant/api/user/UserSearchApi.java
@@ -37,11 +37,49 @@ public class UserSearchApi extends ApiComponentBase {
 		return null;
 	}
 
-	@Input({ @Param(name = "keyword", type = ApiParamType.STRING, desc = "关键字(用户id或名称),模糊查询", isRequired = false), @Param(name = "currentPage", type = ApiParamType.INTEGER, desc = "当前页数", isRequired = false), @Param(name = "pageSize", type = ApiParamType.INTEGER, desc = "每页展示数量 默认10", isRequired = false) })
-	@Output({ @Param(name = "userList", type = ApiParamType.JSONARRAY, desc = "用户信息list"), @Param(name = "pageCount", type = ApiParamType.INTEGER, desc = "总页数"), @Param(name = "currentPage", type = ApiParamType.INTEGER, desc = "当前页数"), @Param(name = "pageSize", type = ApiParamType.INTEGER, desc = "每页展示数量"), @Param(name = "userId", type = ApiParamType.STRING, desc = "用户Id"), @Param(name = "userName", type = ApiParamType.STRING, desc = "用户名"), @Param(name = "email", type = ApiParamType.STRING, desc = "邮箱"), @Param(name = "phone", type = ApiParamType.STRING, desc = "电话") })
+	@Input({
+			@Param(name = "keyword",
+					type = ApiParamType.STRING,
+					desc = "关键字(用户id或名称),模糊查询",
+					isRequired = false,
+					xss = true),
+			@Param(name = "currentPage",
+					type = ApiParamType.INTEGER,
+					desc = "当前页数",
+					isRequired = false),
+			@Param(name = "pageSize",
+					type = ApiParamType.INTEGER,
+					desc = "每页展示数量 默认10",
+					isRequired = false) })
+	@Output({
+			@Param(name = "userList",
+					type = ApiParamType.JSONARRAY,
+					desc = "用户信息list"),
+			@Param(name = "pageCount",
+					type = ApiParamType.INTEGER,
+					desc = "总页数"),
+			@Param(name = "currentPage",
+					type = ApiParamType.INTEGER,
+					desc = "当前页数"),
+			@Param(name = "pageSize",
+					type = ApiParamType.INTEGER,
+					desc = "每页展示数量"),
+			@Param(name = "userId",
+					type = ApiParamType.STRING,
+					desc = "用户Id"),
+			@Param(name = "userName",
+					type = ApiParamType.STRING,
+					desc = "用户名"),
+			@Param(name = "email",
+					type = ApiParamType.STRING,
+					desc = "邮箱"),
+			@Param(name = "phone",
+					type = ApiParamType.STRING,
+					desc = "电话") })
 	@Description(desc = "查询用户接口")
 	@Override
 	public Object myDoService(JSONObject jsonObj) throws Exception {
+		System.out.println(jsonObj.toJSONString());
 		JSONObject json = new JSONObject();
 		UserVo userVo = new UserVo();
 		userVo.setKeyword(jsonObj.getString("keyword"));
