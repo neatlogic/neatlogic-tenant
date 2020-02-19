5dfe06cc52d0013cad9d43dc660addfb6fba5baa
lixs <lixs@techsure.com.cn>
2020-02-19 17:53:12 +0800

    用户组织架构

--- src/main/java/codedriver/module/tenant/service/TeamServiceImpl.java
+++ src/main/java/codedriver/module/tenant/service/TeamServiceImpl.java
@@ -1,8 +1,14 @@
 package codedriver.module.tenant.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import codedriver.module.tenant.util.UuidUtil;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.sun.org.apache.regexp.internal.RE;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -57,5 +63,42 @@ public class TeamServiceImpl implements TeamService {
 		teamVo.setSort(sort+1);
 
 		teamMapper.insertTeam(teamVo);
+	}
+
+	@Override
+	public JSONArray getTeamTree() {
+		List<TeamVo> teamList = teamMapper.getTeamTree();
+		Map<String, List<TeamVo>> map = new HashMap<>();
+		for (TeamVo teamVo : teamList){
+			if(map.containsKey(teamVo.getParentUuid())){
+				map.get(teamVo.getParentUuid()).add(teamVo);
+			}else {
+				List<TeamVo> teams = new ArrayList<>();
+				teams.add(teamVo);
+				map.put(teamVo.getParentUuid(), teams);
+			}
+		}
+		List<TeamVo> startList = map.get("0");
+		if (startList != null && startList.size() > 0){
+			return buildData(startList, map);
+		}
+		return null;
+	}
+
+	public JSONArray buildData(List<TeamVo> startList, Map<String, List<TeamVo>> map){
+		JSONArray children = new JSONArray();
+		for (TeamVo teamVo: startList){
+			JSONObject teamObj = new JSONObject();
+			teamObj.put("name", teamVo.getName());
+			teamObj.put("uuid", teamVo.getUuid());
+			teamObj.put("sort", teamVo.getSort());
+			teamObj.put("parentUuid", teamVo.getParentUuid());
+			if (map.containsKey(teamVo.getUuid())){
+				List<TeamVo> teams = map.get(teamVo.getUuid());
+				teamObj.put("children", buildData(teams, map));
+			}
+			children.add(teamObj);
+		}
+		return children;
 	}
 }
