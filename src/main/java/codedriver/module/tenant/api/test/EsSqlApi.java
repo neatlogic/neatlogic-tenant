e2270195aabf5cc9b33dd76bf072719969f2a2b4
lawrence <liangwz@techsure.com.cn>
2020-02-21 10:16:36 +0800

    完善测试接口

--- src/main/java/codedriver/module/tenant/api/test/EsSqlApi.java
+++ src/main/java/codedriver/module/tenant/api/test/EsSqlApi.java
@@ -12,11 +12,13 @@ import com.techsure.multiattrsearch.query.QuerySyntaxException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+@Service
 public class EsSqlApi extends ApiComponentBase {
     private static final Logger logger = LoggerFactory.getLogger(EsSearchApi.class);
 
