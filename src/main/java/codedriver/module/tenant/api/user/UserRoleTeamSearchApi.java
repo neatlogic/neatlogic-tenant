05553949c5775679fcae33fe326bab15cf958520
吕佐康 <lvzk@techsure.com.cn>
2020-03-09 18:30:48 +0800

    update

--- src/main/java/codedriver/module/tenant/api/user/UserRoleTeamSearchApi.java
+++ src/main/java/codedriver/module/tenant/api/user/UserRoleTeamSearchApi.java
@@ -40,8 +40,8 @@ public class UserRoleTeamSearchApi extends ApiComponentBase {
 		@Param(name = "keyword", type = ApiParamType.STRING, desc = "关键字(用户id或名称),模糊查询", isRequired = false, xss = true),
 		@Param(name = "valueList", type = ApiParamType.JSONARRAY,  isRequired = false, desc = "用于回显的参数列表"),
 		@Param(name = "excludeList", type = ApiParamType.JSONARRAY,  isRequired = false, desc = "用于过滤回显参数"),
-		@Param(name = "groupList", type = ApiParamType.JSONARRAY,  isRequired = false, desc = "限制接口返回类型，['processUserType','user','team','role']"),
-		@Param(name = "total", type = ApiParamType.INTEGER, desc = "公展示数量 默认18", isRequired = false)
+		@Param(name = "groupList", type = ApiParamType.JSONARRAY,  isRequired = true, desc = "限制接口返回类型，['processUserType','user','team','role']"),
+		@Param(name = "total", type = ApiParamType.INTEGER, desc = "共展示数量 默认18", isRequired = false)
 		})
 	@Output({
 		@Param(name="text", type = ApiParamType.STRING, desc="类型中文名"),
