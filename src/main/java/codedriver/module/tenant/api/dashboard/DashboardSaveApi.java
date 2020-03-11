931d610fd36b035951f0a15a3954786c1835966a
chenqiwei <chenqw@techsure.com.cn>
2020-03-11 10:00:25 +0800

    dashboard

---
+++ src/main/java/codedriver/module/tenant/api/dashboard/DashboardSaveApi.java
@@ -0,0 +1,66 @@
+package codedriver.module.tenant.api.dashboard;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.alibaba.fastjson.JSONObject;
+
+import codedriver.framework.apiparam.core.ApiParamType;
+import codedriver.framework.dashboard.dao.mapper.DashboardMapper;
+import codedriver.framework.dashboard.dto.DashboardVo;
+import codedriver.framework.dashboard.dto.DashboardWidgetVo;
+import codedriver.framework.restful.annotation.Description;
+import codedriver.framework.restful.annotation.Input;
+import codedriver.framework.restful.annotation.Output;
+import codedriver.framework.restful.annotation.Param;
+import codedriver.framework.restful.core.ApiComponentBase;
+import codedriver.module.tenant.exception.dashboard.DashboardNameExistsException;
+
+@Service
+@Transactional
+public class DashboardSaveApi extends ApiComponentBase {
+
+	@Autowired
+	private DashboardMapper dashboardMapper;
+
+	@Override
+	public String getToken() {
+		return "dashboard/save";
+	}
+
+	@Override
+	public String getName() {
+		return "仪表板保存接口";
+	}
+
+	@Override
+	public String getConfig() {
+		return null;
+	}
+
+	@Input({ @Param(name = "uuid", type = ApiParamType.STRING, desc = "仪表板uuid，为空代表新增"), @Param(name = "name", type = ApiParamType.STRING, desc = "仪表板名称", isRequired = true), @Param(name = "isActive", type = ApiParamType.INTEGER, desc = "是否激活，1：激活，0：禁用", isRequired = true),
+			@Param(name = "widgetList", type = ApiParamType.JSONARRAY, desc = "组件列表，范例：\"chartType\": \"barchart\"," + "\"h\": 4," + "\"handler\": \"codedriver.module.process.dashboard.handler.ProcessTaskDashboardHandler\"," + "\"i\": 0," + "\"name\": \"组件1\"," + "\"refreshInterval\": 3," + "\"uuid\": \"aaaa\"," + "\"w\": 5," + "\"x\": 0," + "\"y\": 0") })
+	@Output({ @Param(name = "uuid", type = ApiParamType.STRING, desc = "仪表板uuid") })
+	@Description(desc = "仪表板保存接口")
+	@Override
+	public Object myDoService(JSONObject jsonObj) throws Exception {
+		DashboardVo dashboardVo = JSONObject.toJavaObject(jsonObj, DashboardVo.class);
+		if (dashboardMapper.checkDashboardNameIsExists(dashboardVo) > 0) {
+			throw new DashboardNameExistsException(dashboardVo.getName());
+		}
+		if (dashboardMapper.getDashboardByUuid(dashboardVo.getUuid()) == null) {
+			dashboardMapper.insertDashboard(dashboardVo);
+		} else {
+			dashboardMapper.updateDashboard(dashboardVo);
+			dashboardMapper.deleteDashboardWidgetByDashboardUuid(dashboardVo.getUuid());
+		}
+		if (dashboardVo.getWidgetList() != null && dashboardVo.getWidgetList().size() > 0) {
+			for (DashboardWidgetVo widgetVo : dashboardVo.getWidgetList()) {
+				widgetVo.setDashboardUuid(dashboardVo.getUuid());
+				dashboardMapper.insertDashboardWidget(widgetVo);
+			}
+		}
+		return dashboardVo.getUuid();
+	}
+}
---
+++ src/main/java/codedriver/module/tenant/api/dashboard/DashboardSearchApi.java
@@ -0,0 +1,92 @@
+package codedriver.module.tenant.api.dashboard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSONObject;
+
+import codedriver.framework.apiparam.core.ApiParamType;
+import codedriver.framework.asynchronization.threadlocal.UserContext;
+import codedriver.framework.common.util.PageUtil;
+import codedriver.framework.dashboard.dao.mapper.DashboardMapper;
+import codedriver.framework.dashboard.dto.DashboardRoleVo;
+import codedriver.framework.dashboard.dto.DashboardVo;
+import codedriver.framework.exception.user.NoUserException;
+import codedriver.framework.restful.annotation.Description;
+import codedriver.framework.restful.annotation.Input;
+import codedriver.framework.restful.annotation.Output;
+import codedriver.framework.restful.annotation.Param;
+import codedriver.framework.restful.core.ApiComponentBase;
+
+@Service
+public class DashboardSearchApi extends ApiComponentBase {
+
+	@Autowired
+	private DashboardMapper dashboardMapper;
+
+	@Override
+	public String getToken() {
+		return "dashboard/search";
+	}
+
+	@Override
+	public String getName() {
+		return "仪表板查询接口";
+	}
+
+	@Override
+	public String getConfig() {
+		return null;
+	}
+
+	@SuppressWarnings("serial")
+	@Input({ @Param(name = "currentPage", type = ApiParamType.INTEGER, desc = "当前页数", isRequired = false), @Param(name = "pageSize", type = ApiParamType.INTEGER, desc = "每页展示数量 默认20", isRequired = false), @Param(name = "needPage", type = ApiParamType.BOOLEAN, desc = "是否分页") })
+	@Output({ @Param(name = "pageCount", type = ApiParamType.INTEGER, desc = "总页数"), @Param(name = "currentPage", type = ApiParamType.INTEGER, desc = "当前页数"), @Param(name = "pageSize", type = ApiParamType.INTEGER, desc = "每页展示数量"), @Param(name = "dashboardList", explode = DashboardVo[].class, desc = "仪表板列表") })
+	@Description(desc = "仪表板查询接口")
+	@Override
+	public Object myDoService(JSONObject jsonObj) throws Exception {
+		DashboardVo dashboardVo = new DashboardVo();
+		if (jsonObj.containsKey("currentPage")) {
+			dashboardVo.setCurrentPage(jsonObj.getInteger("currentPage"));
+		}
+		if (jsonObj.containsKey("pageSize")) {
+			dashboardVo.setPageSize(jsonObj.getInteger("pageSize"));
+		}
+		String userId = UserContext.get().getUserId();
+		if (StringUtils.isNotBlank(userId)) {
+			dashboardVo.setFcu(userId);
+		} else {
+			throw new NoUserException();
+		}
+		int rowNum = dashboardMapper.searchDashboardCount(dashboardVo);
+		int pageCount = PageUtil.getPageCount(rowNum, dashboardVo.getPageSize());
+		List<DashboardVo> dashboardList = dashboardMapper.searchDashboard(dashboardVo);
+		// 补充权限数据
+		for (DashboardVo dashboard : dashboardList) {
+			if (dashboard.getFcu().equals(userId)) {
+				// 自己创建的dashboard拥有所有权限
+				dashboard.setRoleList(new ArrayList<String>() {
+					{
+						this.add(DashboardRoleVo.ActionType.READ.getValue());
+						this.add(DashboardRoleVo.ActionType.WRITE.getValue());
+						this.add(DashboardRoleVo.ActionType.SHARE.getValue());
+					}
+				});
+			} else {
+				dashboard.setRoleList(dashboardMapper.getDashboardRoleByDashboardUuid(dashboard.getUuid(), userId));
+			}
+		}
+
+		JSONObject returnObj = new JSONObject();
+		returnObj.put("rowNum", rowNum);
+		returnObj.put("pageCount", pageCount);
+		returnObj.put("currentPage", dashboardVo.getCurrentPage());
+		returnObj.put("pageSize", dashboardVo.getPageSize());
+		returnObj.put("dashboardList", dashboardList);
+		return returnObj;
+	}
+}
