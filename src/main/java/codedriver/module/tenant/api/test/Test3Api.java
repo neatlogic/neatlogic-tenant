030ee28826226041902df31d6cfab0620074d43c
林邦泉 <linbq@techsure.com.cn>
2020-03-30 11:32:03 +0800

    --story=1002289 --user=林邦泉 接口管理页面 https://www.tapd.cn/54247054/s/1054933

---
+++ src/main/java/codedriver/module/tenant/api/test/Test3Api.java
@@ -0,0 +1,36 @@
+package codedriver.module.tenant.api.test;
+
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSONObject;
+
+import codedriver.framework.restful.core.ApiComponentBase;
+@Service
+public class Test3Api extends ApiComponentBase {
+	
+	@Override
+	public boolean isPrivate() {
+		return false;
+	}
+
+	@Override
+	public String getToken() {
+		return "test3";
+	}
+
+	@Override
+	public String getName() {
+		return "测试3";
+	}
+
+	@Override
+	public String getConfig() {
+		return null;
+	}
+
+	@Override
+	public Object myDoService(JSONObject jsonObj) throws Exception {
+		return "test3";
+	}
+
+}
