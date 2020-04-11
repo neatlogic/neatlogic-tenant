f2a4635292d37871ae09933ae17132080a62ce59
chenqiwei <chenqw@techsure.com.cn>
2020-04-11 22:35:11 +0800

    adjust

--- src/main/java/codedriver/module/tenant/api/file/ImageDownloadApi.java
+++ src/main/java/codedriver/module/tenant/api/file/ImageDownloadApi.java
@@ -19,10 +19,7 @@ import com.alibaba.fastjson.JSONObject;
 
 import codedriver.framework.apiparam.core.ApiParamType;
 import codedriver.framework.asynchronization.threadlocal.TenantContext;
-import codedriver.framework.asynchronization.threadlocal.UserContext;
 import codedriver.framework.exception.user.NoTenantException;
-import codedriver.framework.file.core.FileTypeHandlerFactory;
-import codedriver.framework.file.core.IFileTypeHandler;
 import codedriver.framework.file.dao.mapper.FileMapper;
 import codedriver.framework.file.dto.FileVo;
 import codedriver.framework.restful.annotation.Description;
@@ -30,13 +27,11 @@ import codedriver.framework.restful.annotation.Input;
 import codedriver.framework.restful.annotation.IsActived;
 import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.core.BinaryStreamApiComponentBase;
-import codedriver.module.tenant.exception.file.FileAccessDeniedException;
 import codedriver.module.tenant.exception.file.FileNotFoundException;
-import codedriver.module.tenant.exception.file.FileTypeHandlerNotFoundException;
 
 @Service
 @IsActived
-public class FileDownloadApi extends BinaryStreamApiComponentBase {
+public class ImageDownloadApi extends BinaryStreamApiComponentBase {
 
 	@Autowired
 	private FileMapper fileMapper;
@@ -46,12 +41,12 @@ public class FileDownloadApi extends BinaryStreamApiComponentBase {
 
 	@Override
 	public String getToken() {
-		return "file/download";
+		return "image/download";
 	}
 
 	@Override
 	public String getName() {
-		return "附件下载接口";
+		return "图片下载接口";
 	}
 
 	@Override
@@ -59,8 +54,8 @@ public class FileDownloadApi extends BinaryStreamApiComponentBase {
 		return null;
 	}
 
-	@Input({ @Param(name = "uuid", type = ApiParamType.STRING, desc = "附件uuid", isRequired = true) })
-	@Description(desc = "附件下载接口")
+	@Input({ @Param(name = "uuid", type = ApiParamType.STRING, desc = "图片uuid", isRequired = true) })
+	@Description(desc = "图片下载接口")
 	@Override
 	public Object myDoService(JSONObject paramObj, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		String uuid = paramObj.getString("uuid");
@@ -70,39 +65,24 @@ public class FileDownloadApi extends BinaryStreamApiComponentBase {
 			throw new NoTenantException();
 		}
 		if (fileVo != null) {
-			IFileTypeHandler fileTypeHandler = FileTypeHandlerFactory.getHandler(fileVo.getType());
-			if (fileTypeHandler != null) {
-				if (fileTypeHandler.valid(UserContext.get().getUserId(), paramObj)) {
-					ServletOutputStream os = null;
-					FSDataInputStream in = fileSystem.open(new Path("/" + tenantUuid + "/upload/" + fileVo.getType() + "/" + fileVo.getUuid()));
-					String fileNameEncode = "";
-					Boolean flag = request.getHeader("User-Agent").indexOf("like Gecko") > 0;
-					if (request.getHeader("User-Agent").toLowerCase().indexOf("msie") > 0 || flag) {
-						fileNameEncode = URLEncoder.encode(fileVo.getName(), "UTF-8");// IE浏览器
-					} else {
-						fileNameEncode = new String(fileVo.getName().replace(" ", "").getBytes(StandardCharsets.UTF_8), "ISO8859-1");
-					}
-
-					if (StringUtils.isBlank(fileVo.getContentType())) {
-						response.setContentType("aplication/x-msdownload");
-					} else {
-						response.setContentType(fileVo.getContentType());
-					}
-					response.setHeader("Content-Disposition", "attachment;fileName=\"" + fileNameEncode + "\"");
-					os = response.getOutputStream();
-					IOUtils.copyLarge(in, os);
-					if (os != null) {
-						os.flush();
-						os.close();
-					}
-					if (in != null) {
-						in.close();
-					}
-				} else {
-					throw new FileAccessDeniedException(fileVo.getName());
-				}
+			ServletOutputStream os = null;
+			FSDataInputStream in = fileSystem.open(new Path("/" + tenantUuid + "/images/" + fileVo.getUuid()));
+			String fileNameEncode = "";
+			Boolean flag = request.getHeader("User-Agent").indexOf("like Gecko") > 0;
+			if (request.getHeader("User-Agent").toLowerCase().indexOf("msie") > 0 || flag) {
+				fileNameEncode = URLEncoder.encode(fileVo.getName(), "UTF-8");// IE浏览器
 			} else {
-				throw new FileTypeHandlerNotFoundException(fileVo.getType());
+				fileNameEncode = new String(fileVo.getName().replace(" ", "").getBytes(StandardCharsets.UTF_8), "ISO8859-1");
+			}
+			response.setContentType(fileVo.getContentType());
+			os = response.getOutputStream();
+			IOUtils.copyLarge(in, os);
+			if (os != null) {
+				os.flush();
+				os.close();
+			}
+			if (in != null) {
+				in.close();
 			}
 		} else {
 			throw new FileNotFoundException(uuid);
---
+++ src/main/java/codedriver/module/tenant/api/file/ImageUploadApi.java
@@ -0,0 +1,105 @@
+package codedriver.module.tenant.api.file;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.alibaba.fastjson.JSONObject;
+
+import codedriver.framework.asynchronization.threadlocal.TenantContext;
+import codedriver.framework.asynchronization.threadlocal.UserContext;
+import codedriver.framework.exception.user.NoTenantException;
+import codedriver.framework.file.dao.mapper.FileMapper;
+import codedriver.framework.file.dto.FileVo;
+import codedriver.framework.restful.annotation.Description;
+import codedriver.framework.restful.annotation.IsActived;
+import codedriver.framework.restful.annotation.Output;
+import codedriver.framework.restful.annotation.Param;
+import codedriver.framework.restful.core.BinaryStreamApiComponentBase;
+
+@Service
+@IsActived
+public class ImageUploadApi extends BinaryStreamApiComponentBase {
+
+	@Autowired
+	private FileSystem fileSystem;
+
+	@Autowired
+	private FileMapper fileMapper;
+
+	@Override
+	public String getToken() {
+		return "image/upload";
+	}
+
+	@Override
+	public String getName() {
+		return "图片上传接口";
+	}
+
+	@Override
+	public String getConfig() {
+		return null;
+	}
+
+	@Override
+	public boolean isRaw() {
+		return true;
+	}
+
+	@Output({ @Param(explode = FileVo.class) })
+	@Description(desc = "图片上传接口")
+	@Override
+	public Object myDoService(JSONObject paramObj, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String tenantUuid = TenantContext.get().getTenantUuid();
+		if (StringUtils.isBlank(tenantUuid)) {
+			throw new NoTenantException();
+		}
+		MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+		String paramName = "upload";
+		JSONObject returnObj = new JSONObject();
+		try {
+			MultipartFile multipartFile = multipartRequest.getFile(paramName);
+			if (multipartFile != null && multipartFile.getName() != null && multipartFile.getContentType().startsWith("image")) {
+				String userId = UserContext.get().getUserId();
+				String oldFileName = multipartFile.getOriginalFilename();
+				Long size = multipartFile.getSize();
+
+				FileVo fileVo = new FileVo();
+				fileVo.setName(oldFileName);
+				fileVo.setSize(size);
+				fileVo.setUserId(userId);
+				fileVo.setType("image");
+				fileVo.setContentType(multipartFile.getContentType());
+
+				String finalPath = "/" + tenantUuid + "/images/" + fileVo.getUuid();
+				FSDataOutputStream fos = fileSystem.create(new Path(finalPath));
+				IOUtils.copyLarge(multipartFile.getInputStream(), fos);
+				fos.flush();
+				fos.close();
+
+				fileMapper.insertFile(fileVo);
+
+				returnObj.put("uploaded", true);
+				returnObj.put("url", fileVo.getUuid());
+			} else {
+				returnObj.put("uploaded", false);
+				returnObj.put("error", "请选择图片文件");
+			}
+		} catch (Exception ex) {
+			returnObj.put("uploaded", false);
+			returnObj.put("error", ex.getMessage());
+		}
+		return returnObj;
+
+	}
+}
