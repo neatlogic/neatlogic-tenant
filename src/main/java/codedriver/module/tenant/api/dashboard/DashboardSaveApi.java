77db495545c0463ebbdb300c5188d4832ed49a32
吕佐康 <lvzk@techsure.com.cn>
2020-04-28 15:58:59 +0800

    update

--- src/main/java/codedriver/module/tenant/api/dashboard/DashboardSaveApi.java
+++ src/main/java/codedriver/module/tenant/api/dashboard/DashboardSaveApi.java
@@ -107,7 +107,7 @@ public class DashboardSaveApi extends ApiComponentBase {
 			}
 		}else {
 			if(StringUtils.isBlank(oldDashboardVo.getFcu())) {
-				dashboardMapper.insertDashboardDefault(userId, oldDashboardVo.getUuid(),null);
+				dashboardMapper.insertDashboardDefault(userId, oldDashboardVo.getUuid(),type);
 			}
 		}
 		if(StringUtils.isBlank(uuid)) {
