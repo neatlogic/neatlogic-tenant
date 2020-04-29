88881d44d208d7d14d35cb6820d38c5a3481b9de
chenqiwei <chenqw@techsure.com.cn>
2020-04-29 09:42:52 +0800

    integration

---
+++ src/main/java/codedriver/module/tenant/api/integration/IntegrationAuthenticationTypeListApi.java
@@ -0,0 +1,48 @@
+package codedriver.module.tenant.api.integration;
+
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import codedriver.framework.apiparam.core.ApiParamType;
+import codedriver.framework.integration.authentication.costvalue.AuthenticateType;
+import codedriver.framework.restful.annotation.Description;
+import codedriver.framework.restful.annotation.IsActived;
+import codedriver.framework.restful.annotation.Output;
+import codedriver.framework.restful.annotation.Param;
+import codedriver.framework.restful.core.ApiComponentBase;
+
+@Service
+@IsActived
+public class IntegrationAuthenticationTypeListApi extends ApiComponentBase {
+
+	@Override
+	public String getToken() {
+		return "integration/authenticationtype/list";
+	}
+
+	@Override
+	public String getName() {
+		return "集成配置认证类型列表接口";
+	}
+
+	@Override
+	public String getConfig() {
+		return null;
+	}
+
+	@Output({ @Param(name = "value", type = ApiParamType.STRING, desc = "类型"), @Param(name = "text", type = ApiParamType.STRING, desc = "类型中文名") })
+	@Description(desc = "集成配置认证类型列表接口")
+	@Override
+	public Object myDoService(JSONObject jsonObj) throws Exception {
+		JSONArray returnList = new JSONArray();
+		for (AuthenticateType type : AuthenticateType.values()) {
+			JSONObject j = new JSONObject();
+			j.put("value", type.getValue());
+			j.put("text", type.getText());
+			returnList.add(j);
+		}
+		return returnList;
+	}
+}
---
+++ src/main/java/codedriver/module/tenant/api/integration/IntegrationRunApi.java
@@ -0,0 +1,62 @@
+package codedriver.module.tenant.api.integration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSONObject;
+
+import codedriver.framework.apiparam.core.ApiParamType;
+import codedriver.framework.exception.integration.IntegrationHandlerNotFoundException;
+import codedriver.framework.exception.integration.IntegrationNotFoundException;
+import codedriver.framework.integration.core.IIntegrationHandler;
+import codedriver.framework.integration.core.IntegrationHandlerFactory;
+import codedriver.framework.integration.dao.mapper.IntegrationMapper;
+import codedriver.framework.integration.dto.IntegrationResultVo;
+import codedriver.framework.integration.dto.IntegrationVo;
+import codedriver.framework.restful.annotation.Description;
+import codedriver.framework.restful.annotation.Input;
+import codedriver.framework.restful.annotation.IsActived;
+import codedriver.framework.restful.annotation.Param;
+import codedriver.framework.restful.core.ApiComponentBase;
+
+@Service
+@IsActived
+public class IntegrationRunApi extends ApiComponentBase {
+
+	@Autowired
+	private IntegrationMapper integrationMapper;
+
+	@Override
+	public String getToken() {
+		return "integration/run/{uuid}";
+	}
+
+	@Override
+	public String getName() {
+		return "集成配置执行接口";
+	}
+
+	@Override
+	public String getConfig() {
+		return null;
+	}
+
+	@Input({ @Param(name = "uuid", type = ApiParamType.STRING, desc = "集成配置uuid", isRequired = true) })
+	@Description(desc = "集成配置执行接口")
+	@Override
+	public Object myDoService(JSONObject jsonObj) throws Exception {
+		IntegrationVo integrationVo = integrationMapper.getIntegrationByUuid(jsonObj.getString("uuid"));
+		if (integrationVo == null) {
+			throw new IntegrationNotFoundException(jsonObj.getString("uuid"));
+		}
+		jsonObj.remove("uuid");
+		integrationVo.setParamObj(jsonObj);
+		IIntegrationHandler handler = IntegrationHandlerFactory.getHandler(integrationVo.getHandler());
+		if (handler == null) {
+			throw new IntegrationHandlerNotFoundException(integrationVo.getHandler());
+		}
+
+		IntegrationResultVo resultVo = handler.sendRequest(integrationVo);
+		return resultVo;
+	}
+}
