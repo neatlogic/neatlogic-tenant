dbabd3f7ddafb71434e2389f5caab489fcd1d3ea
林邦泉 <linbq@techsure.com.cn>
2020-06-03 11:34:42 +0800

    --story=1002990 --user=林邦泉 通知配置采用通知策略，以及通知模板优化 https://www.tapd.cn/54247054/s/1068380

--- src/main/java/codedriver/module/tenant/api/notify/NotifyPolicyParamListApi.java
+++ src/main/java/codedriver/module/tenant/api/notify/NotifyPolicyParamListApi.java
@@ -12,13 +12,10 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
 import codedriver.framework.apiparam.core.ApiParamType;
-import codedriver.framework.notify.core.INotifyPolicyHandler;
 import codedriver.framework.notify.core.NotifyPolicyFactory;
-import codedriver.framework.notify.core.NotifyPolicyHandlerFactory;
 import codedriver.framework.notify.dao.mapper.NotifyMapper;
 import codedriver.framework.notify.dto.NotifyPolicyParamVo;
 import codedriver.framework.notify.dto.NotifyPolicyVo;
-import codedriver.framework.notify.exception.NotifyPolicyHandlerNotFoundException;
 import codedriver.framework.notify.exception.NotifyPolicyNotFoundException;
 import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.Input;
@@ -77,40 +74,6 @@ public class NotifyPolicyParamListApi extends ApiComponentBase {
 				paramList.add(notifyPolicyParamVo);
 			}
 		}
-//		String source = jsonObj.getString("source");
-//		if(!"system".equals(source)) {
-//			JSONObject configObj = notifyPolicyVo.getConfigObj();
-//			List<NotifyPolicyParamVo> customParamList = JSON.parseArray(configObj.getJSONArray("paramList").toJSONString(), NotifyPolicyParamVo.class);
-//			if(CollectionUtils.isNotEmpty(customParamList)) {
-//				for(NotifyPolicyParamVo notifyPolicyParamVo : customParamList) {
-//					if(StringUtils.isNotBlank(keyword)) {
-//						if(!notifyPolicyParamVo.getName().toLowerCase().contains(keyword.toLowerCase()) 
-//								&& !notifyPolicyParamVo.getDescription().toLowerCase().contains(keyword.toLowerCase())) {
-//							continue;
-//						}
-//					}
-//					paramList.add(notifyPolicyParamVo);
-//				}
-//			}
-//		}
-//		if(!"custom".equals(source)) {
-//			INotifyPolicyHandler notifyPolicyHandler = NotifyPolicyHandlerFactory.getHandler(notifyPolicyVo.getHandler());
-//			if(notifyPolicyHandler == null) {
-//				throw new NotifyPolicyHandlerNotFoundException(notifyPolicyVo.getHandler());
-//			}
-//			List<NotifyPolicyParamVo> systemParamList = notifyPolicyHandler.getSystemParamList();
-//			if(CollectionUtils.isNotEmpty(systemParamList)) {
-//				for(NotifyPolicyParamVo notifyPolicyParamVo : systemParamList) {
-//					if(StringUtils.isNotBlank(keyword)) {
-//						if(!notifyPolicyParamVo.getName().toLowerCase().contains(keyword.toLowerCase()) 
-//								&& !notifyPolicyParamVo.getDescription().toLowerCase().contains(keyword.toLowerCase())) {
-//							continue;
-//						}
-//					}
-//					paramList.add(notifyPolicyParamVo);
-//				}
-//			}
-//		}
 		JSONObject resultObj = new JSONObject();
 		resultObj.put("paramList", paramList);
 		return resultObj;
@@ -125,40 +88,21 @@ public class NotifyPolicyParamListApi extends ApiComponentBase {
 			throw new NotifyPolicyNotFoundException(policyId.toString());
 		}
 		String keyword = jsonObj.getString("keyword");
-		String source = jsonObj.getString("source");
-		if(!"system".equals(source)) {
-			JSONObject configObj = notifyPolicyVo.getConfigObj();
-			List<NotifyPolicyParamVo> customParamList = JSON.parseArray(configObj.getJSONArray("paramList").toJSONString(), NotifyPolicyParamVo.class);
-			if(CollectionUtils.isNotEmpty(customParamList)) {
-				for(NotifyPolicyParamVo notifyPolicyParamVo : customParamList) {
-					if(StringUtils.isNotBlank(keyword)) {
-						if(!notifyPolicyParamVo.getName().toLowerCase().contains(keyword.toLowerCase()) 
-								&& !notifyPolicyParamVo.getDescription().toLowerCase().contains(keyword.toLowerCase())) {
-							continue;
-						}
+		JSONObject configObj = notifyPolicyVo.getConfigObj();
+		List<NotifyPolicyParamVo> customParamList = JSON.parseArray(configObj.getJSONArray("paramList").toJSONString(), NotifyPolicyParamVo.class);
+		if(CollectionUtils.isNotEmpty(customParamList)) {
+			for(NotifyPolicyParamVo notifyPolicyParamVo : customParamList) {
+				if(StringUtils.isNotBlank(keyword)) {
+					if(!notifyPolicyParamVo.getName().toLowerCase().contains(keyword.toLowerCase()) 
+							&& !notifyPolicyParamVo.getDescription().toLowerCase().contains(keyword.toLowerCase())) {
+						continue;
 					}
-					paramList.add(notifyPolicyParamVo);
 				}
+				paramList.add(notifyPolicyParamVo);
 			}
 		}
-		if(!"custom".equals(source)) {
-			INotifyPolicyHandler notifyPolicyHandler = NotifyPolicyHandlerFactory.getHandler(notifyPolicyVo.getHandler());
-			if(notifyPolicyHandler == null) {
-				throw new NotifyPolicyHandlerNotFoundException(notifyPolicyVo.getHandler());
-			}
-			List<NotifyPolicyParamVo> systemParamList = notifyPolicyHandler.getSystemParamList();
-			if(CollectionUtils.isNotEmpty(systemParamList)) {
-				for(NotifyPolicyParamVo notifyPolicyParamVo : systemParamList) {
-					if(StringUtils.isNotBlank(keyword)) {
-						if(!notifyPolicyParamVo.getName().toLowerCase().contains(keyword.toLowerCase()) 
-								&& !notifyPolicyParamVo.getDescription().toLowerCase().contains(keyword.toLowerCase())) {
-							continue;
-						}
-					}
-					paramList.add(notifyPolicyParamVo);
-				}
-			}
-		}
-		return paramList;
+		JSONObject resultObj = new JSONObject();
+		resultObj.put("paramList", paramList);
+		return resultObj;
 	}
 }
