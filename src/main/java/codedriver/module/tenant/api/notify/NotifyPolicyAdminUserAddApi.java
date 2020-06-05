9625426c2cb8b8d281227909eba140c827a42b2a
林邦泉 <linbq@techsure.com.cn>
2020-06-05 11:42:19 +0800

    --story=1002990 --user=林邦泉 通知配置采用通知策略，以及通知模板优化 https://www.tapd.cn/54247054/s/1069156

--- src/main/java/codedriver/module/tenant/api/notify/NotifyPolicyAdminUserAddApi.java
+++ src/main/java/codedriver/module/tenant/api/notify/NotifyPolicyAdminUserAddApi.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
@@ -19,10 +20,12 @@ import codedriver.framework.notify.dto.NotifyPolicyVo;
 import codedriver.framework.notify.exception.NotifyPolicyNotFoundException;
 import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.Input;
+import codedriver.framework.restful.annotation.IsActived;
 import codedriver.framework.restful.annotation.Output;
 import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.core.ApiComponentBase;
-
+@Service
+@IsActived
 public class NotifyPolicyAdminUserAddApi extends ApiComponentBase {
 	
 	@Autowired
--- src/main/java/codedriver/module/tenant/api/notify/NotifyPolicyAdminUserDeleteApi.java
+++ src/main/java/codedriver/module/tenant/api/notify/NotifyPolicyAdminUserDeleteApi.java
@@ -1,6 +1,7 @@
 package codedriver.module.tenant.api.notify;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSONObject;
 
@@ -10,9 +11,11 @@ import codedriver.framework.notify.dto.NotifyPolicyVo;
 import codedriver.framework.notify.exception.NotifyPolicyNotFoundException;
 import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.Input;
+import codedriver.framework.restful.annotation.IsActived;
 import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.core.ApiComponentBase;
-
+@Service
+@IsActived
 public class NotifyPolicyAdminUserDeleteApi extends ApiComponentBase {
 	
 	@Autowired
