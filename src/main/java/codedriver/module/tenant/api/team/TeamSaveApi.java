5f9607da3434c26c275f8dae5cf6e9b9bbb04031
林邦泉 <linbq@techsure.com.cn>
2020-06-16 16:42:23 +0800

    --story=1003036 --user=林邦泉 增加分组中“公司、中心、部门、组”等分类标识 https://www.tapd.cn/54247054/s/1071344

--- src/main/java/codedriver/module/tenant/api/team/TeamSaveApi.java
+++ src/main/java/codedriver/module/tenant/api/team/TeamSaveApi.java
@@ -13,10 +13,12 @@ import com.alibaba.fastjson.JSONObject;
 
 import codedriver.framework.auth.core.AuthAction;
 import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.common.constvalue.TeamLevel;
 import codedriver.framework.dao.mapper.TeamMapper;
 import codedriver.framework.dao.mapper.UserMapper;
 import codedriver.framework.dto.TeamUserVo;
 import codedriver.framework.dto.TeamVo;
+import codedriver.framework.exception.team.TeamLevelNotFoundException;
 import codedriver.framework.exception.team.TeamNotFoundException;
 import codedriver.framework.exception.user.UserNotFoundException;
 import codedriver.framework.restful.annotation.Description;
@@ -57,24 +59,27 @@ public class TeamSaveApi extends ApiComponentBase{
 	}
 
 
-	@Input({ @Param(name = "uuid", type = ApiParamType.STRING, desc = "组id",isRequired=false),
+	@Input({ 
+		@Param(name = "uuid", type = ApiParamType.STRING, desc = "组id",isRequired=false),
 		@Param(name = "name", type = ApiParamType.REGEX, rule = "^[A-Za-z_\\d\\u4e00-\\u9fa5]+$", desc = "组名",isRequired=true, xss=true),
 		@Param(name = "parentUuid", type = ApiParamType.STRING, desc = "父级组id"),
-		//@Param(name = "sort", type = ApiParamType.INTEGER, desc = "排序", isRequired = false),
+		@Param(name = "level", type = ApiParamType.STRING, isRequired = true, desc = "层级"),
 		@Param(name = "tagIdList", type = ApiParamType.JSONARRAY, desc = "标签ID集合"),
-			@Param( name = "userUuidList", type = ApiParamType.JSONARRAY, desc = "用户uuid集合")
-		/*@Param(name = "isHandleChildtask", type = ApiParamType.STRING, desc = "是否允许处理下级任务",isRequired=true)*/
+		@Param( name = "userUuidList", type = ApiParamType.JSONARRAY, desc = "用户uuid集合")
 	})
 	@Output({@Param(name = "uuid", type = ApiParamType.STRING, desc = "保存的组id")})
 	@Description(desc = "保存组信息")
 	@Override
 	public Object myDoService(JSONObject jsonObj) throws Exception {
 		JSONObject returnObj = new JSONObject();
+		String level = jsonObj.getString("level");
+		if(TeamLevel.getValue(level) == null) {
+			throw new TeamLevelNotFoundException(level);
+		}
 		String uuid = jsonObj.getString("uuid");
 		TeamVo teamVo = new TeamVo();
 		teamVo.setName(jsonObj.getString("name"));
-//		teamVo.setSort(jsonObj.getInteger("sort"));
-
+		teamVo.setLevel(level);
 		if(StringUtils.isNotBlank(uuid)){
 			if(teamMapper.checkTeamIsExists(uuid) == 0) {
 				throw new TeamNotFoundException(uuid);
