0ae4eb29fd3d38360f69035cdbdada61dcb3fe13
林邦泉 <linbq@techsure.com.cn>
2020-06-20 13:18:20 +0800

    --story=1003045 --user=林邦泉 restful事件-普通节点增加“动作设置” https://www.tapd.cn/54247054/s/1072434

--- src/main/java/codedriver/module/tenant/api/expression/ExpressionListApi.java
+++ src/main/java/codedriver/module/tenant/api/expression/ExpressionListApi.java
@@ -10,11 +10,13 @@ import com.alibaba.fastjson.JSONObject;
 import codedriver.framework.common.constvalue.Expression;
 import codedriver.framework.notify.dto.ExpressionVo;
 import codedriver.framework.restful.annotation.Description;
+import codedriver.framework.restful.annotation.IsActived;
 import codedriver.framework.restful.annotation.Output;
 import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.core.ApiComponentBase;
 
 @Service
+@IsActived
 public class ExpressionListApi extends ApiComponentBase {
 
 	@Override
