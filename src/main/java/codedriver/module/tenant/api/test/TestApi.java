6d980d95b4fc82f963709879dfaa16b60ed2ae40
chenqiwei <chenqw@techsure.com.cn>
2020-07-13 10:16:20 +0800

    adjust

--- src/main/java/codedriver/module/tenant/api/test/TestApi.java
+++ src/main/java/codedriver/module/tenant/api/test/TestApi.java
@@ -1,14 +1,7 @@
 package codedriver.module.tenant.api.test;
 
-import java.io.File;
-import java.io.FileInputStream;
-
-import org.apache.commons.io.IOUtils;
-import org.apache.hadoop.fs.FSDataOutputStream;
-import org.apache.hadoop.fs.Path;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.alibaba.fastjson.JSONObject;
 
@@ -17,7 +10,6 @@ import codedriver.framework.dto.UserVo;
 import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.core.ApiComponentBase;
 
-@Transactional
 @Component
 public class TestApi extends ApiComponentBase {
 
@@ -26,12 +18,12 @@ public class TestApi extends ApiComponentBase {
 
 	@Override
 	public String getToken() {
-		return "/haha/test";
+		return "/test/loadclass";
 	}
 
 	@Override
 	public String getName() {
-		return "测试输出log接口";
+		return "测试动态加载类";
 	}
 
 	@Override
@@ -39,13 +31,9 @@ public class TestApi extends ApiComponentBase {
 		return null;
 	}
 
-	@Description(desc = "测试输出log接口")
+	@Description(desc = "测试动态加载类")
 	@Override
 	public Object myDoService(JSONObject jsonObj) throws Exception {
-		UserVo userVo = new UserVo();
-		userVo.setUuid("20dea39b97cf11ea94ff005056c00001");
-		userVo.setIsActive(1);
-		userMapper.updateUserActive(userVo);
 		return null;
 	}
 
