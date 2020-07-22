0ca96d402c608df8d0a51435ca4be5aef14a1834
Aienao <laiwt@techsure.com.cn>
2020-07-22 12:10:51 +0800

    改造接口管理目录树-增加是否有子目录标识暂存

--- src/main/java/codedriver/module/tenant/api/apimanage/ApiManageTreeSearchApi.java
+++ src/main/java/codedriver/module/tenant/api/apimanage/ApiManageTreeSearchApi.java
@@ -2,14 +2,12 @@ package codedriver.module.tenant.api.apimanage;
 
 import codedriver.framework.common.util.ModuleUtil;
 import codedriver.framework.dto.ModuleGroupVo;
-import codedriver.framework.dto.ModuleVo;
 import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.Input;
 import codedriver.framework.restful.annotation.Output;
 import codedriver.framework.restful.core.ApiComponentBase;
 import codedriver.framework.restful.core.ApiComponentFactory;
 import codedriver.framework.restful.dao.mapper.ApiMapper;
-import codedriver.framework.restful.dto.ApiAuditVo;
 import codedriver.framework.restful.dto.ApiVo;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
@@ -54,12 +52,13 @@ public class ApiManageTreeSearchApi extends ApiComponentBase {
 	@Description(desc = "接口管理-树形目录接口")
 	@Override
 	public Object myDoService(JSONObject jsonObj) throws Exception {
-		// TODO 根据apiType判断返回系统接口目录还是自定义接口目录
+		// TODO 根据apiType判断返回系统接口目录还是自定义接口目录，还要有全部的目录，以提供给操作审计使用
 		// TODO 默认展示两层目录，在第二层标识是否有子目录
 		// TODO 增加一个接口，点击第二层目录时查询其下所有子目录
 		String apiMenuType = jsonObj.getString("apiMenuType");
 		//存储最终目录的数组
 		JSONArray menuJsonArray = new JSONArray();
+		List<Map<String,Object>> menuMapList = new ArrayList<>();
 		if(StringUtils.isNotBlank(apiMenuType)){
 			List<ApiVo> apiList = null;
 			//获取系统中所有的模块
@@ -80,60 +79,119 @@ public class ApiManageTreeSearchApi extends ApiComponentBase {
 			}
 			if(CollectionUtils.isNotEmpty(apiList)){
 				for(Map.Entry<String, ModuleGroupVo> vo : moduleGroupVoMap.entrySet()){
-					JSONObject moduleJson = new JSONObject();
-					moduleJson.put("moduleGroup",vo.getKey());
-					moduleJson.put("moduleGroupName",vo.getValue().getGroupName());
+					Map<String,Object> moduleMap = new JSONObject();
+					moduleMap.put("moduleGroup",vo.getKey());
+					moduleMap.put("moduleGroupName",vo.getValue().getGroupName());
 					//多个token的第一个单词相同，用Set可以去重
-					Set<String> funcSet = new HashSet<>(16);
+					Set<Func> funcSet = new HashSet<>(16);
 					for(ApiVo apiVo : apiList){
 						String moduleGroup = apiVo.getModuleGroup();
 						if(vo.getKey().equals(moduleGroup)){
 							String token = apiVo.getToken();
-							String funcId;
+							Func func = new Func();
 //						int count = (token.length() - token.replace("/", "").length());
 //						System.out.println(count);
 							//有些API的token没有“/”，比如登出接口
 							if(token.indexOf("/") < 0){
-								funcId = token;
+								func.setFuncId(token);
 							}else{
-								funcId = token.substring(0,token.indexOf("/"));
+								func.setFuncId(token.substring(0,token.indexOf("/")));
 							}
-							funcSet.add(funcId);
+							funcSet.add(func);
 						}
 					}
-					moduleJson.put("funcList",funcSet);
-					menuJsonArray.add(moduleJson);
+					moduleMap.put("funcList",funcSet);
+					menuMapList.add(moduleMap);
+				}
+			}
+			//TODO 分析出每个模块每个功能下最长的token有几个"/"，以此来判断是否有子目录
+			List<Map<String,Object>> menuArray = new ArrayList();
+			for(Map<String,Object> map : menuMapList){
+				Set<Func> funcSet = (Set<Func>)map.get("funcList");
+				Map<String,Object> moduleMap = new HashMap<>();
+				moduleMap.put("moduleGroup",map.get("moduleGroup").toString());
+				List<Map<String,Object>> funcList = new ArrayList<>();
+				for(Func func : funcSet){
+					Map<String,Object> funcMap = new HashMap<>();
+					funcMap.put("funcId",func.getFuncId());
+					int count = 0;
+					for(ApiVo apiVo : apiList){
+						String moduleGroup = apiVo.getModuleGroup();
+						if(map.get("moduleGroup").toString().equals(moduleGroup)){
+							String token = apiVo.getToken();
+							if(token.startsWith(func.getFuncId())){
+								if((token.length() - token.replace("/", "").length()) >= count){
+									count = (token.length() - token.replace("/", "").length());
+								}
+							}
+							funcMap.put("count",count);
+						}
+
+					}
+					funcList.add(funcMap);
+				}
+				moduleMap.put("funcList",funcList);
+				menuArray.add(moduleMap);
+			}
+			System.out.println(menuArray);
+
+			//TODO 匹配目录集，去给hasChildren赋值
+			for(Map<String,Object> moduleMap : menuMapList){
+				for(Map<String,Object> map : menuArray){
+					if(moduleMap.get("moduleGroup").equals(map.get("moduleGroup"))){
+						Set<Func> funcList = (Set<Func>) moduleMap.get("funcList");
+						List<Map<String,Object>> countFuncList = (List<Map<String,Object>>)map.get("funcList");
+						for(Func func : funcList){
+							for(Map<String,Object> countMap : countFuncList){
+								if(func.getFuncId().equals(countMap.get("funcId"))){
+									if(Integer.parseInt(countMap.get("count").toString()) > 1){
+										func.setHasChildren(true);
+									}
+								}
+							}
+						}
+					}
 				}
 			}
 		}
-		return menuJsonArray;
+		return menuMapList;
 	}
 
-//	class Func{
-//		private String funcId;
-//
-//		public String getFuncId() {
-//			return funcId;
-//		}
-//
-//		public void setFuncId(String funcId) {
-//			this.funcId = funcId;
-//		}
-//
-//		@Override
-//		public boolean equals(Object o) {
-//			if (this == o) return true;
-//			if (o == null || getClass() != o.getClass()) return false;
-//			Func func = (Func) o;
-//			return Objects.equals(funcId, func.funcId);
-//		}
-//
-//		@Override
-//		public int hashCode() {
-//			final int prime = 31;
-//			int result = 1;
-//			result = prime * result + ((funcId == null) ? 0 : funcId.hashCode());
-//			return result;
-//		}
-//	}
+	class Func{
+		private String funcId;
+
+		private Boolean hasChildren = false;
+
+		public String getFuncId() {
+			return funcId;
+		}
+
+		public void setFuncId(String funcId) {
+			this.funcId = funcId;
+		}
+
+		public Boolean getHasChildren() {
+			return hasChildren;
+		}
+
+		public void setHasChildren(Boolean hasChildren) {
+			this.hasChildren = hasChildren;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			Func func = (Func) o;
+			return Objects.equals(funcId, func.funcId);
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((funcId == null) ? 0 : funcId.hashCode());
+			return result;
+		}
+	}
 }
