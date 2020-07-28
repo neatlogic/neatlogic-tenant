2cc6b64cf8b7808ddc6cb6d845c6d2f957d054b1
吕佐康 <lvzk@techsure.com.cn>
2020-07-28 14:18:08 +0800

    update

--- src/main/java/codedriver/module/tenant/api/groupsearch/GroupSearchListApi.java
+++ src/main/java/codedriver/module/tenant/api/groupsearch/GroupSearchListApi.java
@@ -1,18 +1,20 @@
 package codedriver.module.tenant.api.groupsearch;
 
-import codedriver.framework.reminder.core.OperationTypeEnum;
-import codedriver.framework.restful.annotation.*;
 import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
-import codedriver.framework.auth.core.AuthAction;
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.common.constvalue.GroupSearch;
+import codedriver.framework.reminder.core.OperationTypeEnum;
+import codedriver.framework.restful.annotation.Description;
+import codedriver.framework.restful.annotation.Input;
+import codedriver.framework.restful.annotation.OperationType;
+import codedriver.framework.restful.annotation.Output;
+import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.core.ApiComponentBase;
 
-@AuthAction(name = "SYSTEM_USER_EDIT")
 @Service
 @OperationType(type = OperationTypeEnum.SEARCH)
 public class GroupSearchListApi extends ApiComponentBase {
