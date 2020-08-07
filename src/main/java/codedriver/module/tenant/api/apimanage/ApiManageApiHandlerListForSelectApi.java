1e5070bc206041fbcc1a20a8f8da51112ce3b64c
Aienao <laiwt@techsure.com.cn>
2020-08-07 15:41:34 +0800

    --story=1003202 --user=赖文韬 pc端下拉接口统一由search改成专门下拉接口 https://www.tapd.cn/54247054/s/1081400

---
+++ src/main/java/codedriver/module/tenant/api/apimanage/ApiManageApiHandlerListForSelectApi.java
@@ -0,0 +1,59 @@
+package codedriver.module.tenant.api.apimanage;
+
+import codedriver.framework.common.dto.ValueTextVo;
+import codedriver.framework.reminder.core.OperationTypeEnum;
+import codedriver.framework.restful.annotation.*;
+import codedriver.framework.restful.core.ApiComponentBase;
+import codedriver.framework.restful.core.ApiComponentFactory;
+import codedriver.framework.restful.dto.ApiHandlerVo;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@OperationType(type = OperationTypeEnum.SEARCH)
+public class ApiManageApiHandlerListForSelectApi extends ApiComponentBase {
+
+    @Override
+    public String getToken() {
+        return "apimanage/apihandler/list/forselect";
+    }
+
+    @Override
+    public String getName() {
+        return "获取接口组件列表_下拉框";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Input({})
+    @Output({@Param(explode = ValueTextVo[].class, desc = "接口组件列表")})
+    @Description(desc = "获取接口组件列表_下拉框")
+    @Override
+    public Object myDoService(JSONObject jsonObj) throws Exception {
+
+        List<ValueTextVo> resultList = new ArrayList<>();
+
+        List<ApiHandlerVo> apiHandlerList = new ArrayList<>();
+        for (ApiHandlerVo apiHandlerVo : ApiComponentFactory.getApiHandlerList()) {
+            if (apiHandlerVo.isPrivate() != false) {
+                continue;
+            }
+            apiHandlerList.add(apiHandlerVo);
+        }
+        apiHandlerList.sort((apiHandler1, apiHandler2) -> apiHandler1.getHandler().compareTo(apiHandler2.getHandler()));
+
+        for(ApiHandlerVo vo : apiHandlerList){
+            resultList.add(new ValueTextVo(vo.getHandler(),vo.getName()));
+        }
+
+        return resultList;
+
+    }
+
+}
