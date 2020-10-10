42bd426f6db9fd1b52bdca543b84eb916ee31d7e
chenqiwei <chenqw@techsure.com.cn>
2020-10-10 15:13:35 +0800

    增加动态修改日志级别接口

---
+++ src/main/java/codedriver/module/tenant/api/logger/changeLoggerLevelApi.java
@@ -0,0 +1,52 @@
+
+package codedriver.module.tenant.api.logger;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.alibaba.fastjson.JSONObject;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.reminder.core.OperationTypeEnum;
+import codedriver.framework.restful.annotation.Description;
+import codedriver.framework.restful.annotation.Input;
+import codedriver.framework.restful.annotation.OperationType;
+import codedriver.framework.restful.annotation.Output;
+import codedriver.framework.restful.annotation.Param;
+import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+
+@Component
+@OperationType(type = OperationTypeEnum.UPDATE)
+public class changeLoggerLevelApi extends PrivateApiComponentBase {
+
+    @Override
+    public String getToken() {
+        return "logger/updatelevel";
+    }
+
+    @Override
+    public String getName() {
+        return "修改日志级别";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Input({@Param(name = "level", type = ApiParamType.ENUM, rule = "ALL,TRACE,DEBUG,INFO,WARN,ERROR,OFF",
+        isRequired = true, desc = "日志级别:ALL,TRACE,DEBUG,INFO,WARN,ERROR,OFF")})
+    @Output({@Param(type = ApiParamType.STRING, desc = "当前日志级别")})
+    @Description(desc = "修改日志级别接口")
+    @Override
+    public Object myDoService(JSONObject jsonObj) throws Exception {
+        String level = jsonObj.getString("level");
+        LoggerContext loggerContext = (LoggerContext)LoggerFactory.getILoggerFactory();
+        ch.qos.logback.classic.Logger logger = loggerContext.getLogger("codedriver");
+        logger.setLevel(Level.toLevel(level));
+        return logger.getLevel().levelStr;
+    }
+
+}
