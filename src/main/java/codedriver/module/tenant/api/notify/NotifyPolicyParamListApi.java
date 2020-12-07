5f3bc3677bd5e3739eeca55fb3b4b7a909e613f8
Aienao <laiwt@techsure.com.cn>
2020-12-07 17:51:43 +0800

    --story=1003767 --user=赖文韬 通知策略页面优化 https://www.tapd.cn/54247054/s/1113629

--- src/main/java/codedriver/module/tenant/api/notify/NotifyPolicyParamListApi.java
+++ src/main/java/codedriver/module/tenant/api/notify/NotifyPolicyParamListApi.java
@@ -3,10 +3,14 @@ package codedriver.module.tenant.api.notify;
 import java.util.ArrayList;
 import java.util.List;
 
+import codedriver.framework.common.dto.BasePageVo;
+import codedriver.framework.common.util.PageUtil;
 import codedriver.framework.reminder.core.OperationTypeEnum;
 import codedriver.framework.restful.annotation.*;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -45,9 +49,17 @@ public class NotifyPolicyParamListApi extends PrivateApiComponentBase {
         return null;
     }
 
-    @Input({@Param(name = "keyword", type = ApiParamType.STRING, desc = "模糊匹配"),
-        @Param(name = "policyId", type = ApiParamType.LONG, isRequired = true, desc = "策略id")})
-    @Output({@Param(name = "paramList", explode = ConditionParamVo[].class, desc = "参数列表")})
+    @Input({
+            @Param(name = "policyId", type = ApiParamType.LONG, isRequired = true, desc = "策略id"),
+            @Param(name = "keyword", type = ApiParamType.STRING, desc = "关键字", xss=true),
+            @Param(name = "currentPage", type = ApiParamType.INTEGER, desc = "当前页"),
+            @Param(name = "pageSize", type = ApiParamType.INTEGER, desc = "每页数据条目"),
+            @Param(name = "needPage", type = ApiParamType.BOOLEAN, desc = "是否需要分页，默认true")
+    })
+    @Output({
+            @Param(explode = BasePageVo.class),
+            @Param(name = "paramList", explode = ConditionParamVo[].class, desc = "参数列表")
+    })
     @Description(desc = "通知策略参数列表接口")
     @Override
     public Object myDoService(JSONObject jsonObj) throws Exception {
@@ -65,11 +77,11 @@ public class NotifyPolicyParamListApi extends PrivateApiComponentBase {
         NotifyPolicyConfigVo config = notifyPolicyVo.getConfig();
         List<ConditionParamVo> customParamList = config.getParamList();
         systemParamList.addAll(customParamList);
-        String keyword = jsonObj.getString("keyword");
+        BasePageVo basePageVo = JSON.parseObject(jsonObj.toJSONString(), new TypeReference<BasePageVo>() { });
         for (ConditionParamVo notifyPolicyParamVo : systemParamList) {
-            if (StringUtils.isNotBlank(keyword)) {
-                if (!notifyPolicyParamVo.getName().toLowerCase().contains(keyword.toLowerCase())
-                    && !notifyPolicyParamVo.getLabel().toLowerCase().contains(keyword.toLowerCase())) {
+            if (StringUtils.isNotBlank(basePageVo.getKeyword())) {
+                if (!notifyPolicyParamVo.getName().toLowerCase().contains(basePageVo.getKeyword().toLowerCase())
+                    && !notifyPolicyParamVo.getLabel().toLowerCase().contains(basePageVo.getKeyword().toLowerCase())) {
                     continue;
                 }
             }
@@ -77,6 +89,20 @@ public class NotifyPolicyParamListApi extends PrivateApiComponentBase {
         }
         paramList.sort((e1, e2) -> e1.getName().compareToIgnoreCase(e2.getName()));
         JSONObject resultObj = new JSONObject();
+        if(basePageVo.getNeedPage()){
+            int rowNum = paramList.size();
+            int pageCount = PageUtil.getPageCount(rowNum, basePageVo.getPageSize());
+            resultObj.put("currentPage", basePageVo.getCurrentPage());
+            resultObj.put("pageSize", basePageVo.getPageSize());
+            resultObj.put("pageCount", pageCount);
+            resultObj.put("rowNum", rowNum);
+            int fromIndex = basePageVo.getStartNum();
+            if(fromIndex < rowNum) {
+                int toIndex = fromIndex + basePageVo.getPageSize();
+                toIndex = toIndex > rowNum ? rowNum : toIndex;
+                paramList = paramList.subList(fromIndex, toIndex);
+            }
+        }
         resultObj.put("paramList", paramList);
         return resultObj;
     }
--- src/main/java/codedriver/module/tenant/api/notify/NotifyPolicyTemplateListApi.java
+++ src/main/java/codedriver/module/tenant/api/notify/NotifyPolicyTemplateListApi.java
@@ -3,10 +3,14 @@ package codedriver.module.tenant.api.notify;
 import java.util.ArrayList;
 import java.util.List;
 
+import codedriver.framework.common.dto.BasePageVo;
+import codedriver.framework.common.util.PageUtil;
 import codedriver.framework.reminder.core.OperationTypeEnum;
 import codedriver.framework.restful.annotation.*;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -45,10 +49,18 @@ public class NotifyPolicyTemplateListApi extends PrivateApiComponentBase {
         return null;
     }
 
-    @Input({@Param(name = "keyword", type = ApiParamType.STRING, desc = "模糊匹配"),
-        @Param(name = "policyId", type = ApiParamType.LONG, isRequired = true, desc = "策略id"),
-        @Param(name = "notifyHandler", type = ApiParamType.STRING, desc = "通知处理器")})
-    @Output({@Param(name = "templateList", explode = NotifyTemplateVo[].class, desc = "通知模板列表")})
+    @Input({
+            @Param(name = "policyId", type = ApiParamType.LONG, isRequired = true, desc = "策略id"),
+            @Param(name = "notifyHandler", type = ApiParamType.STRING, desc = "通知处理器"),
+            @Param(name = "keyword", type = ApiParamType.STRING, desc = "关键字", xss=true),
+            @Param(name = "currentPage", type = ApiParamType.INTEGER, desc = "当前页"),
+            @Param(name = "pageSize", type = ApiParamType.INTEGER, desc = "每页数据条目"),
+            @Param(name = "needPage", type = ApiParamType.BOOLEAN, desc = "是否需要分页，默认true")
+    })
+    @Output({
+            @Param(explode = BasePageVo.class),
+            @Param(name = "templateList", explode = NotifyTemplateVo[].class, desc = "通知模板列表")
+    })
     @Description(desc = "通知策略模板列表接口")
     @Override
     public Object myDoService(JSONObject jsonObj) throws Exception {
@@ -67,22 +79,34 @@ public class NotifyPolicyTemplateListApi extends PrivateApiComponentBase {
         }
 
         List<NotifyTemplateVo> templateList = new ArrayList<>();
-        String keyword = jsonObj.getString("keyword");
-        if (StringUtils.isNotBlank(keyword)) {
-            keyword = keyword.toLowerCase();
-        }
+        BasePageVo basePageVo = JSON.parseObject(jsonObj.toJSONString(), new TypeReference<NotifyTemplateVo>(){});
+
         NotifyPolicyConfigVo config = notifyPolicyVo.getConfig();
         for (NotifyTemplateVo notifyTemplateVo : config.getTemplateList()) {
             if (StringUtils.isNotBlank(notifyHandler) && !notifyHandler.equals(notifyTemplateVo.getNotifyHandler())) {
                 continue;
             }
-            if (StringUtils.isNotBlank(keyword) && !notifyTemplateVo.getName().toLowerCase().contains(keyword)) {
+            if (StringUtils.isNotBlank(basePageVo.getKeyword()) && !notifyTemplateVo.getName().toLowerCase().contains(basePageVo.getKeyword().toLowerCase())) {
                 continue;
             }
             templateList.add(notifyTemplateVo);
         }
-
         JSONObject resultObj = new JSONObject();
+        if(basePageVo.getNeedPage()){
+            int rowNum = templateList.size();
+            int pageCount = PageUtil.getPageCount(rowNum, basePageVo.getPageSize());
+            resultObj.put("currentPage", basePageVo.getCurrentPage());
+            resultObj.put("pageSize", basePageVo.getPageSize());
+            resultObj.put("pageCount", pageCount);
+            resultObj.put("rowNum", rowNum);
+            int fromIndex = basePageVo.getStartNum();
+            if(fromIndex < rowNum) {
+                int toIndex = fromIndex + basePageVo.getPageSize();
+                toIndex = toIndex > rowNum ? rowNum : toIndex;
+                templateList = templateList.subList(fromIndex, toIndex);
+            }
+        }
+
         resultObj.put("templateList", templateList);
         return resultObj;
     }
