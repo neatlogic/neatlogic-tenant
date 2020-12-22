c434825e0904f926e25406a295e2a2c5e3ae2d08
Aienao <laiwt@techsure.com.cn>
2020-12-22 14:33:23 +0800

    --story=1003797 --user=赖文韬 定时发送待办任务通知 https://www.tapd.cn/54247054/s/1118016 【通知内容插件工厂】

---
+++ src/main/java/codedriver/module/tenant/api/notify/job/NotifyContentHandlerListApi.java
@@ -0,0 +1,37 @@
+package codedriver.module.tenant.api.notify.job;
+
+import codedriver.framework.common.dto.ValueTextVo;
+import codedriver.framework.notify.core.NotifyContentHandlerFactory;
+import codedriver.framework.reminder.core.OperationTypeEnum;
+import codedriver.framework.restful.annotation.*;
+import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Service;
+
+@Service
+@OperationType(type = OperationTypeEnum.SEARCH)
+public class NotifyContentHandlerListApi extends PrivateApiComponentBase {
+
+	@Override
+	public String getToken() {
+		return "notify/content/handler/list";
+	}
+
+	@Override
+	public String getName() {
+		return "获取通知内容插件列表";
+	}
+
+	@Override
+	public String getConfig() {
+		return null;
+	}
+
+	@Input({})
+	@Output({@Param(explode = ValueTextVo[].class,desc = "通知内容插件列表")})
+	@Description(desc = "获取通知内容插件列表")
+	@Override
+	public Object myDoService(JSONObject jsonObj) throws Exception {
+		return NotifyContentHandlerFactory.getNotifyContentHandlerList();
+	}
+}
