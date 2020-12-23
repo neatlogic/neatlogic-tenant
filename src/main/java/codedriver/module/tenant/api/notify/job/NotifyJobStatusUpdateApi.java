c13d501e6645ba6c4ac8d4611a725180615c9fc1
Aienao <laiwt@techsure.com.cn>
2020-12-23 18:24:53 +0800

    --story=1003797 --user=赖文韬 定时发送待办任务通知 https://www.tapd.cn/54247054/s/1118488 【更改激活状态接口】

--- src/main/java/codedriver/module/tenant/api/notify/job/NotifyJobStatusUpdateApi.java
+++ src/main/java/codedriver/module/tenant/api/notify/job/NotifyJobStatusUpdateApi.java
@@ -1,7 +1,11 @@
 package codedriver.module.tenant.api.notify.job;
 
+import codedriver.framework.asynchronization.threadlocal.UserContext;
 import codedriver.framework.auth.core.AuthAction;
 import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.notify.dao.mapper.NotifyJobMapper;
+import codedriver.framework.notify.dto.job.NotifyJobVo;
+import codedriver.framework.notify.exception.NotifyJobNotFoundException;
 import codedriver.framework.reminder.core.OperationTypeEnum;
 import codedriver.framework.restful.annotation.*;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
@@ -15,6 +19,9 @@ import org.springframework.stereotype.Service;
 @AuthAction(action = NOTIFY_JOB_MODIFY.class)
 @OperationType(type = OperationTypeEnum.UPDATE)
 public class NotifyJobStatusUpdateApi extends PrivateApiComponentBase {
+
+	@Autowired
+	private NotifyJobMapper notifyJobMapper;
 
 	@Autowired
 	private SchedulerManager schedulerManager;
@@ -42,7 +49,17 @@ public class NotifyJobStatusUpdateApi extends PrivateApiComponentBase {
 	@Description(desc = "修改通知定时任务激活状态")
 	@Override
 	public Object myDoService(JSONObject jsonObj) throws Exception {
-
+		Long id = jsonObj.getLong("id");
+		Integer isActive = jsonObj.getInteger("isActive");
+		NotifyJobVo job = notifyJobMapper.getJobBaseInfoById(id);
+		if(job == null){
+			throw new NotifyJobNotFoundException(id);
+		}else{
+			job.setLcu(UserContext.get().getUserUuid());
+			job.setIsActive(isActive);
+			notifyJobMapper.updateJobStatus(job);
+		}
+		//TODO 启动或停止定时任务
 		return null;
 	}
 }
