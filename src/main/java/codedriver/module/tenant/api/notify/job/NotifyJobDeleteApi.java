45b5ff4798d06959ba3ff4a2bb64065ce3e8807f
Aienao <laiwt@techsure.com.cn>
2020-12-24 17:13:28 +0800

    --story=1003797 --user=赖文韬 定时发送待办任务通知 https://www.tapd.cn/54247054/s/1118488 【定时器暂存】

--- src/main/java/codedriver/module/tenant/api/notify/job/NotifyJobDeleteApi.java
+++ src/main/java/codedriver/module/tenant/api/notify/job/NotifyJobDeleteApi.java
@@ -1,14 +1,19 @@
 package codedriver.module.tenant.api.notify.job;
 
+import codedriver.framework.asynchronization.threadlocal.TenantContext;
 import codedriver.framework.auth.core.AuthAction;
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.notify.dao.mapper.NotifyJobMapper;
+import codedriver.framework.notify.dto.job.NotifyJobVo;
 import codedriver.framework.notify.exception.NotifyJobNotFoundException;
+import codedriver.framework.notify.schedule.plugin.NotifyContentJob;
 import codedriver.framework.reminder.core.OperationTypeEnum;
 import codedriver.framework.restful.annotation.*;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import codedriver.framework.scheduler.core.IJob;
 import codedriver.framework.scheduler.core.SchedulerManager;
 import codedriver.framework.scheduler.dao.mapper.SchedulerMapper;
+import codedriver.framework.scheduler.dto.JobObject;
 import codedriver.module.tenant.auth.label.NOTIFY_JOB_MODIFY;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -51,13 +56,18 @@ public class NotifyJobDeleteApi extends PrivateApiComponentBase {
 	@Override
 	public Object myDoService(JSONObject jsonObj) throws Exception {
 		Long id = jsonObj.getLong("id");
-		if(notifyJobMapper.getJobBaseInfoById(id) == null){
+		NotifyJobVo job = notifyJobMapper.getJobBaseInfoById(id);
+		if(job == null){
 			throw new NotifyJobNotFoundException(id);
 		}
+		IJob handler = SchedulerManager.getHandler(NotifyContentJob.class.getName());
+		String tenantUuid = TenantContext.get().getTenantUuid();
+		JobObject newJobObject = new JobObject.Builder(job.getId().toString(), handler.getGroupName(), handler.getClassName(), tenantUuid).withCron(job.getCron()).addData("notifyContentJobId",job.getId()).build();
+		schedulerManager.unloadJob(newJobObject);
+		schedulerMapper.deleteJobAuditByJobUuid(id.toString());
 		notifyJobMapper.deleteJobById(id);
 		notifyJobMapper.deleteReceiverByJobId(id);
 
-		// TODO 清除定时任务
 		return null;
 	}
 }
