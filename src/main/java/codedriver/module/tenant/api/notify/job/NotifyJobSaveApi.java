2c184877ba60364566c6c2a5c68e322e02d8ebf1
Aienao <laiwt@techsure.com.cn>
2021-01-06 17:52:17 +0800

    --story=1003797 --user=赖文韬 定时发送待办任务通知 https://www.tapd.cn/54247054/s/1121726

--- src/main/java/codedriver/module/tenant/api/notify/job/NotifyJobSaveApi.java
+++ src/main/java/codedriver/module/tenant/api/notify/job/NotifyJobSaveApi.java
@@ -20,10 +20,7 @@ import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import codedriver.framework.scheduler.core.SchedulerManager;
 import codedriver.framework.scheduler.exception.ScheduleIllegalParameterException;
 import codedriver.module.tenant.auth.label.NOTIFY_JOB_MODIFY;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.TypeReference;
+import com.alibaba.fastjson.*;
 import org.apache.commons.collections4.CollectionUtils;
 import org.quartz.CronExpression;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -91,14 +88,16 @@ public class NotifyJobSaveApi extends PrivateApiComponentBase {
 			throw new NotifyJobNameRepeatException(job.getName());
 		}
 
-		/**
-		 * 组装接收人与抄送人
-		 */
-		JSONArray toArray = jsonObj.getJSONArray("toList");
-		JSONArray ccArray = jsonObj.getJSONArray("ccList");
+		Object messageConfig = JSONPath.read(jsonObj.toJSONString(), "config.messageConfig");
 		List<NotifyJobReceiverVo> toVoList = new ArrayList<>();
 		List<NotifyJobReceiverVo> ccVoList = new ArrayList<>();
-		getReceiverList(job, toArray,ccArray, toVoList,ccVoList);
+		if(messageConfig != null){
+			JSONObject jsonObject = JSONObject.parseObject(messageConfig.toString());
+			/**组装接收人与抄送人*/
+			JSONArray toArray = jsonObject.getJSONArray("toList");
+			JSONArray ccArray = jsonObject.getJSONArray("ccList");
+			getReceiverList(job, toArray,ccArray, toVoList,ccVoList);
+		}
 
 		NotifyJobVo oldJob = notifyJobMapper.getJobBaseInfoById(job.getId());
 		job.setLcu(UserContext.get().getUserUuid());
