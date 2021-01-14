30fa76abc069d071f0c6bc2d4def2cc5d2db91ff
Aienao <laiwt@techsure.com.cn>
2021-01-14 09:32:40 +0800

    --story=1003884 --user=赖文韬 系统公告-后端 https://www.tapd.cn/54247054/s/1124312

---
+++ src/main/java/codedriver/module/tenant/api/systemnotice/SystemNoticeSaveApi.java
@@ -0,0 +1,91 @@
+package codedriver.module.tenant.api.systemnotice;
+
+import codedriver.framework.asynchronization.threadlocal.UserContext;
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.restful.annotation.*;
+import codedriver.framework.restful.constvalue.OperationTypeEnum;
+import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import codedriver.framework.systemnotice.dao.mapper.SystemNoticeMapper;
+import codedriver.framework.systemnotice.dto.SystemNoticeRecipientVo;
+import codedriver.framework.systemnotice.dto.SystemNoticeVo;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * @Title: SystemNoticeSaveApi
+ * @Package: codedriver.module.tenant.api.systemnotice
+ * @Description: 系统公告保存接口
+ * @Author: laiwt
+ * @Date: 2021/1/13 18:01
+ * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ **/
+
+@Service
+@OperationType(type = OperationTypeEnum.CREATE)
+@Transactional
+public class SystemNoticeSaveApi  extends PrivateApiComponentBase {
+
+    @Autowired
+    private SystemNoticeMapper systemNoticeMapper;
+
+    @Override
+    public String getToken() {
+        return "systemnotice/save";
+    }
+
+    @Override
+    public String getName() {
+        return "保存系统公告";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+    @Input({
+            @Param(name = "id", type = ApiParamType.LONG, desc = "公告ID"),
+            @Param(name = "title", type = ApiParamType.STRING, isRequired = true, desc = "标题"),
+            @Param(name = "content", type = ApiParamType.STRING, isRequired = true, desc = "内容"),
+            @Param(name = "recipientList", type = ApiParamType.JSONARRAY, isRequired = true, desc = "通知对象列表,可多选，格式[\"user#userUuid\",\"team#teamUuid\",\"role#roleUuid\"]"),
+    })
+    @Output({@Param(explode = SystemNoticeVo.class)})
+    @Description(desc = "保存系统公告")
+    @Override
+    public Object myDoService(JSONObject jsonObj) throws Exception {
+        SystemNoticeVo vo = JSON.parseObject(jsonObj.toJSONString(), new TypeReference<SystemNoticeVo>(){});
+
+        SystemNoticeVo oldVo = systemNoticeMapper.getSystemNoticeBaseInfoById(vo.getId());
+
+        vo.setLcu(UserContext.get().getUserUuid());
+        if(oldVo == null){
+            vo.setFcu(UserContext.get().getUserUuid());
+            systemNoticeMapper.insertSystemNotice(vo);
+        }else{
+            if(SystemNoticeVo.Status.ISSUED.getValue().equals(oldVo.getStatus())){
+                // todo 先停用再编辑
+            }
+            systemNoticeMapper.updateSystemNotice(vo);
+            systemNoticeMapper.deleteRecipientByNoticeId(vo.getId());
+            // todo 只删在线用户
+            systemNoticeMapper.deleteNoticeUserByNoticeId(vo.getId());
+        }
+
+        //todo 插入通知对象
+        List<SystemNoticeRecipientVo> recipientVoList = null;
+        if(CollectionUtils.isNotEmpty(recipientVoList)){
+            systemNoticeMapper.batchInsertSystemNoticeRecipient(recipientVoList);
+        }
+
+
+
+        return null;
+    }
+}
