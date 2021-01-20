03b0d45cea0ae64b7f6a26ccf0692a48451c7b33
林邦泉 <linbq@techsure.com.cn>
2021-01-20 15:30:09 +0800

    --bug=1007045 --user=林邦泉 用户管理--用户被禁用后直接从用户列表消失 https://www.tapd.cn/54247054/s/1127510

--- src/main/java/codedriver/module/tenant/api/user/UserActiveUpdateApi.java
+++ src/main/java/codedriver/module/tenant/api/user/UserActiveUpdateApi.java
@@ -3,6 +3,7 @@ package codedriver.module.tenant.api.user;
 import codedriver.framework.auth.core.AuthAction;
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.dao.mapper.UserMapper;
+import codedriver.framework.dto.UserAuthVo;
 import codedriver.framework.dto.UserVo;
 import codedriver.framework.exception.user.UserNotFoundException;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
@@ -26,40 +27,48 @@ import java.util.List;
 @AuthAction(action = AUTHORITY_MODIFY.class)
 public class UserActiveUpdateApi extends PrivateApiComponentBase {
 
-	@Autowired
-	private UserMapper userMapper;
+    @Autowired
+    private UserMapper userMapper;
 
-	@Override
-	public String getToken() {
-		return "user/active";
-	}
+    @Override
+    public String getToken() {
+        return "user/active";
+    }
 
-	@Override
-	public String getName() {
-		return "用户有效性变更接口";
-	}
+    @Override
+    public String getName() {
+        return "用户有效性变更接口";
+    }
 
-	@Override
-	public String getConfig() {
-		return null;
-	}
+    @Override
+    public String getConfig() {
+        return null;
+    }
 
-	@Input({ @Param(name = "userUuidList", type = ApiParamType.JSONARRAY, desc = "用户uuid集合", isRequired = true), @Param(name = "isActive", type = ApiParamType.INTEGER, desc = "有效性", isRequired = true) })
-	@Override
-	public Object myDoService(JSONObject jsonObj) throws Exception {
-		Integer isActive = jsonObj.getInteger("isActive");
-		List<String> userUuidList = JSON.parseArray(jsonObj.getString("userUuidList"), String.class);
-		if (CollectionUtils.isNotEmpty(userUuidList)) {
-			UserVo userVo = new UserVo();
-			userVo.setIsActive(isActive);
-			for (String userUuid : userUuidList) {
-				if (userMapper.checkUserIsExists(userUuid) == 0) {
-					throw new UserNotFoundException(userUuid);
-				}
-				userVo.setUuid(userUuid);
-				userMapper.updateUserActive(userVo);
-			}
-		}
-		return null;
-	}
+    @Input({
+            @Param(name = "userUuidList", type = ApiParamType.JSONARRAY, desc = "用户uuid集合", isRequired = true),
+            @Param(name = "isActive", type = ApiParamType.ENUM, rule = "0,1", desc = "有效性", isRequired = true)
+    })
+    @Override
+    public Object myDoService(JSONObject jsonObj) throws Exception {
+        Integer isActive = jsonObj.getInteger("isActive");
+        List<String> userUuidList = JSON.parseArray(jsonObj.getString("userUuidList"), String.class);
+        if (CollectionUtils.isNotEmpty(userUuidList)) {
+            UserVo userVo = new UserVo();
+            userVo.setIsActive(isActive);
+            for (String userUuid : userUuidList) {
+                if (userMapper.checkUserIsExists(userUuid) == 0) {
+                    throw new UserNotFoundException(userUuid);
+                }
+                userVo.setUuid(userUuid);
+                userMapper.updateUserActive(userVo);
+                if(isActive == 0){
+                    userMapper.deleteUserAuth(new UserAuthVo(userUuid));
+                    userMapper.deleteUserRoleByUserUuid(userUuid);
+                    userMapper.deleteUserTeamByUserUuid(userUuid);
+                }
+            }
+        }
+        return null;
+    }
 }
