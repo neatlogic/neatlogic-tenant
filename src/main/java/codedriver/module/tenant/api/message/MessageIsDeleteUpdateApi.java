19bd2f2e37aa039308dc3126508effc58b7fb887
林邦泉 <linbq@techsure.com.cn>
2021-01-22 15:51:03 +0800

    --bug=1007102 --user=林邦泉 消息中心-删除全天历史消息异常 https://www.tapd.cn/54247054/s/1128739

--- src/main/java/codedriver/module/tenant/api/message/MessageIsDeleteUpdateApi.java
+++ src/main/java/codedriver/module/tenant/api/message/MessageIsDeleteUpdateApi.java
@@ -2,7 +2,10 @@ package codedriver.module.tenant.api.message;
 
 import codedriver.framework.asynchronization.threadlocal.UserContext;
 import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.common.dto.BasePageVo;
 import codedriver.framework.message.dao.mapper.MessageMapper;
+import codedriver.framework.message.dto.MessageSearchVo;
+import codedriver.framework.message.dto.MessageVo;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.annotation.*;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
@@ -53,28 +56,37 @@ public class MessageIsDeleteUpdateApi extends PrivateApiComponentBase {
             @Param(name = "messageIdList", type = ApiParamType.JSONARRAY, desc = "消息id列表"),
             @Param(name = "date", type = ApiParamType.LONG, desc = "日期")
     })
+    @Output({
+            @Param(name = "newCount", type = ApiParamType.INTEGER, desc = "新消息总数")
+    })
     @Description(desc = "更新消息为已删除")
     @Override
     public Object myDoService(JSONObject jsonObj) throws Exception {
+        String userUuid = UserContext.get().getUserUuid(true);
         Long messageId = jsonObj.getLong("messageId");
         if (messageId != null) {
-            messageMapper.updateMessageUserIsDeleteByUserUuidAndMessageId(UserContext.get().getUserUuid(true), messageId);
+            messageMapper.updateMessageUserIsDeleteByUserUuidAndMessageId(userUuid, messageId);
         } else {
             List<Long> messageIdList = (List<Long>) jsonObj.get("messageIdList");
             if (CollectionUtils.isNotEmpty(messageIdList)) {
-                messageMapper.updateMessageUserIsDeleteByUserUuidAndMessageIdList(UserContext.get().getUserUuid(true), messageIdList);
+                messageMapper.updateMessageUserIsDeleteByUserUuidAndMessageIdList(userUuid, messageIdList);
             } else {
                 Date date = jsonObj.getDate("date");
                 if (date != null) {
                     Long fromMessageId = messageMapper.getMessageMaxIdByLessThanFcd(date);
                     Date nextDay = new Date(date.getTime() + TimeUnit.HOURS.toMillis(24));
                     Long toMessageId = messageMapper.getMessageMaxIdByLessThanFcd(nextDay);
-                    messageMapper.updateMessageUserIsDeleteByUserUuidAndMessageIdRange(UserContext.get().getUserUuid(true), fromMessageId, toMessageId);
+                    messageMapper.updateMessageUserIsDeleteByUserUuidAndMessageIdRange(userUuid, fromMessageId, toMessageId);
                 } else {
-                    messageMapper.updateMessageUserIsDeleteByUserUuid(UserContext.get().getUserUuid(true));
+                    messageMapper.updateMessageUserIsDeleteByUserUuid(userUuid);
                 }
             }
         }
+        JSONObject resultObj = new JSONObject();
+        MessageSearchVo searchVo = new MessageSearchVo();
+        searchVo.setUserUuid(userUuid);
+        int newCount = messageMapper.getMessageNewCount(searchVo);
+        resultObj.put("newCount", newCount);
         return null;
     }
 }
--- src/main/java/codedriver/module/tenant/api/message/MessagePullApi.java
+++ src/main/java/codedriver/module/tenant/api/message/MessagePullApi.java
@@ -60,6 +60,7 @@ public class MessagePullApi extends PrivateApiComponentBase {
     @Output({
             @Param(name = "tbodyList", explode = MessageVo[].class, desc = "消息列表"),
             @Param(name = "hasSubscription", type = ApiParamType.INTEGER, desc = "是否有订阅消息"),
+            @Param(name = "newCount", type = ApiParamType.INTEGER, desc = "新消息总数"),
             @Param(explode = BasePageVo.class)
     })
     @Description(desc = "拉取新消息列表")
