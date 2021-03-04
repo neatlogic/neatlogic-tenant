1b76cc667a09074fa10f42771b8d84b8e9c5f8f9
林邦泉 <linbq@techsure.com.cn>
2021-03-04 11:06:37 +0800

    --bug=1007506 --user=林邦泉 角色、分组、授权管理-关联用户时，用户列表中仍然显示已禁用的用户 https://www.tapd.cn/54247054/s/1139623

--- src/main/java/codedriver/module/tenant/api/user/UserSearchApi.java
+++ src/main/java/codedriver/module/tenant/api/user/UserSearchApi.java
@@ -4,7 +4,6 @@ import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.annotation.*;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSON;
@@ -15,92 +14,95 @@ import codedriver.framework.common.util.PageUtil;
 import codedriver.framework.dao.mapper.UserMapper;
 import codedriver.framework.dto.UserVo;
 
+import javax.annotation.Resource;
+
 @Service
 @OperationType(type = OperationTypeEnum.SEARCH)
 public class UserSearchApi extends PrivateApiComponentBase {
 
-	@Autowired
-	private UserMapper userMapper;
+    @Resource
+    private UserMapper userMapper;
 
-	@Override
-	public String getToken() {
-		return "user/search";
-	}
+    @Override
+    public String getToken() {
+        return "user/search";
+    }
 
-	@Override
-	public String getName() {
-		return "查询用户";
-	}
+    @Override
+    public String getName() {
+        return "查询用户";
+    }
 
-	@Override
-	public String getConfig() {
-		return null;
-	}
+    @Override
+    public String getConfig() {
+        return null;
+    }
 
-	@Input({
-			@Param(name = "keyword",
-					type = ApiParamType.STRING,
-					desc = "关键字(用户id或名称),模糊查询",
-					isRequired = true,
-					xss = true),
-			@Param(name = "vipLevel",
-					type = ApiParamType.INTEGER,
-					desc = "VIP等级"),
-			@Param(name = "authGroup",
-					type = ApiParamType.STRING,
-					desc = "权限模块"),
-			@Param(name = "auth",
-					type = ApiParamType.STRING,
-					desc = "权限"),
-			@Param(name = "teamUuid",
-					type = ApiParamType.STRING,
-					desc = "用户组uuid"
-			),
-			@Param(name = "roleUuid",
-					type = ApiParamType.STRING,
-					desc = "角色uuid"),
-			@Param(name = "currentPage",
-					type = ApiParamType.INTEGER,
-					desc = "当前页数",
-					isRequired = false),
-			@Param(name = "pageSize",
-					type = ApiParamType.INTEGER,
-					desc = "每页展示数量 默认10",
-					isRequired = false),
+    @Input({
+            @Param(name = "keyword",
+                    type = ApiParamType.STRING,
+                    desc = "关键字(用户id或名称),模糊查询",
+                    isRequired = true,
+                    xss = true),
+            @Param(name = "isActive", type = ApiParamType.ENUM, rule = "0,1", desc = "状态"),
+            @Param(name = "vipLevel",
+                    type = ApiParamType.INTEGER,
+                    desc = "VIP等级"),
+            @Param(name = "authGroup",
+                    type = ApiParamType.STRING,
+                    desc = "权限模块"),
+            @Param(name = "auth",
+                    type = ApiParamType.STRING,
+                    desc = "权限"),
+            @Param(name = "teamUuid",
+                    type = ApiParamType.STRING,
+                    desc = "用户组uuid"
+            ),
+            @Param(name = "roleUuid",
+                    type = ApiParamType.STRING,
+                    desc = "角色uuid"),
+            @Param(name = "currentPage",
+                    type = ApiParamType.INTEGER,
+                    desc = "当前页数",
+                    isRequired = false),
+            @Param(name = "pageSize",
+                    type = ApiParamType.INTEGER,
+                    desc = "每页展示数量 默认10",
+                    isRequired = false),
             @Param(name = "needPage",
                     type = ApiParamType.BOOLEAN,
                     desc = "是否分页")})
-	@Output({
-			@Param(name = "tbodyList",
-					type = ApiParamType.JSONARRAY,
-					explode = UserVo[].class,
-					desc = "table数据列表"),
-			@Param(name = "pageCount",
-					type = ApiParamType.INTEGER,
-					desc = "总页数"),
-			@Param(name = "currentPage",
-					type = ApiParamType.INTEGER,
-					desc = "当前页数"),
-			@Param(name = "pageSize",
-					type = ApiParamType.INTEGER,
-					desc = "每页展示数量"),
-			@Param(name = "rowNum",
-					type = ApiParamType.INTEGER,
-			 		desc = "总条目数")})
-	@Description(desc = "查询用户接口")
-	@Override
-	public Object myDoService(JSONObject jsonObj) throws Exception {
-		
-		JSONObject resultObj = new JSONObject();
-		UserVo userVo = JSON.toJavaObject(jsonObj, UserVo.class);
-		if (userVo.getNeedPage()) {
-			int rowNum = userMapper.searchUserCount(userVo);
-			resultObj.put("rowNum", rowNum);
-			resultObj.put("pageSize", userVo.getPageSize());
-			resultObj.put("currentPage", userVo.getCurrentPage());
-			resultObj.put("pageCount", PageUtil.getPageCount(rowNum, userVo.getPageSize()));
-		}
-		resultObj.put("tbodyList", userMapper.searchUser(userVo));
-		return resultObj;
-	}
+    @Output({
+            @Param(name = "tbodyList",
+                    type = ApiParamType.JSONARRAY,
+                    explode = UserVo[].class,
+                    desc = "table数据列表"),
+            @Param(name = "pageCount",
+                    type = ApiParamType.INTEGER,
+                    desc = "总页数"),
+            @Param(name = "currentPage",
+                    type = ApiParamType.INTEGER,
+                    desc = "当前页数"),
+            @Param(name = "pageSize",
+                    type = ApiParamType.INTEGER,
+                    desc = "每页展示数量"),
+            @Param(name = "rowNum",
+                    type = ApiParamType.INTEGER,
+                    desc = "总条目数")})
+    @Description(desc = "查询用户接口")
+    @Override
+    public Object myDoService(JSONObject jsonObj) throws Exception {
+
+        JSONObject resultObj = new JSONObject();
+        UserVo userVo = JSON.toJavaObject(jsonObj, UserVo.class);
+        if (userVo.getNeedPage()) {
+            int rowNum = userMapper.searchUserCount(userVo);
+            resultObj.put("rowNum", rowNum);
+            resultObj.put("pageSize", userVo.getPageSize());
+            resultObj.put("currentPage", userVo.getCurrentPage());
+            resultObj.put("pageCount", PageUtil.getPageCount(rowNum, userVo.getPageSize()));
+        }
+        resultObj.put("tbodyList", userMapper.searchUser(userVo));
+        return resultObj;
+    }
 }
