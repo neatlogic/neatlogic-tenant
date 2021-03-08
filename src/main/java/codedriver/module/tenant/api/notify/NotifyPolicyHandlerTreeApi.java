be342a014cea4ce25bbd2fe4a6937edb66ae89cd
林邦泉 <linbq@techsure.com.cn>
2021-03-08 19:51:43 +0800

    --bug=1007463 --user=林邦泉 流程设置-变更创建节点动作配置中不应有子任务相关的的触发点 https://www.tapd.cn/54247054/s/1140793

---
+++ src/main/java/codedriver/module/tenant/api/notify/NotifyPolicyHandlerTreeApi.java
@@ -0,0 +1,105 @@
+package codedriver.module.tenant.api.notify;
+
+import codedriver.framework.auth.core.AuthActionChecker;
+import codedriver.framework.common.dto.ValueTextVo;
+import codedriver.framework.exception.type.PermissionDeniedException;
+import codedriver.framework.notify.core.INotifyPolicyHandler;
+import codedriver.framework.notify.core.NotifyPolicyHandlerFactory;
+import codedriver.framework.notify.dto.NotifyTreeVo;
+import codedriver.framework.restful.annotation.Description;
+import codedriver.framework.restful.annotation.OperationType;
+import codedriver.framework.restful.annotation.Output;
+import codedriver.framework.restful.annotation.Param;
+import codedriver.framework.restful.constvalue.OperationTypeEnum;
+import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import codedriver.module.tenant.auth.label.NOTIFY_JOB_MODIFY;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Service
+@OperationType(type = OperationTypeEnum.SEARCH)
+public class NotifyPolicyHandlerTreeApi extends PrivateApiComponentBase {
+
+	@Override
+	public String getToken() {
+		return "notify/policy/handler/tree";
+	}
+
+	@Override
+	public String getName() {
+		return "通知策略分类树接口";
+	}
+
+	@Override
+	public String getConfig() {
+		return null;
+	}
+	
+	@Output({
+		@Param(explode = NotifyTreeVo[].class, desc = "通知策略分类树")
+	})
+	@Description(desc = "通知策略分类树接口")
+	@Override
+	public Object myDoService(JSONObject jsonObj) throws Exception {
+		List<NotifyTreeVo> notifyPolicyTreeVoList = copy(NotifyPolicyHandlerFactory.getNotifyPolicyTreeVoList());
+		authActionChecker(notifyPolicyTreeVoList);
+		if(CollectionUtils.isEmpty(notifyPolicyTreeVoList)){
+			throw new PermissionDeniedException();
+		}
+		return notifyPolicyTreeVoList;
+	}
+
+	/**
+	 * @Description: 检查权限，没有权限的移除
+	 * @Author: linbq
+	 * @Date: 2021/3/8 18:22
+	 * @Params:[notifyPolicyTreeVoList]
+	 * @Returns:void
+	 **/
+	private void authActionChecker(List<NotifyTreeVo> notifyPolicyTreeVoList){
+		Iterator<NotifyTreeVo> iterator = notifyPolicyTreeVoList.iterator();
+		while (iterator.hasNext()){
+			NotifyTreeVo next = iterator.next();
+			INotifyPolicyHandler handler = NotifyPolicyHandlerFactory.getHandler(next.getUuid());
+			if(handler == null){
+				List<NotifyTreeVo> children = next.getChildren();
+				if(CollectionUtils.isNotEmpty(children)){
+					authActionChecker(children);
+					if(CollectionUtils.isEmpty(children)){
+						iterator.remove();
+					}
+				}else{
+					iterator.remove();
+				}
+			}else{
+				/** 通知策略与权限绑定，例如没有流程管理权限则无法编辑流程及流程步骤通知策略 */
+				if(!AuthActionChecker.check(handler.getAuthName())){
+					iterator.remove();
+				}
+			}
+		}
+	}
+	/**
+	 * @Description: 深拷贝
+	 * @Author: linbq
+	 * @Date: 2021/3/8 18:22
+	 * @Params:[notifyTreeVoList]
+	 * @Returns:java.util.List<codedriver.framework.notify.dto.NotifyTreeVo>
+	 **/
+	private List<NotifyTreeVo> copy(List<NotifyTreeVo> notifyTreeVoList){
+		List<NotifyTreeVo> resultList = new ArrayList<>(notifyTreeVoList.size());
+		for(NotifyTreeVo notifyTreeVo : notifyTreeVoList){
+			NotifyTreeVo newNotifyTreeVo = new NotifyTreeVo(notifyTreeVo.getUuid(), notifyTreeVo.getName());
+			resultList.add(newNotifyTreeVo);
+			if(notifyTreeVo.getChildren() != null){
+				newNotifyTreeVo.setChildren(copy(notifyTreeVo.getChildren()));
+			}
+		}
+		return resultList;
+	}
+}
