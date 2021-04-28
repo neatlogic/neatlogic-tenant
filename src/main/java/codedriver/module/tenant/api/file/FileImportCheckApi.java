244db72403d01b72b133dd6c1b42fe0bfae90acc
林邦泉 <linbq@techsure.com.cn>
2021-04-28 15:49:18 +0800

    --story=1004353 --user=林邦泉 导入导出 https://www.tapd.cn/54247054/s/1152663

--- src/main/java/codedriver/module/tenant/api/file/FileImportCheckApi.java
+++ src/main/java/codedriver/module/tenant/api/file/FileImportCheckApi.java
@@ -42,88 +42,49 @@ import java.util.zip.ZipOutputStream;
 @Transactional
 @OperationType(type = OperationTypeEnum.SEARCH)
 public class FileImportCheckApi extends PrivateBinaryStreamApiComponentBase {
-	
-	@Override
-	public String getToken() {
-		return "file/import/check";
-	}
 
-	@Override
-	public String getName() {
-		return "检查导入的文件";
-	}
+    @Override
+    public String getToken() {
+        return "file/import/check";
+    }
 
-	@Override
-	public String getConfig() {
-		return null;
-	}
+    @Override
+    public String getName() {
+        return "检查导入的文件";
+    }
 
-	@Output({
-			@Param(explode = ValueTextVo[].class, desc = "文件名称列表")
-	})
-	@Description(desc = "检查导入的文件")
-	@Override
-	public Object myDoService(JSONObject paramObj, HttpServletRequest request, HttpServletResponse response) throws Exception {
-		MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
-		//获取所有导入文件
-		Map<String, MultipartFile> multipartFileMap = multipartRequest.getFileMap();
-		//如果没有导入文件, 抛异常
-		if(multipartFileMap == null || multipartFileMap.isEmpty()) {
-			throw new FileNotUploadException();
-		}
-		List<ValueTextVo> resultList = new ArrayList<>();
-		//遍历导入文件, 目前只获取第一个文件内容, 其余的放弃
-		for(Entry<String, MultipartFile> entry : multipartFileMap.entrySet()) {
-			MultipartFile multipartFile = entry.getValue();
-			try (ZipInputStream zipis = new ZipInputStream(multipartFile.getInputStream())) {
-				ZipEntry zipEntry = null;
-				while((zipEntry = zipis.getNextEntry()) != null){
-					resultList.add(new ValueTextVo(zipEntry.getName(), zipEntry.getName()));
-				}
-			}catch(IOException e) {
-				throw new FileExtNotAllowedException(multipartFile.getOriginalFilename());
-			}
-		}
-		return resultList;
-	}
+    @Override
+    public String getConfig() {
+        return null;
+    }
 
-	public static void main(String[] args) throws FileNotFoundException, IOException {
-//		ValueTextVo valueTextVo = new ValueTextVo(11111111, "linbq_20210427");
-//		String toJsonString = JSONObject.toJSONString(valueTextVo);
-//		FileOutputStream fos = new FileOutputStream("linbq_20210427.zip");
-//		GZIPOutputStream gzip = new GZIPOutputStream(fos);
-//		gzip.write(toJsonString.getBytes(StandardCharsets.UTF_8));
-//		gzip.close();
+    @Output({
+            @Param(explode = ValueTextVo[].class, desc = "文件名称列表")
+    })
+    @Description(desc = "检查导入的文件")
+    @Override
+    public Object myDoService(JSONObject paramObj, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+        //获取所有导入文件
+        Map<String, MultipartFile> multipartFileMap = multipartRequest.getFileMap();
+        //如果没有导入文件, 抛异常
+        if (multipartFileMap == null || multipartFileMap.isEmpty()) {
+            throw new FileNotUploadException();
+        }
+        List<ValueTextVo> resultList = new ArrayList<>();
+        //遍历导入文件, 目前只获取第一个文件内容, 其余的放弃
+        for (Entry<String, MultipartFile> entry : multipartFileMap.entrySet()) {
+            MultipartFile multipartFile = entry.getValue();
+            try (ZipInputStream zipis = new ZipInputStream(multipartFile.getInputStream())) {
+                ZipEntry zipEntry = null;
+                while ((zipEntry = zipis.getNextEntry()) != null) {
+                    resultList.add(new ValueTextVo(zipEntry.getName(), zipEntry.getName()));
+                }
+            } catch (IOException e) {
+                throw new FileExtNotAllowedException(multipartFile.getOriginalFilename());
+            }
+        }
+        return resultList;
+    }
 
-//		FileOutputStream fos = new FileOutputStream("linbq_20210427_2.zip");
-//		ZipOutputStream zipos = new ZipOutputStream(fos);
-//		ValueTextVo valueTextVo = new ValueTextVo(11111111, "linbq_20210427_1");
-//		zipos.putNextEntry(new ZipEntry("linbq_20210427_1.json"));
-//		zipos.write(JSONObject.toJSONBytes(valueTextVo));
-//		zipos.closeEntry();
-//		valueTextVo = new ValueTextVo(22222222, "linbq_20210427_2");
-//		zipos.putNextEntry(new ZipEntry("linbq_20210427_2.json"));
-//		zipos.write(JSONObject.toJSONBytes(valueTextVo));
-//		zipos.closeEntry();
-//		zipos.close();
-
-		FileInputStream fis = new FileInputStream("linbq_20210427_2.zip");
-		ZipInputStream zipis = new ZipInputStream(fis);
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		ZipEntry entry = null;
-		byte[] buf = new byte[1024];
-		while((entry = zipis.getNextEntry()) != null){
-			System.out.println(entry.getName());
-			int len = 0;
-			while((len = zipis.read(buf)) != -1){
-				out.write(buf, 0, len);
-			}
-			System.out.println(new String(out.toByteArray(), StandardCharsets.UTF_8));
-
-			out.reset();
-		}
-		out.close();
-		zipis.close();
-		fis.close();
-	}
 }
