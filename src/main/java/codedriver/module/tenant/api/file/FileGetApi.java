6884c1e4935ec1fea286b3ce1d2870d8de23d067
陈其炜 <chenqw@techsure.com.cn>
2021-05-06 09:01:55 +0800

    cmdb
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

---
+++ src/main/java/codedriver/module/tenant/api/file/FileGetApi.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.module.tenant.api.file;
+
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.file.dao.mapper.FileMapper;
+import codedriver.framework.file.dto.FileVo;
+import codedriver.framework.restful.annotation.*;
+import codedriver.framework.restful.constvalue.OperationTypeEnum;
+import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+@OperationType(type = OperationTypeEnum.SEARCH)
+public class FileGetApi extends PrivateApiComponentBase {
+
+    @Resource
+    private FileMapper fileMapper;
+
+    @Override
+    public String getToken() {
+        return "file/get";
+    }
+
+    @Override
+    public String getName() {
+        return "获取附件信息";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Override
+    @Input({
+            @Param(name = "id", type = ApiParamType.LONG, isRequired = true, desc = "附件id")
+    })
+    @Output({@Param(explode = FileVo.class)})
+    @Description(desc = "获取附件信息接口")
+    public Object myDoService(JSONObject jsonObj) throws Exception {
+        Long fileId = jsonObj.getLong("id");
+        return fileMapper.getFileById(fileId);
+    }
+
+}
