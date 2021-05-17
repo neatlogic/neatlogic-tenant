18d1bf2d30b4fd62ea0b5377727e1e8769851145
吕佐康 <lvzk@techsure.com.cn>
2021-05-17 19:00:55 +0800

    update get form handler List

--- src/main/java/codedriver/module/tenant/api/form/FormHandlerListApi.java
+++ src/main/java/codedriver/module/tenant/api/form/FormHandlerListApi.java
@@ -5,9 +5,9 @@
 
 package codedriver.module.tenant.api.form;
 
-import codedriver.framework.auth.core.AuthAction;
 import codedriver.framework.common.constvalue.ApiParamType;
-import codedriver.framework.form.constvalue.IFormHandlerType;
+import codedriver.framework.form.attribute.core.FormAttributeHandlerFactory;
+import codedriver.framework.form.attribute.core.IFormAttributeHandler;
 import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.OperationType;
 import codedriver.framework.restful.annotation.Output;
@@ -16,20 +16,12 @@ import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import org.reflections.Reflections;
 import org.springframework.stereotype.Service;
-
-import java.util.Set;
 
 @Service
 
 @OperationType(type = OperationTypeEnum.SEARCH)
 public class FormHandlerListApi extends PrivateApiComponentBase {
-    /**
-     * 标记是否未初始化数据，只初始化一次
-     **/
-    private static volatile boolean isUninitialized = true;
-    private static JSONArray handlerList = new JSONArray();
 
     @Override
     public String getToken() {
@@ -49,44 +41,32 @@ public class FormHandlerListApi extends PrivateApiComponentBase {
     @Output({@Param(name = "handler", type = ApiParamType.STRING, desc = "处理器"),
             @Param(name = "name", type = ApiParamType.STRING, desc = "处理器中文名"),
             @Param(name = "icon", type = ApiParamType.STRING, desc = "图标"),
-            @Param(name = "type", type = ApiParamType.ENUM, desc = "分类，form（表单组件）|control（控制组件）")})
+            @Param(name = "type", type = ApiParamType.ENUM, desc = "分类，form（表单组件）|control（控制组件）"),
+            @Param(name = "isConditionable", type = ApiParamType.INTEGER, desc = "是否可设置为条件"),
+            @Param(name = "isExtendable", type = ApiParamType.ENUM, desc = "是否有拓展属性"),
+            @Param(name = "isFilterable", type = ApiParamType.ENUM, desc = "是否可设置过滤"),
+            @Param(name = "isShowable", type = ApiParamType.ENUM, desc = "是否可设置显示隐藏"),
+            @Param(name = "isValueable", type = ApiParamType.ENUM, desc = "是否可设置赋值"),
+            @Param(name = "module", type = ApiParamType.ENUM, desc = "属模块"),
+    })
     @Description(desc = "获取表单组件列表接口")
     @Override
     public Object myDoService(JSONObject jsonObj) throws Exception {
-        return handlerList;
-    }
-
-    static {
-        if (isUninitialized) {
-            synchronized (IFormHandlerType.class) {
-                if (isUninitialized) {
-                    Reflections reflections = new Reflections("codedriver");
-                    Set<Class<? extends IFormHandlerType>> classSet =
-                            reflections.getSubTypesOf(IFormHandlerType.class);
-                    for (Class<? extends IFormHandlerType> c : classSet) {
-                        try {
-                            for (IFormHandlerType type : c.getEnumConstants()) {
-                                JSONObject jsonObj = new JSONObject();
-                                jsonObj.put("handler", type.getHandler());
-                                jsonObj.put("name", type.getHandlerName());
-                                jsonObj.put("icon", type.getIcon());
-                                jsonObj.put("type", type.getType());
-                                jsonObj.put("isConditionable", type.isConditionable());
-                                jsonObj.put("isExtendable", type.isExtendable());
-                                jsonObj.put("isFilterable", type.isFilterable());
-                                jsonObj.put("isShowable", type.isShowable());
-                                jsonObj.put("isValueable", type.isValueable());
-                                jsonObj.put("module", type.getModule());
-                                handlerList.add(jsonObj);
-                            }
-                        } catch (Exception e) {
-
-                        }
-                    }
-                    isUninitialized = false;
-                }
-            }
+        JSONArray jsonArray = new JSONArray();
+        for(IFormAttributeHandler handler : FormAttributeHandlerFactory.getHandlerList()){
+            JSONObject json = new JSONObject();
+            json.put("handler", handler.getHandler());
+            json.put("name", handler.getHandlerName());
+            json.put("icon", handler.getIcon());
+            json.put("type", handler.getType());
+            json.put("isConditionable", handler.isConditionable());
+            json.put("isExtendable", handler.isExtendable());
+            json.put("isFilterable", handler.isFilterable());
+            json.put("isShowable", handler.isShowable());
+            json.put("isValueable", handler.isValueable());
+            json.put("module", handler.getModule());
+            jsonArray.add(json);
         }
+        return jsonArray;
     }
-
 }
