1de1a90767b9eee26355310ecf5e8c635735594b
longrf <longrf@techsure.com.cn>
2021-10-25 18:41:29 +0800

    --story=1005084 --user=龙儒锋 Tagent管理功能迁移 https://www.tapd.cn/54247054/s/1182920

--- src/main/java/codedriver/module/tenant/api/file/FileDeleteApi.java
+++ src/main/java/codedriver/module/tenant/api/file/FileDeleteApi.java
@@ -7,7 +7,6 @@ package codedriver.module.tenant.api.file;
 
 import codedriver.framework.asynchronization.threadlocal.TenantContext;
 import codedriver.framework.asynchronization.threadlocal.UserContext;
-import codedriver.framework.auth.core.AuthAction;
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.common.util.FileUtil;
 import codedriver.framework.exception.file.FileAccessDeniedException;
@@ -18,13 +17,12 @@ import codedriver.framework.file.core.FileTypeHandlerFactory;
 import codedriver.framework.file.core.IFileTypeHandler;
 import codedriver.framework.file.dao.mapper.FileMapper;
 import codedriver.framework.file.dto.FileVo;
-import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.Input;
 import codedriver.framework.restful.annotation.OperationType;
 import codedriver.framework.restful.annotation.Param;
+import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
-
 import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -71,6 +69,7 @@ public class FileDeleteApi extends PrivateApiComponentBase {
             IFileTypeHandler fileTypeHandler = FileTypeHandlerFactory.getHandler(fileVo.getType());
             if (fileTypeHandler != null) {
                 if (fileTypeHandler.valid(UserContext.get().getUserUuid(), fileVo, paramObj)) {
+                    fileTypeHandler.myDelete(paramObj);
                     fileMapper.deleteFile(fileVo.getId());
                     FileUtil.deleteData(fileVo.getPath());
                 } else {
