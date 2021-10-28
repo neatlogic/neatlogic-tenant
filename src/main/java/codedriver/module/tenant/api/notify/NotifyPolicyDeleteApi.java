a4116da2e14d1f4e1e3fb56526b7f14979b770e6
赖文韬 <laiwt@techsure.com.cn>
2021-10-28 17:39:52 +0800

    --story=1005403 --user=赖文韬 苏州银行-发送和解析邮件流传异常时 实现异常信息管理员邮箱的发送配置 https://www.tapd.cn/54247054/s/1185344

--- src/main/java/codedriver/module/tenant/api/notify/NotifyPolicyDeleteApi.java
+++ src/main/java/codedriver/module/tenant/api/notify/NotifyPolicyDeleteApi.java
@@ -13,7 +13,6 @@ import codedriver.framework.restful.annotation.*;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 
 import codedriver.framework.auth.label.NOTIFY_POLICY_MODIFY;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,44 +21,47 @@ import com.alibaba.fastjson.JSONObject;
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.notify.dao.mapper.NotifyMapper;
 import codedriver.framework.notify.exception.NotifyPolicyReferencedCannotBeDeletedException;
+
+import javax.annotation.Resource;
+
 @Service
 @Transactional
 @AuthAction(action = NOTIFY_POLICY_MODIFY.class)
 @OperationType(type = OperationTypeEnum.DELETE)
-public class NotifyPolicyDeleteApi  extends PrivateApiComponentBase {
-	
-	@Autowired
-	private NotifyMapper notifyMapper;
+public class NotifyPolicyDeleteApi extends PrivateApiComponentBase {
 
-	@Override
-	public String getToken() {
-		return "notify/policy/delete";
-	}
+    @Resource
+    private NotifyMapper notifyMapper;
 
-	@Override
-	public String getName() {
-		return "通知策略删除接口";
-	}
+    @Override
+    public String getToken() {
+        return "notify/policy/delete";
+    }
 
-	@Override
-	public String getConfig() {
-		return null;
-	}
+    @Override
+    public String getName() {
+        return "通知策略删除接口";
+    }
 
-	@Input({
-		@Param(name = "id", type = ApiParamType.LONG, isRequired = true, desc = "策略id")
-	})
-	@Output({})
-	@Description(desc = "通知策略删除接口")
-	@Override
-	public Object myDoService(JSONObject jsonObj) throws Exception {
-		Long id = jsonObj.getLong("id");
-		int count = DependencyManager.getDependencyCount(CalleeType.NOTIFY_POLICY, id);
-		if(count > 0) {
-			throw new NotifyPolicyReferencedCannotBeDeletedException(id.toString());
-		}
-		notifyMapper.deleteNotifyPolicyById(id);
-		return null;
-	}
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Input({
+            @Param(name = "id", type = ApiParamType.LONG, isRequired = true, desc = "策略id")
+    })
+    @Output({})
+    @Description(desc = "通知策略删除接口")
+    @Override
+    public Object myDoService(JSONObject jsonObj) throws Exception {
+        Long id = jsonObj.getLong("id");
+        int count = DependencyManager.getDependencyCount(CalleeType.NOTIFY_POLICY, id);
+        if (count > 0) {
+            throw new NotifyPolicyReferencedCannotBeDeletedException(id.toString());
+        }
+        notifyMapper.deleteNotifyPolicyById(id);
+        return null;
+    }
 
 }
