e3dd870b1167954958b4e6250849893ce6a4c2a7
陈其炜 <chenqw@techsure.com.cn>
2022-01-27 10:13:26 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/module/tenant/api/test/TestApi.java
+++ src/main/java/codedriver/module/tenant/api/test/TestApi.java
@@ -1,22 +1,25 @@
 /*
- * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
  * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  */
 
 package codedriver.module.tenant.api.test;
 
-import codedriver.framework.auth.core.AuthAction;
+import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.restful.annotation.Description;
+import codedriver.framework.restful.annotation.Input;
+import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.annotation.ResubmitInterval;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import codedriver.framework.transaction.core.AfterTransactionJob;
 import codedriver.module.tenant.dao.mapper.TestMapper;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Deprecated
-
 @Component
+//@Transactional
 public class TestApi extends PrivateApiComponentBase {
 
     @Autowired
@@ -34,10 +37,18 @@ public class TestApi extends PrivateApiComponentBase {
 
     @Description(desc = "测试BG")
     @ResubmitInterval(value = 10)
+    @Input({@Param(name = "content", type = ApiParamType.STRING, isRequired = true)})
     @Override
     public Object myDoService(JSONObject jsonObj) throws Exception {
-        String str = jsonObj.getString("content");
-        return testMapper.getContent(str);
+        String content = jsonObj.getString("content");
+        AfterTransactionJob<String> job = new AfterTransactionJob<>("TEST");
+        job.execute(content, c -> {
+            System.out.println(testMapper.getContent());
+        });
+        testMapper.insertContent(content);
+        System.out.println("done");
+        Thread.sleep(20000L);
+        return null;
     }
 
     @Override
