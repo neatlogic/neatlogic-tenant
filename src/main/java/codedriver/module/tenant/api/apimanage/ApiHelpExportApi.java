c88dab53cc9ac2f526f55df4e389b0157d417441
lvzk <lvzk@techsure.com.cn>
2022-04-27 18:44:40 +0800

    导出接口帮助

--- src/main/java/codedriver/module/tenant/api/apimanage/ApiHelpExportApi.java
+++ src/main/java/codedriver/module/tenant/api/apimanage/ApiHelpExportApi.java
@@ -21,7 +21,9 @@ import codedriver.framework.restful.dto.ApiVo;
 import codedriver.framework.restful.enums.ApiType;
 import codedriver.framework.util.ExportUtil;
 import codedriver.framework.util.FreemarkerUtil;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,84 +36,121 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 import java.util.Objects;
 
 @Service
 @AuthAction(action = INTERFACE_MODIFY.class)
 @OperationType(type = OperationTypeEnum.SEARCH)
 public class ApiHelpExportApi extends PrivateBinaryStreamApiComponentBase {
-	static String template;
-	static Logger logger = LoggerFactory.getLogger(ApiHelpExportApi.class);
+    static String template;
+    static Logger logger = LoggerFactory.getLogger(ApiHelpExportApi.class);
 
-	@Autowired
-	private ApiMapper apiMapper;
-	
-	@Override
-	public String getToken() {
-		return "api/help/export";
-	}
+    @Autowired
+    private ApiMapper apiMapper;
 
-	@Override
-	public String getName() {
-		return "导出接口帮助";
-	}
+    @Override
+    public String getToken() {
+        return "api/help/export";
+    }
 
-	@Override
-	public String getConfig() {
-		return null;
-	}
+    @Override
+    public String getName() {
+        return "导出接口帮助";
+    }
 
-	@Input({
-		@Param(name = "token", type = ApiParamType.STRING, desc = "接口token")
-	})
-	@Output({
-		@Param(name = "Return", explode = ApiVo.class, isRequired = true, desc = "接口配置信息")
-	})
-	@Description(desc = "导出接口帮助接口")
-	@Override
-	public Object myDoService(JSONObject jsonObj, HttpServletRequest request, HttpServletResponse response) throws Exception {
-		JSONObject helpJson = new JSONObject();
-		try {
-			InputStreamReader reader = new InputStreamReader(Objects.requireNonNull(ApiHelpExportApi.class.getClassLoader()
-					.getResourceAsStream("template/api-help-template.ftl")), StandardCharsets.UTF_8.name());
-			template = IOUtils.toString(reader);
-		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
-		}
-		String token = jsonObj.getString("token");
-		ApiVo interfaceVo = PrivateApiComponentFactory.getApiByToken(token);
-		if (interfaceVo == null) {
-			interfaceVo = apiMapper.getApiByToken(token);
-			if (interfaceVo == null || !interfaceVo.getIsActive().equals(1)) {
-				throw new ApiNotFoundException(token);
-			}
-		}
-		if (interfaceVo.getType().equals(ApiType.OBJECT.getValue())) {
-			IApiComponent restComponent = PrivateApiComponentFactory.getInstance(interfaceVo.getHandler());
-			helpJson = restComponent.help();
-		}else if (interfaceVo.getType().equals(ApiType.STREAM.getValue())) {
-			IJsonStreamApiComponent restComponent = PrivateApiComponentFactory.getStreamInstance(interfaceVo.getHandler());
-			helpJson = restComponent.help();
-		}else if (interfaceVo.getType().equals(ApiType.BINARY.getValue())) {
-			IBinaryStreamApiComponent restComponent = PrivateApiComponentFactory.getBinaryInstance(interfaceVo.getHandler());
-			helpJson = restComponent.help();
-		}
-		String content = FreemarkerUtil.transform(helpJson, template);
-		try (OutputStream os = response.getOutputStream()) {
-			response.setCharacterEncoding("utf-8");
-			response.setContentType("application/pdf");
-			response.setHeader("Content-Disposition",
-					" attachment; filename=\"" + URLEncoder.encode("werw", StandardCharsets.UTF_8.name()) + ".pdf\"");
-			ExportUtil.savePdf(content, os, false);
-			os.flush();
-		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
-		}
-		return null;
-	}
+    @Override
+    public String getConfig() {
+        return null;
+    }
 
-	static {
+    @Input({
+            @Param(name = "tokenList", type = ApiParamType.JSONARRAY, desc = "接口token 列表")
+    })
+    @Output({
+            @Param(name = "Return", explode = ApiVo.class, isRequired = true, desc = "接口配置信息")
+    })
+    @Description(desc = "导出接口帮助接口")
+    @Override
+    public Object myDoService(JSONObject jsonObj, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        try {
+            InputStreamReader reader = new InputStreamReader(Objects.requireNonNull(ApiHelpExportApi.class.getClassLoader()
+                    .getResourceAsStream("template/api-help-template.ftl")), StandardCharsets.UTF_8.name());
+            template = IOUtils.toString(reader);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
 
-	}
+        JSONArray tokenArray = jsonObj.getJSONArray("tokenList");
+        JSONArray apiHelpJsonArray = new JSONArray();
+        if (CollectionUtils.isNotEmpty(tokenArray)) {
+            for (int t = 0; t < tokenArray.size(); t++) {
+                String token = tokenArray.getString(t);
+                ApiVo interfaceVo = PrivateApiComponentFactory.getApiByToken(token);
+                if (interfaceVo == null) {
+                    interfaceVo = apiMapper.getApiByToken(token);
+                    if (interfaceVo == null || !interfaceVo.getIsActive().equals(1)) {
+                        throw new ApiNotFoundException(token);
+                    }
+                }
+                if (interfaceVo.getType().equals(ApiType.OBJECT.getValue())) {
+                    IApiComponent restComponent = PrivateApiComponentFactory.getInstance(interfaceVo.getHandler());
+                    JSONObject helpJson = restComponent.help();
+                    helpJson.put("token",interfaceVo.getToken());
+                    helpJson.put("name",interfaceVo.getName());
+                    apiHelpJsonArray.add(helpJson);
+                } else if (interfaceVo.getType().equals(ApiType.STREAM.getValue())) {
+                    IJsonStreamApiComponent restComponent = PrivateApiComponentFactory.getStreamInstance(interfaceVo.getHandler());
+                    JSONObject helpJson = restComponent.help();
+                    helpJson.put("token",interfaceVo.getToken());
+                    helpJson.put("name",interfaceVo.getName());
+                    apiHelpJsonArray.add(helpJson);
+                } else if (interfaceVo.getType().equals(ApiType.BINARY.getValue())) {
+                    IBinaryStreamApiComponent restComponent = PrivateApiComponentFactory.getBinaryInstance(interfaceVo.getHandler());
+                    JSONObject helpJson = restComponent.help();
+                    helpJson.put("token",interfaceVo.getToken());
+                    helpJson.put("name",interfaceVo.getName());
+                    apiHelpJsonArray.add(helpJson);
+                }
+            }
+        } else {
+            for (Map.Entry<String,IApiComponent> entry : PrivateApiComponentFactory.getComponentMap().entrySet()) {
+                JSONObject helpJson = entry.getValue().help();
+                helpJson.put("token",entry.getKey());
+                helpJson.put("name",entry.getValue().getName());
+                apiHelpJsonArray.add(helpJson);
+            }
+            for (Map.Entry<String,IBinaryStreamApiComponent> entry : PrivateApiComponentFactory.getBinaryStreamComponentMap().entrySet()) {
+                JSONObject helpJson = entry.getValue().help();
+                helpJson.put("token",entry.getKey());
+                helpJson.put("name",entry.getValue().getName());
+                apiHelpJsonArray.add(helpJson);
+            }
+            for (Map.Entry<String,IJsonStreamApiComponent> entry : PrivateApiComponentFactory.getJsonStreamComponentMap().entrySet()) {
+                JSONObject helpJson = entry.getValue().help();
+                helpJson.put("token",entry.getKey());
+                helpJson.put("name",entry.getValue().getName());
+                apiHelpJsonArray.add(helpJson);
+            }
+        }
+
+        String content = FreemarkerUtil.transform(apiHelpJsonArray, template);
+        //TODO 先用html在浏览器导出pdf ， 代码导出失败
+        try (OutputStream os = response.getOutputStream()) {
+            response.setCharacterEncoding("utf-8");
+            response.setContentType("application/pdf");
+            response.setHeader("Content-Disposition",
+                    " attachment; filename=\"" + URLEncoder.encode("werw", StandardCharsets.UTF_8.name()) + ".pdf\"");
+            ExportUtil.savePdf(content, os, false);
+            os.flush();
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+        return null;
+    }
+
+    static {
+
+    }
 
 }
--- src/main/resources/template/api-help-template.ftl
+++ src/main/resources/template/api-help-template.ftl
@@ -6,6 +6,9 @@
         body {
             font-family: "SimSun";
         }
+        p{
+            font-weight: bold;
+        }
         .innerTable {
             width: 100%;
             text-align: center;
@@ -25,59 +28,70 @@
     </style>
 </head>
 
-<body style = "font-family: SimSun;">
-<p>${DATA.description}</p>
-<#--正文-->
-
-<#--输入参数-->
-<#if DATA.input?? && DATA.input?size gt 0>
-    <p>输入参数</p>
-    <table class="innerTable">
-        <thead>
-        <tr>
-            <th>名称</th>
-            <th>类型</th>
-            <th>是否必填</th>
-            <th>说明</th>
-        </tr>
-        </thead>
-        <tbody>
-        <#list DATA.input as value>
-            <tr>
-                <td>${value["name"]}</td>
-                <td>${value["type"]}</td>
-                <td>${value["isRequired"]}</td>
-                <td>${value["description"]}</td>
-            </tr>
-        </#list>
-        </tbody>
-    </table>
+<body style="font-family: SimSun;">
+<#if DATA?? && DATA?size gt 0>
+    <#list DATA as api>
+        <p>接口token</p>
+        <span>${api.token}</span>
+        <p>接口名称</p>
+        <span>${api.name}</span>
+        <p>描述</p>
+        <span>${api.description}</span>
+    <#--输入参数-->
+        <#if api.input?? && api.input?size gt 0>
+            <p>输入参数</p>
+            <table class="innerTable">
+                <thead>
+                <tr>
+                    <th>名称</th>
+                    <th>类型</th>
+                    <th>是否必填</th>
+                    <th>说明</th>
+                </tr>
+                </thead>
+                <tbody>
+                <#list api.input as value>
+                    <tr>
+                        <td>${value["name"]}</td>
+                        <td>${value["type"]}</td>
+                        <td>${value["isRequired"]}</td>
+                        <td>${value["description"]}</td>
+                    </tr>
+                </#list>
+                </tbody>
+            </table>
+        </#if>
+    <#--输出参数-->
+        <#if api.output?? && api.output?size gt 0>
+            <p>输出参数</p>
+            <table class="innerTable">
+                <thead>
+                <tr>
+                    <th>名称</th>
+                    <th>类型</th>
+                    <th>是否必填</th>
+                    <th>说明</th>
+                </tr>
+                </thead>
+                <tbody>
+                <#list api.output as value>
+                    <tr>
+                        <td>${value["name"]}</td>
+                        <td>${value["type"]}</td>
+                        <td>${value["isRequired"]}</td>
+                        <td>${value["description"]}</td>
+                    </tr>
+                </#list>
+                </tbody>
+            </table>
+        </#if>
+        <p></p>
+        <p></p>
+        <p>----------------------------------------------------------------------------------------------------------------------------</p>
+        <p></p>
+        <p></p>
+    </#list>
 </#if>
-
-<#if DATA.output?? && DATA.output?size gt 0>
-    <p>输出参数</p>
-    <table class="innerTable">
-        <thead>
-        <tr>
-            <th>名称</th>
-            <th>类型</th>
-            <th>是否必填</th>
-            <th>说明</th>
-        </tr>
-        </thead>
-        <tbody>
-        <#list DATA.output as value>
-            <tr>
-                <td>${value["name"]}</td>
-                <td>${value["type"]}</td>
-                <td>${value["isRequired"]}</td>
-                <td>${value["description"]}</td>
-            </tr>
-        </#list>
-        </tbody>
-    </table>
-</#if>
-
 
 </body>
 </html>
