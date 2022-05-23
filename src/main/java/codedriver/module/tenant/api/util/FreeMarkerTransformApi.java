85c6db1e125b071ea9592469f219b2c606d7ee2c
陈其炜 <chenqw@techsure.com.cn>
2022-05-23 15:38:16 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/module/tenant/api/util/FreeMarkerTransformApi.java
+++ src/main/java/codedriver/module/tenant/api/util/FreeMarkerTransformApi.java
@@ -1,12 +1,11 @@
 /*
- * Copyright(c) 2021 TechSureCo.,Ltd.AllRightsReserved.
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
  * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  */
 
 package codedriver.module.tenant.api.util;
 
 import codedriver.framework.common.constvalue.ApiParamType;
-import codedriver.framework.exception.util.FreemarkerTransformException;
 import codedriver.framework.restful.annotation.Input;
 import codedriver.framework.restful.annotation.OperationType;
 import codedriver.framework.restful.annotation.Output;
@@ -14,7 +13,6 @@ import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import com.alibaba.fastjson.JSONObject;
-import com.google.protobuf.Api;
 import freemarker.cache.StringTemplateLoader;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
