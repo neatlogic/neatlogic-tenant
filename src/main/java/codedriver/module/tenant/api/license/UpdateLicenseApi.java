34e22e508774b163c2864e385967cb9854133d03
lvzk <lvzk@techsure.com.cn>
2022-06-10 14:45:06 +0800

    add update license

---
+++ src/main/java/codedriver/module/tenant/api/license/UpdateLicenseApi.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c)  2021 TechSure Co.,Ltd.  All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.module.tenant.api.license;
+
+import codedriver.framework.asynchronization.threadlocal.TenantContext;
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.dao.mapper.LicenseMapper;
+import codedriver.framework.dao.mapper.TenantMapper;
+import codedriver.framework.dto.TenantVo;
+import codedriver.framework.license.LicenseManager;
+import codedriver.framework.restful.annotation.*;
+import codedriver.framework.restful.constvalue.OperationTypeEnum;
+import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+@OperationType(type = OperationTypeEnum.OPERATE)
+public class UpdateLicenseApi extends PrivateApiComponentBase {
+
+    @Resource
+    LicenseMapper licenseMapper;
+
+    @Resource
+    TenantMapper tenantMapper;
+
+    @Override
+    public String getName() {
+        return "跟新license";
+    }
+
+    @Override
+    public String getToken() {
+        return "license/update";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Input({
+            @Param(name = "license", type = ApiParamType.STRING, desc = "license 串")
+    })
+    @Output({
+    })
+    @Description(desc = "跟新license接口,不存在则会insert")
+    @Override
+    public Object myDoService(JSONObject paramObj) throws Exception {
+        String licenseStr = paramObj.getString("license");
+        String tenantUuid = TenantContext.get().getTenantUuid();
+        TenantContext.get().setUseDefaultDatasource(true);
+        TenantVo tenantVo = tenantMapper.getTenantByUuid(tenantUuid);
+        if (StringUtils.isNotBlank(licenseStr)) {
+            licenseMapper.insertLicenseByTenantUuid(tenantVo.getId(), tenantVo.getUuid(), licenseStr);
+        } else {
+            licenseStr = licenseMapper.getTenantLicenseByTenantUuid(TenantContext.get().getTenantUuid());
+        }
+        LicenseManager.getLicenseVo(TenantContext.get().getTenantUuid(), licenseStr);
+        TenantContext.get().setUseDefaultDatasource(false);
+        return null;
+    }
+
+}
