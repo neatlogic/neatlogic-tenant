f289ea903edcdba4510a48d5310886d639139aad
陈其炜 <chenqw@techsure.com.cn>
2022-06-21 17:02:43 +0800

    优化数据仓库管理工能
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/module/tenant/api/healthcheck/RebuildTableApi.java
+++ src/main/java/codedriver/module/tenant/api/healthcheck/RebuildTableApi.java
@@ -9,13 +9,13 @@ import codedriver.framework.auth.core.AuthAction;
 import codedriver.framework.auth.label.ADMIN;
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.dto.healthcheck.DatabaseFragmentVo;
+import codedriver.framework.healthcheck.dao.mapper.DatabaseFragmentMapper;
 import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.Input;
 import codedriver.framework.restful.annotation.OperationType;
 import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
-import codedriver.module.tenant.dao.mapper.healthcheck.DatabaseFragmentMapper;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Service;
 
