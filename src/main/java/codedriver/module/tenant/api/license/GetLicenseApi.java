9fb2e640d7a5cdd1d0fc57d7d913f1da8d1d42f8
lvzk <lvzk@techsure.com.cn>
2022-06-24 19:44:08 +0800

    update license

--- src/main/java/codedriver/module/tenant/api/license/GetLicenseApi.java
+++ src/main/java/codedriver/module/tenant/api/license/GetLicenseApi.java
@@ -68,62 +68,68 @@ public class GetLicenseApi extends PrivateApiComponentBase {
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
         JSONObject result = new JSONObject();
-        Map<String,String> moduleGroupMap = ModuleUtil.getAllModuleGroupList().stream().collect(Collectors.toMap(o->o.getGroup().toUpperCase(Locale.ROOT), ModuleGroupVo::getGroupName));;
+        Map<String, String> moduleGroupMap = ModuleUtil.getAllModuleGroupList().stream().collect(Collectors.toMap(o -> o.getGroup().toUpperCase(Locale.ROOT), ModuleGroupVo::getGroupName));
+        ;
         LicenseVo licenseVo = TenantContext.get().getLicenseVo();
         String tenant = TenantContext.get().getTenantUuid();
         TenantContext.get().setUseDefaultDatasource(true);
         TenantVo tenantVo = tenantMapper.getTenantByUuid(tenant);
-        result.put("purchaser",licenseVo.getPurchaser());
-        result.put("tenantUuid",tenant);
-        result.put("tenant",tenantVo.getName());
+        result.put("purchaser", licenseVo.getPurchaser());
+        result.put("tenantUuid", tenant);
+        result.put("tenant", tenantVo.getName());
         result.put("issueTime", TimeUtil.convertDateToString(licenseVo.getIssueTime(), TimeUtil.YYYY_MM_DD));
         result.put("expiredTime", TimeUtil.convertDateToString(licenseVo.getExpireTime(), TimeUtil.YYYY_MM_DD));
         result.put("stopServiceTime", TimeUtil.addDateByDay(licenseVo.getExpireTime(), licenseVo.getExpiredDay(), TimeUtil.YYYY_MM_DD));
         LicenseAuthVo licenseAuthVo = licenseVo.getAuth();
         List<LicenseAuthModuleGroupVo> licenseAuthModuleGroupVoList = licenseAuthVo.getModuleGroupList();
         JSONArray moduleGroupArray = new JSONArray();
-        result.put("moduleGroupList",moduleGroupArray);
+        result.put("moduleGroupList", moduleGroupArray);
         Optional<LicenseAuthModuleGroupVo> allModuleOptional = licenseAuthModuleGroupVoList.stream().filter(o -> Objects.equals("ALL", o.getName().toUpperCase(Locale.ROOT))).findFirst();
         if (allModuleOptional.isPresent()) {
             JSONObject moduleGroupJson = new JSONObject();
-            moduleGroupJson.put("moduleGroup","ALL");
-            moduleGroupJson.put("moduleGroupName","所有");
+            moduleGroupJson.put("moduleGroup", "ALL");
+            moduleGroupJson.put("moduleGroupName", "所有");
             moduleGroupArray.add(moduleGroupJson);
-            initAuth( new ArrayList<>(), moduleGroupJson,true);
+            initAuth(new ArrayList<>(), moduleGroupJson, true);
             List<String> operationList = allModuleOptional.get().getOperationTypeList();
-            initOperation( operationList, moduleGroupJson);
-        }else{
-            for(LicenseAuthModuleGroupVo licenseAuthModuleGroupVo : licenseAuthModuleGroupVoList){
+            initOperation(operationList, moduleGroupJson, true);
+        } else {
+            for (LicenseAuthModuleGroupVo licenseAuthModuleGroupVo : licenseAuthModuleGroupVoList) {
                 JSONObject moduleGroupJson = new JSONObject();
-                moduleGroupJson.put("moduleGroup",licenseAuthModuleGroupVo.getName());
-                moduleGroupJson.put("moduleGroupName",moduleGroupMap.get(licenseAuthModuleGroupVo.getName().toUpperCase(Locale.ROOT)));
-                initAuth( licenseAuthModuleGroupVo.getAuthList(), moduleGroupJson,false);
+                moduleGroupJson.put("moduleGroup", licenseAuthModuleGroupVo.getName());
+                moduleGroupJson.put("moduleGroupName", moduleGroupMap.get(licenseAuthModuleGroupVo.getName().toUpperCase(Locale.ROOT)));
+                initAuth(licenseAuthModuleGroupVo.getAuthList(), moduleGroupJson, false);
                 List<String> operationList = licenseAuthModuleGroupVo.getOperationTypeList();
-                initOperation( operationList, moduleGroupJson);
+                initOperation(operationList, moduleGroupJson, false);
                 moduleGroupArray.add(moduleGroupJson);
             }
         }
-        result.put("license",licenseMapper.getTenantLicenseByTenantUuid(tenant));
+        result.put("license", licenseMapper.getTenantLicenseByTenantUuid(tenant));
         TenantContext.get().setUseDefaultDatasource(false);
         return result;
     }
 
     /**
      * 初始化操作
-     * @param operationList 操作列表
+     *
+     * @param operationList   操作列表
      * @param moduleGroupJson 模块组
      */
-    private void initOperation( List<String> operationList,JSONObject moduleGroupJson){
+    private void initOperation(List<String> operationList, JSONObject moduleGroupJson, boolean isAllModule) {
         Optional<String> allOperationOptional = operationList.stream().filter(o -> Objects.equals("ALL", o.toUpperCase(Locale.ROOT))).findFirst();
         JSONArray operationArray = new JSONArray();
         moduleGroupJson.put("operationList", operationArray);
-        if(allOperationOptional.isPresent()){
+        if (allOperationOptional.isPresent()) {
             JSONObject operationJson = new JSONObject();
             operationJson.put("name", "ALL");
             operationJson.put("displayName", "所有");
-            operationJson.put("desc", "拥有此模块所有操作");
+            if (isAllModule) {
+                operationJson.put("desc", "拥有所有模块所有操作");
+            } else {
+                operationJson.put("desc", "拥有此模块所有操作");
+            }
             operationArray.add(operationJson);
-        }else {
+        } else {
             for (String operation : operationList) {
                 JSONObject operationJson = new JSONObject();
                 operationJson.put("name", operation);
@@ -135,24 +141,29 @@ public class GetLicenseApi extends PrivateApiComponentBase {
 
     /**
      * 初始化操作
-     * @param authList 权限列表
+     *
+     * @param authList        权限列表
      * @param moduleGroupJson 模块组
      */
-    private void initAuth( List<String> authList,JSONObject moduleGroupJson, boolean isAllModule){
+    private void initAuth(List<String> authList, JSONObject moduleGroupJson, boolean isAllModule) {
         JSONArray authArray = new JSONArray();
-        moduleGroupJson.put("authList",authArray);
-        if(isAllModule || authList.stream().anyMatch(o -> Objects.equals("ALL", o.toUpperCase(Locale.ROOT)))){
+        moduleGroupJson.put("authList", authArray);
+        if (isAllModule || authList.stream().anyMatch(o -> Objects.equals("ALL", o.toUpperCase(Locale.ROOT)))) {
             JSONObject authJson = new JSONObject();
             authJson.put("name", "ALL");
             authJson.put("displayName", "所有");
-            authJson.put("desc", "拥有此模块所有权限");
+            if (isAllModule) {
+                authJson.put("desc", "拥有所有权限");
+            } else {
+                authJson.put("desc", "拥有此模块所有权限");
+            }
             authArray.add(authJson);
-        }else {
+        } else {
             for (String auth : authList) {
                 JSONObject authJson = new JSONObject();
                 AuthBase authBase = AuthFactory.getAuthInstance(auth.toUpperCase(Locale.ROOT));
                 authJson.put("name", auth.toUpperCase(Locale.ROOT));
-                if(authBase != null) {
+                if (authBase != null) {
                     authJson.put("displayName", authBase.getAuthDisplayName());
                     authJson.put("desc", authBase.getAuthIntroduction());
                 }
