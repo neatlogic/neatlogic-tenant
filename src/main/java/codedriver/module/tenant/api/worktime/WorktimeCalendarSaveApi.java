7bf801160c2626eaaf2865d3a315abe8d9c994d4
linbq@techsure.com.cn <Besiter789>
2022-06-29 17:51:07 +0800

    --bug=1009816 --user=林邦泉 工单中心剩余时间异常 https://www.tapd.cn/54247054/s/1238462

--- src/main/java/codedriver/module/tenant/api/worktime/WorktimeCalendarSaveApi.java
+++ src/main/java/codedriver/module/tenant/api/worktime/WorktimeCalendarSaveApi.java
@@ -15,22 +15,15 @@ import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import codedriver.framework.worktime.dao.mapper.WorktimeMapper;
-import codedriver.framework.worktime.dto.WorktimeConfigVo;
-import codedriver.framework.worktime.dto.WorktimeRangeVo;
 import codedriver.framework.worktime.dto.WorktimeVo;
 import codedriver.framework.worktime.exception.WorktimeNotFoundException;
 import codedriver.module.tenant.service.WorktimeService;
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
