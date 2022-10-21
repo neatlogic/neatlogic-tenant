6c77520d25954db5cadfab92fec7b334f70a6002
linbq@techsure.com.cn <Besiter789>
2022-10-21 10:57:40 +0800

    --story=1007043 --user=林邦泉 接口 https://www.tapd.cn/54247054/s/1257808

---
+++ src/main/java/codedriver/module/tenant/api/matrix/MatrixColumnDataSearchForSelectApi.java
@@ -0,0 +1,176 @@
+/*
+ * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.module.tenant.api.matrix;
+
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.common.dto.BasePageVo;
+import codedriver.framework.common.dto.ValueTextVo;
+import codedriver.framework.matrix.core.IMatrixDataSourceHandler;
+import codedriver.framework.matrix.core.MatrixDataSourceHandlerFactory;
+import codedriver.framework.matrix.core.MatrixPrivateDataSourceHandlerFactory;
+import codedriver.framework.matrix.dao.mapper.MatrixMapper;
+import codedriver.framework.matrix.dto.MatrixColumnVo;
+import codedriver.framework.matrix.dto.MatrixDataVo;
+import codedriver.framework.matrix.dto.MatrixVo;
+import codedriver.framework.matrix.exception.MatrixDataSourceHandlerNotFoundException;
+import codedriver.framework.matrix.exception.MatrixNotFoundException;
+import codedriver.framework.restful.annotation.*;
+import codedriver.framework.restful.constvalue.OperationTypeEnum;
+import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+@Service
+
+@OperationType(type = OperationTypeEnum.SEARCH)
+public class MatrixColumnDataSearchForSelectApi extends PrivateApiComponentBase {
+
+    @Resource
+    private MatrixMapper matrixMapper;
+
+    @Override
+    public String getToken() {
+        return "matrix/column/data/search/forselect";
+    }
+
+    @Override
+    public String getName() {
+        return "矩阵属性数据查询-下拉接口";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean disableReturnCircularReferenceDetect() {
+       return true;
+    }
+
+    @Input({
+            @Param(name = "keyword", desc = "关键字", type = ApiParamType.STRING, xss = true),
+            @Param(name = "matrixUuid", desc = "矩阵Uuid", type = ApiParamType.STRING, isRequired = true),
+            @Param(name = "keywordColumn", desc = "关键字属性uuid", type = ApiParamType.STRING),
+            @Param(name = "value", desc = "value属性uuid", type = ApiParamType.STRING, isRequired = true),
+            @Param(name = "text", desc = "text属性uuid", type = ApiParamType.STRING, isRequired = true),
+            @Param(name = "sourceColumnList", desc = "搜索过滤值集合", type = ApiParamType.JSONARRAY),
+            @Param(name = "pageSize", desc = "显示条目数", type = ApiParamType.INTEGER),
+            @Param(name = "defaultValue", desc = "精确匹配回显数据参数", type = ApiParamType.JSONARRAY),
+            @Param(name = "attrFilterList", desc = "配置项矩阵属性过滤条件", type = ApiParamType.JSONARRAY),
+            @Param(name = "relFilterList", desc = "配置项矩阵关系过滤条件", type = ApiParamType.JSONARRAY),
+            @Param(name = "filterList", desc = "联动过滤数据集合", type = ApiParamType.JSONARRAY)
+    })
+    @Output({
+            @Param(name = "dataList", type = ApiParamType.JSONARRAY, desc = "属性数据集合"),
+            @Param(explode = BasePageVo.class)
+    })
+    @Description(desc = "矩阵属性数据查询-下拉级联接口")
+    @Example(example = "" +
+            "{" +
+            "\"matrixUuid(矩阵uuid，必填)\": \"825e6ba09050406eb0de8c4bdcd4e27c\"," +
+            "\"columnList(需要返回数据的字段列表，必填)\": [" +
+            "\"92196814d8da4ad9bed63e1d650d7e98\"," +
+            "\"a4e9978fd06d46d78b13f947a2b1b188\"," +
+            "\"cf2c2677c18540a79e60cfd9d531b50c\"," +
+            "\"b5d685e9e5fb4ce0baa0604de812e93b\"," +
+            "\"6e4abb9b532b49139cec798a3828c7cd\"," +
+            "\"3d2f5475138744938f0bb1da1f82002c\"," +
+            "\"96d449a58b664a31b32bb1c28090aeee\"" +
+            "]," +
+            "\"searchColumnList(可搜索字段列表，选填)\": [" +
+            "\"92196814d8da4ad9bed63e1d650d7e98\"," +
+            "\"a4e9978fd06d46d78b13f947a2b1b188\"" +
+            "\t]," +
+            "\"currentPage\": 1," +
+            "\"pageSize\": 10," +
+            "\"sourceColumnList(过滤条件列表，选填)\": [" +
+            "{" +
+            "\"column(字段uuid，必填)\": \"a4e9978fd06d46d78b13f947a2b1b188\"," +
+            "\"expression(过滤表达式，必填)\": \"like|notlike|equal|unequal|include|exclude|between|greater-than|less-than|is-null|match|is-not-null\"," +
+            "\"valueList(过滤值列表，必填)\": [" +
+            "\"1\"" +
+            "]" +
+            "}" +
+            "]," +
+            "\"filterList(联动过滤条件列表，选填)\": [" +
+            "{" +
+            "\"uuid(字段uuid，必填)\": \"92196814d8da4ad9bed63e1d650d7e98\"," +
+            "\"valueList(过滤值列表，必填)\": [" +
+            "\"2\"" +
+            "]" +
+            "}" +
+            "]" +
+            "}")
+    @Override
+    public Object myDoService(JSONObject jsonObj) throws Exception {
+        jsonObj.remove("needPage");
+        MatrixDataVo dataVo = jsonObj.toJavaObject(MatrixDataVo.class);
+        MatrixVo matrixVo = MatrixPrivateDataSourceHandlerFactory.getMatrixVo(dataVo.getMatrixUuid());
+        if (matrixVo == null) {
+            matrixVo = matrixMapper.getMatrixByUuid(dataVo.getMatrixUuid());
+            if (matrixVo == null) {
+                throw new MatrixNotFoundException(dataVo.getMatrixUuid());
+            }
+        }
+        IMatrixDataSourceHandler matrixDataSourceHandler = MatrixDataSourceHandlerFactory.getHandler(matrixVo.getType());
+        if (matrixDataSourceHandler == null) {
+            throw new MatrixDataSourceHandlerNotFoundException(matrixVo.getType());
+        }
+        List<MatrixColumnVo> sourceColumnList = dataVo.getSourceColumnList();
+        if (CollectionUtils.isNotEmpty(sourceColumnList)) {
+            Iterator<MatrixColumnVo> iterator = sourceColumnList.iterator();
+            while (iterator.hasNext()) {
+                MatrixColumnVo matrixColumnVo = iterator.next();
+                if (StringUtils.isBlank(matrixColumnVo.getColumn())) {
+                    iterator.remove();
+                } else if (CollectionUtils.isEmpty(matrixColumnVo.getValueList())) {
+                    iterator.remove();
+                }
+            }
+        }
+
+        String value = jsonObj.getString("value");
+        String text = jsonObj.getString("text");
+        List<String> columnList = new ArrayList<>();
+        columnList.add(value);
+        columnList.add(text);
+        dataVo.setColumnList(columnList);
+        List<ValueTextVo> dataList = new ArrayList<>();
+        List<Map<String, JSONObject>> resultList = matrixDataSourceHandler.searchTableColumnData(dataVo);
+        if (CollectionUtils.isNotEmpty(resultList)) {
+            for (Map<String, JSONObject> result : resultList) {
+                String valueStr = null;
+                JSONObject valueObj = result.get(value);
+                if (MapUtils.isNotEmpty(valueObj)) {
+                    valueStr = valueObj.getString("value");
+                }
+                String textStr = null;
+                JSONObject textObj = result.get(text);
+                if (MapUtils.isNotEmpty(textObj)) {
+                    textStr = textObj.getString("value");
+                }
+                dataList.add(new ValueTextVo(valueStr, textStr));
+            }
+        }
+        JSONObject returnObj = new JSONObject();
+        returnObj.put("dataList", dataList);
+        returnObj.put("currentPage", dataVo.getCurrentPage());
+        returnObj.put("pageSize", dataVo.getPageSize());
+        returnObj.put("pageCount", dataVo.getPageCount());
+        returnObj.put("rowNum", dataVo.getRowNum());
+        return returnObj;
+    }
+}
