b77b3881f8279f0bc307ee9107721bd58a564f16
赖文韬 <laiwt@techsure.com.cn>
2022-11-03 10:10:21 +0800

    --story=1007090 --user=赖文韬 系统配置--》数据仓库添加导入导出功能 https://www.tapd.cn/54247054/s/1259785

---
+++ src/main/java/codedriver/module/tenant/api/datawarehouse/ImportDataSourceApi.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright(c) 2021. TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.module.tenant.api.datawarehouse;
+
+import codedriver.framework.auth.core.AuthAction;
+import codedriver.framework.auth.label.DATA_WAREHOUSE_MODIFY;
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.datawarehouse.dto.DataSourceVo;
+import codedriver.framework.exception.file.FileExtNotAllowedException;
+import codedriver.framework.exception.file.FileNotUploadException;
+import codedriver.framework.restful.annotation.*;
+import codedriver.framework.restful.constvalue.OperationTypeEnum;
+import codedriver.framework.restful.core.privateapi.PrivateBinaryStreamApiComponentBase;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.zip.ZipInputStream;
+
+@Service
+@Transactional
+@AuthAction(action = DATA_WAREHOUSE_MODIFY.class)
+@OperationType(type = OperationTypeEnum.OPERATE)
+public class ImportDataSourceApi extends PrivateBinaryStreamApiComponentBase {
+
+    @Override
+    public String getToken() {
+        return "datawarehouse/datasource/import";
+    }
+
+    @Override
+    public String getName() {
+        return "导入数据仓库数据源";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Input({
+    })
+    @Output({
+            @Param(name = "successCount", type = ApiParamType.INTEGER, desc = "导入成功数量"),
+            @Param(name = "failureCount", type = ApiParamType.INTEGER, desc = "导入失败数量"),
+            @Param(name = "failureReasonList", type = ApiParamType.JSONARRAY, desc = "失败原因")
+    })
+    @Description(desc = "导入数据仓库数据源")
+    @Override
+    public Object myDoService(JSONObject paramObj, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        JSONObject resultObj = new JSONObject();
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+        Map<String, MultipartFile> multipartFileMap = multipartRequest.getFileMap();
+        if (multipartFileMap.isEmpty()) {
+            throw new FileNotUploadException();
+        }
+        JSONArray resultList = new JSONArray();
+        byte[] buf = new byte[1024];
+        int successCount = 0;
+        int failureCount = 0;
+        for (Map.Entry<String, MultipartFile> entry : multipartFileMap.entrySet()) {
+            MultipartFile multipartFile = entry.getValue();
+            try (ZipInputStream zis = new ZipInputStream(multipartFile.getInputStream());
+                 ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+                while (zis.getNextEntry() != null) {
+                    int len;
+                    while ((len = zis.read(buf)) != -1) {
+                        out.write(buf, 0, len);
+                    }
+                    DataSourceVo dataSourceVo = JSONObject.parseObject(new String(out.toByteArray(), StandardCharsets.UTF_8), new TypeReference<DataSourceVo>() {
+                    });
+                    JSONObject result = save(dataSourceVo);
+                    if (MapUtils.isNotEmpty(result)) {
+                        resultList.add(result);
+                        failureCount++;
+                    } else {
+                        successCount++;
+                    }
+                    out.reset();
+                }
+            } catch (IOException e) {
+                throw new FileExtNotAllowedException(multipartFile.getOriginalFilename());
+            }
+        }
+        resultObj.put("successCount", successCount);
+        resultObj.put("failureCount", failureCount);
+        if (CollectionUtils.isNotEmpty(resultList)) {
+            resultObj.put("failureReasonList", resultList);
+        }
+        return resultObj;
+    }
+
+    private JSONObject save(DataSourceVo vo) {
+        return null;
+    }
+
+}
