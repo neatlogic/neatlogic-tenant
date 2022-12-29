a87adaea69a8a37f7256dabfa6c28cbfbe111fe8
吕佐康 <lvzk@techsure.com.cn>
2022-12-29 18:17:39 +0800

    --story=1007143 --user=lvzk 自动化 -- 执行目标选择（包括所有入口）-- 过滤器方式 --增加【高级模式】选项 https://www.tapd.cn/54247054/s/1264750

--- src/main/java/codedriver/module/tenant/api/user/UserSearchForSelectApi.java
+++ src/main/java/codedriver/module/tenant/api/user/UserSearchForSelectApi.java
@@ -5,31 +5,23 @@
 
 package codedriver.module.tenant.api.user;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import codedriver.framework.auth.core.AuthAction;
-import org.apache.commons.collections4.CollectionUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.common.util.PageUtil;
+import codedriver.framework.dao.mapper.UserMapper;
+import codedriver.framework.dto.UserVo;
+import codedriver.framework.restful.annotation.*;
+import codedriver.framework.restful.constvalue.OperationTypeEnum;
+import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import codedriver.framework.common.constvalue.ApiParamType;
-import codedriver.framework.common.constvalue.GroupSearch;
-import codedriver.framework.common.util.PageUtil;
-import codedriver.framework.dao.mapper.UserMapper;
-import codedriver.framework.dto.UserVo;
-import codedriver.framework.restful.constvalue.OperationTypeEnum;
-import codedriver.framework.restful.annotation.Description;
-import codedriver.framework.restful.annotation.Input;
-import codedriver.framework.restful.annotation.OperationType;
-import codedriver.framework.restful.annotation.Output;
-import codedriver.framework.restful.annotation.Param;
-import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @OperationType(type = OperationTypeEnum.SEARCH)
@@ -98,12 +90,7 @@ public class UserSearchForSelectApi extends PrivateApiComponentBase {
 		List<UserVo> userList = null;
 		JSONArray defaultValue = jsonObj.getJSONArray("defaultValue");
 	    if(CollectionUtils.isNotEmpty(defaultValue)) {
-	        List<String> uuidList = new ArrayList<>();
-	        for(int i = 0; i < defaultValue.size(); i++) {
-	            String value = defaultValue.getString(i);
-	            uuidList.add(value.replaceAll(GroupSearch.USER.getValuePlugin(),""));
-	        }
-			userList = userMapper.getUserListByUuidList(uuidList);
+			userList = userMapper.getUserListByUuidList(defaultValue.toJavaList(String.class));
 	    }else {
 	        UserVo userVo = JSON.parseObject(jsonObj.toJSONString(), new TypeReference<UserVo>() {});
 	        int pageCount = 0;
@@ -123,7 +110,7 @@ public class UserSearchForSelectApi extends PrivateApiComponentBase {
             JSONArray resultArray = new JSONArray();
 	        for(UserVo user : userList) {
                 JSONObject userObj = new JSONObject();
-                userObj.put("value", "user#" + user.getUuid());
+                userObj.put("value", user.getUuid());
                 userObj.put("text", user.getUserName() + "(" + user.getUserId() + ")");
                 userObj.put("name", user.getUserName());
                 userObj.put("pinyin", user.getPinyin());
