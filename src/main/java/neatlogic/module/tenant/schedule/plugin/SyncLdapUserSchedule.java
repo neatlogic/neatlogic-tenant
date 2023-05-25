be918d65f7e1f32abe261329462427233b6c2483
luoyu <luoyu@techsure.com.cn>
2023-05-25 10:38:16 +0800

    --story=1007671 --user=罗宇 从LDAP自动导入用户和组织架构 https://www.tapd.cn/54247054/s/1273911

--- src/main/java/neatlogic/module/tenant/schedule/plugin/SyncLdapUserSchedule.java
+++ src/main/java/neatlogic/module/tenant/schedule/plugin/SyncLdapUserSchedule.java
@@ -29,6 +29,7 @@ import neatlogic.framework.scheduler.core.PublicJobBase;
 import neatlogic.framework.scheduler.dto.JobAuditVo;
 import neatlogic.framework.scheduler.dto.JobObject;
 import neatlogic.framework.transaction.util.TransactionUtil;
+import neatlogic.framework.util.TimeUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobDetail;
@@ -46,8 +47,7 @@ import javax.naming.directory.Attribute;
 import javax.naming.directory.Attributes;
 import javax.naming.directory.SearchControls;
 import javax.naming.directory.SearchResult;
-import javax.naming.ldap.InitialLdapContext;
-import javax.naming.ldap.LdapContext;
+import javax.naming.ldap.*;
 import java.util.*;
 
 @Component
@@ -94,7 +94,7 @@ public class SyncLdapUserSchedule extends PublicJobBase {
         JobDetail jobDetail = context.getJobDetail();
         String jobUuid = jobDetail.getKey().getName();
 
-        String batchSize = "2000"; //分页数量，默认是1000
+        int pageSize = 2000 ;//分页查询大小
         String ldapUrl = getPropValue(jobObject, "ldapUrl");
         String userDn = getPropValue(jobObject, "userDn");
         String userSecret = getPropValue(jobObject, "userSecret");
@@ -115,15 +115,18 @@ public class SyncLdapUserSchedule extends PublicJobBase {
         }
 
         if (StringUtils.isNotBlank(ldapUrl) && StringUtils.isNotBlank(userDn) && StringUtils.isNotBlank(userSecret)) {
-            TransactionStatus transactionStatus = TransactionUtil.openTx();
+            TransactionStatus transactionStatus = null;
             Hashtable<String, String> HashEnv = new Hashtable<String, String>();
             HashEnv.put(Context.SECURITY_AUTHENTICATION, "simple"); // LDAP访问安全级别
             HashEnv.put(Context.SECURITY_PRINCIPAL, userDn);
             HashEnv.put(Context.SECURITY_CREDENTIALS, userSecret);
             HashEnv.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
             HashEnv.put(Context.PROVIDER_URL, ldapUrl);
-            HashEnv.put(Context.BATCHSIZE, batchSize);
+
             try {
+                int totalResults = 0;
+                byte[] cookie = null;
+
                 LdapContext ctx = new InitialLdapContext(HashEnv, null);
                 // 搜索控制器
                 SearchControls searchCtls = new SearchControls();
@@ -133,101 +136,110 @@ public class SyncLdapUserSchedule extends PublicJobBase {
                 String[] returnedAtts = { "entryUUID","uid","sAMAccountName","cn","mobile","mail" ,"telephoneNumber","description"};
                 searchCtls.setReturningAttributes(returnedAtts);
 
-                List<UserVo> userList = new ArrayList<>();
-                List<String> teamUpNamePathList = new ArrayList<>();
-                // 根据设置的域节点、过滤器类和搜索控制器搜索LDAP得到结果
-                NamingEnumeration answer = ctx.search(searchBase, searchFilter, searchCtls);
-                while (answer.hasMoreElements()) {
-                    SearchResult sr = (SearchResult) answer.next();
-                    String fullName = sr.getNameInNamespace();
-                    String upwardTeamName = getUpwardTeamName(fullName);
-                    String uuid=null , userId = null , userName = null , email =null ,phone = null ;
-                    Attributes Attrs = sr.getAttributes();
-                    if (Attrs != null) {
-                        try {
-                            for (NamingEnumeration ne = Attrs.getAll(); ne.hasMore(); ) {
-                                Attribute Attr = (Attribute) ne.next();
-                                String attrName = Attr.getID().toString();
-                                String attrValue = null;
-                                for (NamingEnumeration e = Attr.getAll(); e.hasMore(); ) {
-                                    attrValue = e.next().toString();
+                //开启分页查询
+                ctx.setRequestControls(new Control[]{new PagedResultsControl(pageSize, Control.CRITICAL)});
+
+                do{
+                    transactionStatus = TransactionUtil.openTx();
+                    List<UserVo> userList = new ArrayList<>();
+                    List<String> teamUpNamePathList = new ArrayList<>();
+                    // 根据设置的域节点、过滤器类和搜索控制器搜索LDAP得到结果
+                    NamingEnumeration answer = ctx.search(searchBase, searchFilter, searchCtls);
+                    while (answer.hasMoreElements()) {
+                        totalResults++;
+                        SearchResult sr = (SearchResult) answer.next();
+                        String fullName = sr.getNameInNamespace();
+                        String upwardTeamName = getUpwardTeamName(fullName);
+                        String uuid = null, userId = null, userName = null, email = null, phone = null;
+                        Attributes Attrs = sr.getAttributes();
+                        if (Attrs != null) {
+                            try {
+                                for (NamingEnumeration ne = Attrs.getAll(); ne.hasMore(); ) {
+                                    Attribute Attr = (Attribute) ne.next();
+                                    String attrName = Attr.getID().toString();
+                                    String attrValue = null;
+                                    for (NamingEnumeration e = Attr.getAll(); e.hasMore(); ) {
+                                        attrValue = e.next().toString();
+                                    }
+                                    if (attrName.equals("uid") || attrName.equals("sAMAccountName")) {
+                                        userId = attrValue;
+                                    } else if (attrName.equals("name") || attrName.equals("cn")) {
+                                        userName = attrValue;
+                                    } else if (attrName.equals("email") || attrName.equals("mail")) {
+                                        email = attrValue;
+                                    } else if (attrName.equals("telephoneNumber")) {
+                                        phone = attrValue;
+                                    } else if (attrName.equals("entryUUID")) {
+                                        uuid = attrValue.replace("-", "");
+                                    }
                                 }
-                                if (attrName.equals("uid") || attrName.equals("sAMAccountName")) {
-                                    userId = attrValue;
-                                }else if(attrName.equals("name") || attrName.equals("cn")){
-                                    userName = attrValue;
-                                }else if(attrName.equals("email") || attrName.equals("mail")){
-                                    email = attrValue;
-                                }else if(attrName.equals("telephoneNumber")){
-                                    phone = attrValue;
-                                }else if(attrName.equals("entryUUID")){
-                                    uuid = attrValue.replace("-", "");
-                                }
+                            } catch (NamingException e) {
+                                logger.error("[Sync Ldap User Error]:" + e.getMessage());
                             }
-                        } catch (NamingException e) {
-                            logger.error("[Sync Ldap User Error]:" + e.getMessage());
+                        }
+
+                        if (StringUtils.isNotBlank(userId)) {
+                            UserVo userVo = new UserVo();
+                            userVo.setUuid(uuid);
+                            userVo.setUserId(userId);
+                            userVo.setUserName(userName);
+                            userVo.setEmail(email);
+                            userVo.setPhone(phone);
+                            //中转
+                            userVo.setTeamUuid(upwardTeamName);
+                            teamUpNamePathList.add(upwardTeamName);
+                            userList.add(userVo);
                         }
                     }
 
-                    if(StringUtils.isNotBlank(userId)){
-                        UserVo userVo = new UserVo();
-                        userVo.setUuid(uuid);
-                        userVo.setUserId(userId);
-                        userVo.setUserName(userName);
-                        userVo.setEmail(email);
-                        userVo.setPhone(phone);
-                        //中转
-                        userVo.setTeamUuid(upwardTeamName);
-                        teamUpNamePathList.add(upwardTeamName);
-                        userList.add(userVo);
+                    Map<String , String> upNamePathMap = new HashMap<>();
+                    List<TeamVo>  teamList = this.teamMapper.getTeamUuidbyUpwardNamePath(teamUpNamePathList);
+                    for (TeamVo teamVo: teamList) {
+                        upNamePathMap.put(teamVo.getUpwardNamePath() , teamVo.getUuid());
                     }
-                }
+
+                    List<String> roleUuidList = null; ;
+                    if(StringUtils.isNotBlank(defaultRole)){
+                        String[] roleNames = defaultRole.split(",");
+                        List<String> nameList = Arrays.asList(roleNames);
+                        if(nameList != null && nameList.size() > 0 ){
+                            roleUuidList = this.roleMapper.getRoleUuidByNameList(nameList);
+                        }
+                    }
+
+                    List<RoleUserVo> userRoleList = new ArrayList<>();
+                    List<TeamUserVo> userTeamList = new ArrayList<TeamUserVo>();
+                    List<String> userUuidList = new ArrayList<>();
+                    for (UserVo userVo: userList) {
+                        userUuidList.add(userVo.getUuid());
+
+                        //人与组织关系
+                        String teamUuid = upNamePathMap.get(userVo.getTeamUuid());
+                        if(StringUtils.isNotBlank(teamUuid)){
+                            userTeamList.add(new TeamUserVo(teamUuid , userVo.getUuid()));
+                        }
+
+                        //人员默认角色
+                        if(roleUuidList != null ){
+                            for (String roleUuid:roleUuidList) {
+                                userRoleList.add(new RoleUserVo(roleUuid , userVo.getUuid()));
+                            }
+                        }
+                    }
+                    this.userMapper.bacthDeleteUserTeam(userUuidList , "ldap");
+                    this.userMapper.batchInsertUser(userList);
+                    this.userMapper.batchInsertUserRole(userRoleList);
+                    this.userMapper.batchInsertUserTeam(userTeamList);
+                    TransactionUtil.commitTx(transactionStatus);
+
+                    cookie = parseControls(ctx.getResponseControls());
+                    ctx.setRequestControls(new Control[]{new PagedResultsControl(pageSize, cookie, Control.CRITICAL)});
+                }while((cookie != null) && (cookie.length != 0));
+                logger.info("时间："+TimeUtil.getDateString("yyyy-MM-dd hh:mm:ss")+"，从ldap同步用户总数=" +  totalResults);
                 ctx.close();
-
-                Map<String , String> upNamePathMap = new HashMap<>();
-                List<TeamVo>  teamList = this.teamMapper.getTeamUuidbyUpwardNamePath(teamUpNamePathList);
-                for (TeamVo teamVo: teamList) {
-                    upNamePathMap.put(teamVo.getUpwardNamePath() , teamVo.getUuid());
-                }
-
-                List<String> roleUuidList = null; ;
-                if(StringUtils.isNotBlank(defaultRole)){
-                    String[] roleNames = defaultRole.split(",");
-                    List<String> nameList = Arrays.asList(roleNames);
-                    if(nameList != null && nameList.size() > 0 ){
-                        roleUuidList = this.roleMapper.getRoleUuidByNameList(nameList);
-                    }
-                }
-
-                List<RoleUserVo> userRoleList = new ArrayList<>();
-                List<TeamUserVo> userTeamList = new ArrayList<TeamUserVo>();
-                List<String> userUuidList = new ArrayList<>();
-                for (UserVo userVo: userList) {
-                    userUuidList.add(userVo.getUuid());
-
-                    //人与组织关系
-                    String teamUuid = upNamePathMap.get(userVo.getTeamUuid());
-                    if(StringUtils.isNotBlank(teamUuid)){
-                        userTeamList.add(new TeamUserVo(teamUuid , userVo.getUuid()));
-                    }
-
-                    //人员默认角色
-                    if(roleUuidList != null ){
-                        for (String roleUuid:roleUuidList) {
-                            userRoleList.add(new RoleUserVo(roleUuid , userVo.getUuid()));
-                        }
-                    }
-                }
-                this.userMapper.bacthDeleteUserTeam(userUuidList , "ldap");
-                this.userMapper.batchInsertUser(userList);
-                this.userMapper.batchInsertUserRole(userRoleList);
-                this.userMapper.batchInsertUserTeam(userTeamList);
-                TransactionUtil.commitTx(transactionStatus);
-            } catch (NamingException e) {
+            } catch (Exception e) {
                 logger.error("[Sync Ldap User Error]:" + e.getMessage());
                 TransactionUtil.rollbackTx(transactionStatus);
-            }finally {
-
             }
         }else{
             logger.error("[Sync Ldap User Error]: Incomplete Plugin parameters.");
@@ -282,4 +294,17 @@ public class SyncLdapUserSchedule extends PublicJobBase {
         return null ;
     }
 
+    private static byte[] parseControls(Control[] controls) throws NamingException {
+        byte[] cookie = null;
+        if (controls != null) {
+            for (int i = 0; i < controls.length; i++) {
+                if (controls[i] instanceof PagedResultsResponseControl) {
+                    PagedResultsResponseControl prrc = (PagedResultsResponseControl) controls[i];
+                    cookie = prrc.getCookie();
+                    System.out.println(">>Next Page \n");
+                }
+            }
+        }
+        return (cookie == null) ? new byte[0] : cookie;
+    }
 }
