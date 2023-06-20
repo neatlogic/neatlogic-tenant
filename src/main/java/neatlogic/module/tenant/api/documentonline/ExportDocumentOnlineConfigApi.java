18e8b6e7533c81939a651690267f359fd4ee1f11
linbangquan <1437892690@qq.com>
2023-06-20 09:53:34 +0800

    --task=1007858 后端-在线帮助文档与模块菜单之间映射维护接口 https://www.tapd.cn/54247054/s/1276016

---
+++ src/main/java/neatlogic/module/tenant/api/documentonline/ExportDocumentOnlineConfigApi.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright(c) 2023 NeatLogic Co., Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package neatlogic.module.tenant.api.documentonline;
+
+import com.alibaba.fastjson.JSONObject;
+import neatlogic.framework.crossover.CrossoverServiceFactory;
+import neatlogic.framework.documentonline.crossover.IDocumentOnlineCrossoverMapper;
+import neatlogic.framework.documentonline.dto.DocumentOnlineConfigVo;
+import neatlogic.framework.restful.annotation.Description;
+import neatlogic.framework.restful.annotation.Input;
+import neatlogic.framework.restful.annotation.OperationType;
+import neatlogic.framework.restful.constvalue.OperationTypeEnum;
+import neatlogic.framework.restful.core.privateapi.PrivateBinaryStreamApiComponentBase;
+import neatlogic.framework.util.FileUtil;
+import neatlogic.module.framework.startup.DocumentOnlineInitializeIndexHandler;
+import org.apache.commons.io.IOUtils;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@OperationType(type = OperationTypeEnum.SEARCH)
+public class ExportDocumentOnlineConfigApi extends PrivateBinaryStreamApiComponentBase {
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Input({})
+    @Description(desc = "下载在线帮助文档配置文件")
+    @Override
+    public Object myDoService(JSONObject paramObj, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        List<DocumentOnlineConfigVo> allList = new ArrayList<>();
+        IDocumentOnlineCrossoverMapper documentOnlineCrossoverMapper = CrossoverServiceFactory.getApi(IDocumentOnlineCrossoverMapper.class);
+        List<DocumentOnlineConfigVo> documentOnlineConfigList = documentOnlineCrossoverMapper.getAllDocumentOnlineConfigList();
+        for (DocumentOnlineConfigVo documentOnlineConfigVo : documentOnlineConfigList) {
+            documentOnlineConfigVo.setSource("database");
+            allList.add(documentOnlineConfigVo);
+        }
+        for (DocumentOnlineConfigVo documentOnlineConfigVo : DocumentOnlineInitializeIndexHandler.getMappingConfigList()) {
+            if (!allList.contains(documentOnlineConfigVo)) {
+                allList.add(documentOnlineConfigVo);
+            }
+        }
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-Disposition", " attachment; filename=\"" + FileUtil.getEncodedFileName("documentonline-mapping.json") + "\"");
+        try (InputStream in = IOUtils.toInputStream(JSONObject.toJSONString(allList), StandardCharsets.UTF_8);
+             ServletOutputStream os = response.getOutputStream()) {
+            IOUtils.copyLarge(in, os);
+            os.flush();
+        }
+        return null;
+    }
+
+    @Override
+    public String getToken() {
+        return "documentonline/config/export";
+    }
+}
