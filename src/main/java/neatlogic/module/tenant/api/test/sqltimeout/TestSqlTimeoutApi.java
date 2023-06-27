6299ddcf1d7956566947163011d04c06d6ed71e4
linbangquan <1437892690@qq.com>
2023-06-27 11:05:05 +0800

    [功能] 设置事务超时时间及捕获事务超时异常信息打印到单独日志文件中
    关联 --story=1007126 事务耗时监控 https://www.tapd.cn/54247054/s/1276472

---
+++ src/main/java/neatlogic/module/tenant/api/test/sqltimeout/TestSqlTimeoutApi.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright(c) 2023 NeatLogic Co., Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package neatlogic.module.tenant.api.test.sqltimeout;
+
+import com.alibaba.fastjson.JSONObject;
+import neatlogic.framework.common.constvalue.ApiParamType;
+import neatlogic.framework.restful.annotation.Description;
+import neatlogic.framework.restful.annotation.Input;
+import neatlogic.framework.restful.annotation.Output;
+import neatlogic.framework.restful.annotation.Param;
+import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
+import neatlogic.framework.transaction.util.TransactionUtil;
+import neatlogic.module.tenant.dao.mapper.TestMapper;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
+@Component
+@Transactional(timeout = 10)
+public class TestSqlTimeoutApi extends PrivateApiComponentBase {
+
+    @Resource
+    private TestMapper testMapper;
+
+    @Override
+    public String getName() {
+        return "Test transaction timeout";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Input({
+            @Param(name = "id1", type = ApiParamType.LONG, isRequired = true, desc = "ID1"),
+            @Param(name = "id2", type = ApiParamType.LONG, isRequired = true, desc = "ID2")
+    })
+    @Output({})
+    @Description(desc = "Test transaction timeout")
+    @Override
+    public Object myDoService(JSONObject paramObj) throws Exception {
+        System.out.println("startTime=" + System.currentTimeMillis());
+//        TransactionStatus transactionStatus = TransactionUtil.openTx();
+//        try {
+            Long id1 = paramObj.getLong("id1");
+            Long id2 = paramObj.getLong("id2");
+            testMapper.getProcessTaskByIdForUpdate(id1);
+            System.out.println("id1=" + id1);
+            testMapper.getProcessTaskByIdForUpdate(id2);
+            System.out.println("id2=" + id2);
+            System.out.println("endTime=" + System.currentTimeMillis());
+//            TransactionUtil.commitTx(transactionStatus);
+//        } catch (Exception e) {
+//            TransactionUtil.rollbackTx(transactionStatus);
+//        }
+        return null;
+    }
+
+    @Override
+    public String getToken() {
+        return "test/sql/timeout";
+    }
+}
