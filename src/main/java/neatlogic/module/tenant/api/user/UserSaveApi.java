1043963121deb624147b1365306881d8885477b6
linbangquan <1437892690@qq.com>
2023-06-30 18:45:39 +0800

    [功能] 将用户物理删除改成逻辑删除
    关联 --task=1007895 后端-用户被禁用,头像提示下这个用户被禁用效果 https://www.tapd.cn/54247054/s/1276896

--- src/main/java/neatlogic/module/tenant/api/user/UserSaveApi.java
+++ src/main/java/neatlogic/module/tenant/api/user/UserSaveApi.java
@@ -27,6 +27,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 @Service
@@ -35,170 +36,149 @@ import java.util.Set;
 @OperationType(type = OperationTypeEnum.UPDATE)
 public class UserSaveApi extends PrivateApiComponentBase {
 
-	@Autowired
-	UserMapper userMapper;
+    @Autowired
+    UserMapper userMapper;
 
-	@Autowired
-	TeamMapper teamMapper;
+    @Autowired
+    TeamMapper teamMapper;
 
-	@Override
-	public String getToken() {
-		return "user/save";
-	}
+    @Override
+    public String getToken() {
+        return "user/save";
+    }
 
-	@Override
-	public String getName() {
-		return "保存用户接口";
-	}
+    @Override
+    public String getName() {
+        return "nmtau.usersaveapi.getname";
+    }
 
-	@Override
-	public String getConfig() {
-		return null;
-	}
+    @Override
+    public String getConfig() {
+        return null;
+    }
 
-	@Input({
-			@Param(name = "uuid",
-				type = ApiParamType.STRING,
-				desc = "用户uuid"),
-			@Param(name = "userId",
-					type = ApiParamType.STRING,
-					desc = "用户Id",
-					isRequired = true,
-                    xss = true),
-			@Param(name = "userName",
-					type = ApiParamType.STRING,
-					desc = "用户姓名",
-					isRequired = true,
-					xss = true),
-			@Param(name = "password",
-					type = ApiParamType.STRING,
-					desc = "用户密码",
-					xss = true),
-			@Param(name = "email",
-					type = ApiParamType.STRING,
-					desc = "用户邮箱",
-					isRequired = false,
-					xss = true),
-			@Param(name = "phone",
-					type = ApiParamType.STRING,
-					desc = "用户电话",
-					isRequired = false,
-					xss = true),
-			@Param(name = "isActive",
-					type = ApiParamType.INTEGER,
-					desc = "是否激活",
-					isRequired = false),
-			@Param(name = "teamUuidList",
-					type = ApiParamType.JSONARRAY,
-					desc = "组织uuid",
-					isRequired = false),
-			@Param(name = "roleUuidList",
-					type = ApiParamType.JSONARRAY,
-					desc = "角色名称",
-					isRequired = false),
-			@Param(name = "userInfo",
-				type = ApiParamType.STRING,
-				desc = "其他信息",
-				isRequired = false,
-				xss = true),
-			@Param(name = "vipLevel",
-					type = ApiParamType.ENUM,
-					desc = "用户等级",
-					isRequired = false,
-					rule = "0,1,2,3,4,5"),
-			@Param(name = "userAuthList",
-				type = ApiParamType.JSONOBJECT,
-				desc = "权限列表"
-			)/*,
-			@Param(name = "saveMode",
-					type = ApiParamType.ENUM,
-					rule = "merge,replace",
-					desc = "保存方式，merge：只更新提供的属性，replace：更新全部属性，不提供的置空")*/ })
-	@Output({})
-	@Description(desc = "保存用户接口")
-	@Override
-	public Object myDoService(JSONObject jsonObj) throws Exception {
-		UserVo userVo = new UserVo();		
-		userVo.setUserId(jsonObj.getString("userId"));
-		userVo.setUserName(jsonObj.getString("userName"));
-		userVo.setPassword(jsonObj.getString("password"));
-		userVo.setEmail(jsonObj.getString("email"));
-		userVo.setPhone(jsonObj.getString("phone"));
-		userVo.setIsActive(jsonObj.getInteger("isActive"));
-		userVo.setUserInfo(jsonObj.getString("userInfo"));
-		userVo.setVipLevel(jsonObj.getInteger("vipLevel"));
-		
-		String uuid = jsonObj.getString("uuid");
-		if(StringUtils.isBlank(uuid)) {
-			userVo.setUuid(UuidUtil.randomUuid());
-			if(userMapper.checkUserIdIsIsRepeat(userVo) > 0) {
-			    throw new UserIdRepeatException(userVo.getUserId());
-			}
-			userMapper.insertUser(userVo);
-			userMapper.insertUserPassword(userVo);
-			JSONObject userAuthObj = jsonObj.getJSONObject("userAuthList");
-			if(MapUtils.isNotEmpty(userAuthObj)) {
-				Set<String> keySet = userAuthObj.keySet();
-				for (String key : keySet){
-					JSONArray authArray = userAuthObj.getJSONArray(key);
-					for (int j = 0; j < authArray.size(); j++){
-						UserAuthVo authVo = new UserAuthVo();
-						authVo.setAuth(authArray.getString(j));
-						authVo.setAuthGroup(key);
-						authVo.setUserUuid(userVo.getUuid());
-						userMapper.insertUserAuth(authVo);
-					}
-				}
-			}		
-		}else {
-			if(userMapper.getUserBaseInfoByUuid(uuid) == null) {
-				throw new UserNotFoundException(uuid);
-			}
-			userVo.setUuid(uuid);
-			if(userMapper.checkUserIdIsIsRepeat(userVo) > 0) {
+    @Input({
+            @Param(name = "uuid", type = ApiParamType.STRING, desc = "common.uuid"),
+            @Param(name = "userId", type = ApiParamType.STRING, desc = "common.userid", isRequired = true, xss = true),
+            @Param(name = "userName", type = ApiParamType.STRING, desc = "common.username", isRequired = true, xss = true),
+            @Param(name = "password", type = ApiParamType.STRING, desc = "common.password", xss = true),
+            @Param(name = "email", type = ApiParamType.STRING, desc = "common.email", isRequired = false, xss = true),
+            @Param(name = "phone", type = ApiParamType.STRING, desc = "common.phone", isRequired = false, xss = true),
+            @Param(name = "isActive", type = ApiParamType.INTEGER, desc = "common.isactive", isRequired = false),
+            @Param(name = "teamUuidList", type = ApiParamType.JSONARRAY, desc = "common.teamuuidlist", isRequired = false),
+            @Param(name = "roleUuidList", type = ApiParamType.JSONARRAY, desc = "common.roleuuidlist", isRequired = false),
+            @Param(name = "userInfo", type = ApiParamType.STRING, desc = "term.framework.userotherinfo", isRequired = false, xss = true),
+            @Param(name = "vipLevel", type = ApiParamType.ENUM, desc = "term.framework.viplevel", isRequired = false, rule = "0,1,2,3,4,5"),
+            @Param(name = "userAuthList", type = ApiParamType.JSONOBJECT, desc = "common.authlist")
+    })
+    @Output({})
+    @Description(desc = "nmtau.usersaveapi.getname")
+    @Override
+    public Object myDoService(JSONObject jsonObj) throws Exception {
+        UserVo userVo = new UserVo();
+        userVo.setUserId(jsonObj.getString("userId"));
+        userVo.setUserName(jsonObj.getString("userName"));
+        userVo.setPassword(jsonObj.getString("password"));
+        userVo.setEmail(jsonObj.getString("email"));
+        userVo.setPhone(jsonObj.getString("phone"));
+        userVo.setIsActive(jsonObj.getInteger("isActive"));
+        userVo.setUserInfo(jsonObj.getString("userInfo"));
+        userVo.setVipLevel(jsonObj.getInteger("vipLevel"));
+
+        String uuid = jsonObj.getString("uuid");
+        if (StringUtils.isBlank(uuid)) {
+            userVo.setUuid(UuidUtil.randomUuid());
+            if (userMapper.checkUserIdIsIsRepeat(userVo) > 0) {
                 throw new UserIdRepeatException(userVo.getUserId());
             }
-			userMapper.updateUser(userVo);
-			userMapper.deleteUserRoleByUserUuid(userVo.getUuid());
-			userMapper.deleteUserTeamByUserUuid(userVo.getUuid());
-			//更新密码
-			if (StringUtils.isNotBlank(userVo.getPassword())){
-				userMapper.updateUserPasswordActive(userVo.getUuid());
-				List<Long> idList = userMapper.getLimitUserPasswordIdList(userVo.getUuid());
-				if (CollectionUtils.isNotEmpty(idList)){
-					userMapper.deleteUserPasswordByLimit(userVo.getUuid(), idList);
-				}
-				userMapper.insertUserPassword(userVo);
-			}
-		}
-		
-		List<String> teamUuidList = JSON.parseArray(jsonObj.getString("teamUuidList"), String.class);
-		if(CollectionUtils.isNotEmpty(teamUuidList)) {
-			for(String teamUuid : teamUuidList) {
-				userMapper.insertUserTeam(userVo.getUuid(), teamUuid.replaceAll(GroupSearch.TEAM.getValuePlugin(), StringUtils.EMPTY));
-			}
-		}
-		
-		List<String> roleUuidList = JSON.parseArray(jsonObj.getString("roleUuidList"), String.class);
-		if(CollectionUtils.isNotEmpty(roleUuidList)) {
-			for(String roleUuid : roleUuidList) {
-				userMapper.insertUserRole(userVo.getUuid(), roleUuid.replaceAll(GroupSearch.ROLE.getValuePlugin(), StringUtils.EMPTY));
-			}
-		}
-		
-		return userVo.getUuid();
-	}
+            UserVo deletedUserVo = userMapper.getUserByUserId(userVo.getUserId());
+            if (deletedUserVo != null) {
+                // userId是已删除的旧用户，重用用户uuid
+                String oldUuid = deletedUserVo.getUuid();
+                userMapper.updateUserIsNotDeletedByUuid(oldUuid);
+                userVo.setUuid(oldUuid);
+                // 删除用户角色
+                userMapper.deleteUserRoleByUserUuid(oldUuid);
+                // 删除用户组
+                userMapper.deleteUserTeamByUserUuid(oldUuid);
+                // 删除用户权限
+                userMapper.deleteUserAuth(new UserAuthVo(oldUuid));
+                // 删除用户密码
+                userMapper.deleteUserPasswordByUserUuid(oldUuid);
+                // 删除用户个性化中弹窗提醒设置数据
+                userMapper.deleteUserDataByUserUuid(oldUuid);
+                // 删除用户个性化中默认模块及各模块首页设置数据
+                userMapper.deleteUserProfileByUserUuidAndModuleId(oldUuid, null);
+                userMapper.updateUser(userVo);
+            } else {
+                userMapper.insertUser(userVo);
+            }
+            userMapper.insertUserPassword(userVo);
+            JSONObject userAuthObj = jsonObj.getJSONObject("userAuthList");
+            if (MapUtils.isNotEmpty(userAuthObj)) {
+                Set<String> keySet = userAuthObj.keySet();
+                for (String key : keySet) {
+                    JSONArray authArray = userAuthObj.getJSONArray(key);
+                    for (int j = 0; j < authArray.size(); j++) {
+                        UserAuthVo authVo = new UserAuthVo();
+                        authVo.setAuth(authArray.getString(j));
+                        authVo.setAuthGroup(key);
+                        authVo.setUserUuid(userVo.getUuid());
+                        userMapper.insertUserAuth(authVo);
+                    }
+                }
+            }
+        } else {
+            UserVo existUserVo = userMapper.getUserBaseInfoByUuid(uuid);
+            if (existUserVo == null || Objects.equals(existUserVo.getIsDelete(), 1)) {
+                throw new UserNotFoundException(uuid);
+            }
+            userVo.setUuid(uuid);
+            userMapper.updateUser(userVo);
+            // 删除用户角色
+            userMapper.deleteUserRoleByUserUuid(userVo.getUuid());
+            // 删除用户组
+            userMapper.deleteUserTeamByUserUuid(userVo.getUuid());
+            //更新密码
+            if (StringUtils.isNotBlank(userVo.getPassword())) {
+                userMapper.updateUserPasswordActive(userVo.getUuid());
+                List<Long> idList = userMapper.getLimitUserPasswordIdList(userVo.getUuid());
+                if (CollectionUtils.isNotEmpty(idList)) {
+                    userMapper.deleteUserPasswordByLimit(userVo.getUuid(), idList);
+                }
+                userMapper.insertUserPassword(userVo);
+            }
+        }
 
-	public IValid userId(){
-		return value -> {
-			UserVo userVo = JSON.toJavaObject(value, UserVo.class);
-			if(StringUtils.isBlank(userVo.getUuid())){
-				userVo.setUuid(UuidUtil.randomUuid());
-			}
-			if(userMapper.checkUserIdIsIsRepeat(userVo) > 0) {
-				return new FieldValidResultVo(new UserIdRepeatException(userVo.getUserId()));
-			}
-			return new FieldValidResultVo();
-		};
-	}
+        JSONArray teamUuidArray = jsonObj.getJSONArray("teamUuidList");
+        if (CollectionUtils.isNotEmpty(teamUuidArray)) {
+            List<String> teamUuidList = teamUuidArray.toJavaList(String.class);
+            for (String teamUuid : teamUuidList) {
+                userMapper.insertUserTeam(userVo.getUuid(), teamUuid.replaceAll(GroupSearch.TEAM.getValuePlugin(), StringUtils.EMPTY));
+            }
+        }
+
+        JSONArray roleUuidArray = jsonObj.getJSONArray("roleUuidList");
+        if (CollectionUtils.isNotEmpty(roleUuidArray)) {
+            List<String> roleUuidList = roleUuidArray.toJavaList(String.class);
+            for (String roleUuid : roleUuidList) {
+                userMapper.insertUserRole(userVo.getUuid(), roleUuid.replaceAll(GroupSearch.ROLE.getValuePlugin(), StringUtils.EMPTY));
+            }
+        }
+
+        return userVo.getUuid();
+    }
+
+    public IValid userId() {
+        return value -> {
+            UserVo userVo = JSON.toJavaObject(value, UserVo.class);
+            if (StringUtils.isBlank(userVo.getUuid())) {
+                userVo.setUuid(UuidUtil.randomUuid());
+            }
+            if (userMapper.checkUserIdIsIsRepeat(userVo) > 0) {
+                return new FieldValidResultVo(new UserIdRepeatException(userVo.getUserId()));
+            }
+            return new FieldValidResultVo();
+        };
+    }
 }
