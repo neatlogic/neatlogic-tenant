fc5fff93ca94a4f31df9408ec4792a6a6d99e692
lvzk <897706680@qq.com>
2023-09-06 15:35:24 +0800

    [功能]获取配置补充商业模块set

--- src/main/java/neatlogic/module/tenant/api/InitConfigGetApi.java
+++ src/main/java/neatlogic/module/tenant/api/InitConfigGetApi.java
@@ -22,11 +22,23 @@ import neatlogic.framework.dto.ThemeVo;
 import neatlogic.framework.restful.annotation.*;
 import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
 import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @OperationType(type = OperationTypeEnum.SEARCH)
 public class InitConfigGetApi extends PrivateApiComponentBase {
+    private final Logger logger = LoggerFactory.getLogger(InitConfigGetApi.class);
+    private final static Set<String> moduleSet = new HashSet<>();
+    private boolean isLoad = false;
 
     @Override
     public String getName() {
@@ -51,7 +63,31 @@ public class InitConfigGetApi extends PrivateApiComponentBase {
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
         JSONObject result = new JSONObject();
+        if (!isLoad) {
+            getCommercialModule();
+        }
+        result.put("commercialModuleSet", moduleSet);
         result.put("ssoTicketKey", Config.SSO_TICKET_KEY());
         return result;
+    }
+
+    private void getCommercialModule() {
+        Reflections reflections = new Reflections("neatlogic");
+        Set<Class<? extends InstantiationAwareBeanPostProcessor>> authClass = reflections.getSubTypesOf(InstantiationAwareBeanPostProcessor.class);
+        for (Class<? extends InstantiationAwareBeanPostProcessor> c : authClass) {
+            try {
+                if (!c.getSimpleName().endsWith("AuthBean")) {
+                    continue;
+                }
+                Field field = c.getDeclaredField("moduleSet");
+                field.setAccessible(true);
+                Object value = field.get(null);
+                moduleSet.addAll((Collection<? extends String>) value);
+            } catch (Exception ex) {
+                logger.error(ex.getMessage(), ex);
+            }
+        }
+        isLoad = true;
+
     }
 }
