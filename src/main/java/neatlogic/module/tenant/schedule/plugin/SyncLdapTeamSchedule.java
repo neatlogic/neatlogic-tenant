05751cdcc34c4b93806891e78c6282d68134eb17
linbangquan <1437892690@qq.com>
2023-10-20 15:07:47 +0800

    [修复] Ldap同步用户和分组 优化
    关联 #[1005841514201088]Ldap同步用户和分组 优化 http://192.168.0.96:8090/demo/rdm.html#/bug-detail/939050947543040/939050947543057/1005841514201088

--- src/main/java/neatlogic/module/tenant/schedule/plugin/SyncLdapTeamSchedule.java
+++ src/main/java/neatlogic/module/tenant/schedule/plugin/SyncLdapTeamSchedule.java
@@ -125,7 +125,6 @@ public class SyncLdapTeamSchedule extends PublicJobBase {
         int totalResults = 0;
         Map<String, String> uuidMap = new HashMap<>();
         String baseDnName = "";
-        String baseDnUuid = "";
         String msg = "baseDN=" + baseDN;
         msg = msg + ", filter=" + searchFilter;
         msg = msg + ", searchScope=SUBTREE_SCOPE";
@@ -133,15 +132,11 @@ public class SyncLdapTeamSchedule extends PublicJobBase {
         do {
             // 根据设置的域节点、过滤器类和搜索控制器搜索LDAP得到结果
             NamingEnumeration answer = ctx.search(baseDN, searchFilter, searchCtls);
-            String rootDn = baseDN;
             while (answer.hasMoreElements()) {
                 totalResults++;
                 SearchResult sr = (SearchResult) answer.next();
                 String uuid = null, teamName = null, parentName = null, upwardNamePath = null;
                 String fullName = sr.getNameInNamespace();
-//                if (Objects.equals(rootDn, fullName)) {
-//                    continue;
-//                }
                 Attributes Attrs = sr.getAttributes();
                 if (Attrs != null) {
                     try {
@@ -164,13 +159,13 @@ public class SyncLdapTeamSchedule extends PublicJobBase {
                     msg = msg + ", attribute " + _uuid + " does not exist";
                     throw new ApiRuntimeException(msg);
                 }
-                if (Objects.equals(rootDn, fullName)) {
+                if (Objects.equals(baseDN, fullName)) {
                     teamName = getTeamName(fullName);
                     parentName = "";
                     upwardNamePath = teamName;
                     baseDnName = teamName;
                 } else {
-                    String pathName = fullName.replace(rootDn, "");
+                    String pathName = fullName.replace(baseDN, "");
                     teamName = getTeamName(pathName);
                     parentName = getParentName(pathName);
                     if (StringUtils.isBlank(parentName)) {
@@ -179,29 +174,17 @@ public class SyncLdapTeamSchedule extends PublicJobBase {
                     upwardNamePath = getUpwardTeamName(pathName, baseDnName);
                 }
 
-
-
-
-//                String[] split = pathName.split(",");
-//                if (split.length > 0) {
-//                    teamName = split[0].split("=")[1];
-//                }
-//                if (split.length > 1) {
-//                    parentName = split[1].split("=")[1];
-//                }
                 uuidMap.put(teamName, uuid);
                 String upwardUuidPath = getUpwardUuidPath(uuidMap, upwardNamePath);
                 TeamVo teamVo = new TeamVo();
                 teamVo.setSource("ldap");
                 teamVo.setUuid(uuid);
                 teamVo.setName(teamName);
-                teamVo.setParentName(parentName);
                 teamVo.setUpwardNamePath(upwardNamePath);
                 teamVo.setUpwardUuidPath(upwardUuidPath);
-//                setUpwardTeamName(teamVo, fullName, rootDn);
-//                setUpwardUuidPath(uuidMap, teamVo);
-                String parentUuid = uuidMap.get(teamVo.getParentName());
-                teamVo.setParentUuid(parentUuid == null ? rootParentUUid : parentUuid);
+                String parentUuid = uuidMap.get(parentName);
+                parentUuid = parentUuid == null ? rootParentUUid : parentUuid;
+                teamVo.setParentUuid(parentUuid);
                 teamVo.setLcd(lcd);
                 this.teamMapper.insertTeamForLdap(teamVo);
             }
@@ -243,7 +226,9 @@ public class SyncLdapTeamSchedule extends PublicJobBase {
 
     private String getUpwardTeamName(String pathName, String baseDnName) {
         List<String> upwardTeamNameList = new ArrayList<>();
-        upwardTeamNameList.add(baseDnName);
+        if (StringUtils.isNotBlank(baseDnName)) {
+            upwardTeamNameList.add(baseDnName);
+        }
         String[] split = pathName.split(",");
         for (int i = split.length - 1; i >= 0; i--) {
             String str = split[i];
@@ -256,30 +241,6 @@ public class SyncLdapTeamSchedule extends PublicJobBase {
         }
         return String.join("/", upwardTeamNameList);
     }
-//    /**
-//     * 计算检索的组 name层级
-//     *
-//     * @param teamVo
-//     * @param fullName
-//     * @param rootDn
-//     */
-//    private static void setUpwardTeamName(TeamVo teamVo, String fullName, String rootDn) {
-//        String handleName = fullName.replace(rootDn, "");
-//        if (StringUtils.isNotBlank(handleName)) {
-//            List<String> upwardTeamNameList = new ArrayList<>();
-//            String[] dnNames = handleName.split(",");
-//            for (String dnName : dnNames) {
-//                if (StringUtils.isNotBlank(dnName)) {
-//                    upwardTeamNameList.add(getTeamName(dnName));
-//                }
-//            }
-//            //补充根
-//            String rootName = getTeamName(rootDn);
-//            upwardTeamNameList.add(rootName);
-//            Collections.reverse(upwardTeamNameList);
-//            teamVo.setUpwardNamePath(String.join("/", upwardTeamNameList));
-//        }
-//    }
 
     private static byte[] parseControls(Control[] controls) throws NamingException {
         byte[] cookie = null;
@@ -293,26 +254,6 @@ public class SyncLdapTeamSchedule extends PublicJobBase {
         }
         return (cookie == null) ? new byte[0] : cookie;
     }
-
-//    /**
-//     * 计算检索的组uuid 层级 path
-//     *
-//     * @param uuidMap
-//     * @param teamVo
-//     */
-//    private static void setUpwardUuidPath(Map<String, String> uuidMap, TeamVo teamVo) {
-//        String namePath = teamVo.getUpwardNamePath();
-//        if (StringUtils.isNotBlank(namePath)) {
-//            String[] namePaths = namePath.split("/");
-//            List<String> upwardTeamUuidList = new ArrayList<>();
-//            for (String name : namePaths) {
-//                if (StringUtils.isNotBlank(name) && uuidMap.containsKey(name)) {
-//                    upwardTeamUuidList.add(uuidMap.get(name));
-//                }
-//            }
-//            teamVo.setUpwardUuidPath(String.join(",", upwardTeamUuidList));
-//        }
-//    }
 
     private String getUpwardUuidPath(Map<String, String> uuidMap, String upwardNamePath) {
         List<String> upwardTeamUuidList = new ArrayList<>();
@@ -342,24 +283,4 @@ public class SyncLdapTeamSchedule extends PublicJobBase {
             return value.toString();
         }
     }
-
-//    private static String getTeamName(String name) {
-//        if (StringUtils.isBlank(name)) {
-//            return "";
-//        }
-//        String groupName = name.split("ou=")[1].split(",")[0];
-//        //去掉顶层结构
-//        if ("groups".equals(groupName)) {
-//            groupName = "";
-//        }
-//        return groupName;
-//    }
-
-//    private static String getParentName(String fullName) {
-//        if (StringUtils.isBlank(fullName)) {
-//            return "";
-//        }
-//        String parentFullName = fullName.substring(fullName.indexOf(",") + 1, fullName.length());
-//        return getTeamName(parentFullName);
-//    }
 }
