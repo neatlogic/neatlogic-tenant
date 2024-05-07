c3db39ef9945b7fa8463270abdbe1c9089aa7b2b
lvzk <897706680@qq.com>
2024-05-07 19:31:52 +0800

    [功能]分组标签管理 #[1123025150246912]IT服务地域管理 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1123025150246912

--- src/main/java/neatlogic/module/tenant/api/teamtag/SearchTeamTagApi.java
+++ src/main/java/neatlogic/module/tenant/api/teamtag/SearchTeamTagApi.java
@@ -15,61 +15,69 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package neatlogic.module.tenant.api.region;
+package neatlogic.module.tenant.api.teamtag;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import neatlogic.framework.auth.core.AuthAction;
+import neatlogic.framework.auth.label.TEAM_MODIFY;
 import neatlogic.framework.common.constvalue.ApiParamType;
-import neatlogic.framework.dao.mapper.region.RegionMapper;
-import neatlogic.framework.dto.TeamVo;
-import neatlogic.framework.dto.region.RegionTeamVo;
+import neatlogic.framework.dao.mapper.teamtag.TeamTagMapper;
+import neatlogic.framework.dto.teamtag.TeamTagVo;
 import neatlogic.framework.restful.annotation.Input;
 import neatlogic.framework.restful.annotation.OperationType;
 import neatlogic.framework.restful.annotation.Param;
 import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
 import neatlogic.framework.util.TableResultUtil;
-import neatlogic.framework.auth.label.REGION_MODIFY;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 
+@AuthAction(action = TEAM_MODIFY.class)
 @Service
-@AuthAction(action = REGION_MODIFY.class)
 @OperationType(type = OperationTypeEnum.SEARCH)
-public class SearchRegionTeamApi extends PrivateApiComponentBase {
+public class SearchTeamTagApi extends PrivateApiComponentBase {
+    
     @Resource
-    RegionMapper regionMapper;
-
+    TeamTagMapper teamTagMapper;
+    
     @Override
     public String getName() {
-        return "nmtar.searchregionteamapi.getname";
+        return "nmtat.searchteamtagapi.getname";
     }
 
-
     @Input({
-            @Param(name = "regionId", type = ApiParamType.LONG, desc = "nmtar.searchregionteamapi.input.param.desc.regionid", isRequired = true, help = "地域id"),
+            @Param(name = "keyword", type = ApiParamType.STRING, desc = "common.keyword", xss = true, help = "名称"),
             @Param(name = "currentPage", type = ApiParamType.INTEGER, desc = "common.currentpage"),
-            @Param(name = "pageSize", type = ApiParamType.INTEGER, desc = "common.pagesize")
+            @Param(name = "pageSize", type = ApiParamType.INTEGER, desc = "common.pagesize"),
+            @Param(name = "defaultValue", type = ApiParamType.JSONARRAY, desc = "common.defaultvalue")
     })
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
-        RegionTeamVo region = JSON.toJavaObject(paramObj, RegionTeamVo.class);
-        List<TeamVo> teamList = new ArrayList<>();
-        int rowNum = regionMapper.searchRegionTeamCount(region);
-        region.setRowNum(rowNum);
-        if (rowNum > 0) {
-            teamList = regionMapper.searchRegionTeam(region);
+        TeamTagVo teamTag = JSON.toJavaObject(paramObj, TeamTagVo.class);
+        JSONArray defaultValue = teamTag.getDefaultValue();
+        List<TeamTagVo> teamTagList = new ArrayList<>();
+        if (CollectionUtils.isNotEmpty(defaultValue)) {
+            List<Long> idList = defaultValue.toJavaList(Long.class);
+            teamTagList = teamTagMapper.getTeamTagListByIdList(idList);
+            return TableResultUtil.getResult(teamTagList);
+        } else {
+            int rowNum = teamTagMapper.searchTeamTagCount(teamTag);
+            teamTag.setRowNum(rowNum);
+            if (rowNum > 0) {
+                teamTagList = teamTagMapper.searchTeamTag(teamTag);
+            }
         }
-
-        return TableResultUtil.getResult(teamList, region);
+        return TableResultUtil.getResult(teamTagList, teamTag);
     }
 
     @Override
     public String getToken() {
-        return "/region/team/search";
+        return "team/tag/search";
     }
 }
