4264826320674e550c3ee8557f245e0f571e5b56
lvzk <897706680@qq.com>
2024-06-07 18:58:27 +0800

    [功能]自定义矩阵导入优化 #[1168152036081664]矩阵导入优化 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1168152036081664

--- src/main/java/neatlogic/module/tenant/api/matrix/MatrixImportApi.java
+++ src/main/java/neatlogic/module/tenant/api/matrix/MatrixImportApi.java
@@ -15,21 +15,27 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 
 package neatlogic.module.tenant.api.matrix;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import neatlogic.framework.auth.core.AuthAction;
+import neatlogic.framework.auth.label.MATRIX_MODIFY;
 import neatlogic.framework.common.constvalue.ApiParamType;
 import neatlogic.framework.matrix.core.IMatrixDataSourceHandler;
 import neatlogic.framework.matrix.core.MatrixDataSourceHandlerFactory;
 import neatlogic.framework.matrix.dao.mapper.MatrixMapper;
 import neatlogic.framework.matrix.dto.MatrixVo;
-import neatlogic.framework.matrix.exception.*;
+import neatlogic.framework.matrix.exception.MatrixDataSourceHandlerNotFoundException;
+import neatlogic.framework.matrix.exception.MatrixFileNotFoundException;
+import neatlogic.framework.matrix.exception.MatrixNotFoundException;
 import neatlogic.framework.restful.annotation.Description;
 import neatlogic.framework.restful.annotation.Input;
 import neatlogic.framework.restful.annotation.OperationType;
 import neatlogic.framework.restful.annotation.Param;
 import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateBinaryStreamApiComponentBase;
-import neatlogic.framework.auth.label.MATRIX_MODIFY;
-import com.alibaba.fastjson.JSONObject;
+import neatlogic.framework.util.$;
+import org.apache.commons.collections4.MapUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -81,6 +87,8 @@ public class MatrixImportApi extends PrivateBinaryStreamApiComponentBase {
         }
         JSONObject returnObj = new JSONObject();
         int update = 0, insert = 0, unExist = 0;
+        JSONObject invalidDataMap = new JSONObject();
+        JSONArray failureReasonList = new JSONArray();
         MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
         //获取所有导入文件
         Map<String, MultipartFile> multipartFileMap = multipartRequest.getFileMap();
@@ -88,7 +96,8 @@ public class MatrixImportApi extends PrivateBinaryStreamApiComponentBase {
             throw new MatrixFileNotFoundException();
         }
         for (Map.Entry<String, MultipartFile> entry : multipartFileMap.entrySet()) {
-            JSONObject resultObj = matrixDataSourceHandler.importMatrix(matrixVo, entry.getValue());
+            MultipartFile multipartFile = entry.getValue();
+            JSONObject resultObj = matrixDataSourceHandler.importMatrix(matrixVo, multipartFile);
             Integer insertCount = resultObj.getInteger("insert");
             if (insertCount != null) {
                 insert += insertCount;
@@ -101,132 +110,29 @@ public class MatrixImportApi extends PrivateBinaryStreamApiComponentBase {
             if (unExistCount != null) {
                 unExist += unExistCount;
             }
+            JSONObject invalidData = resultObj.getJSONObject("invalidDataMap");
+            invalidDataMap.putAll(invalidData);
+            if (MapUtils.isNotEmpty(invalidDataMap)) {
+                JSONObject failureReason = new JSONObject();
+                JSONArray failureList = new JSONArray();
+                failureReasonList.add(failureReason);
+                failureReason.put("list",failureList);
+                failureReason.put("item",$.t("nmtam.matriximportapi.mydoservice.filevalidtip",multipartFile.getOriginalFilename()));
+                for (Map.Entry<String, Object> entryInvalid : invalidDataMap.entrySet()) {
+                    int rowIndex = Integer.parseInt(entryInvalid.getKey());
+                    JSONObject col = JSON.parseObject(JSON.toJSONString(entryInvalid.getValue()));
+                    for (Map.Entry<String, Object> colEntry : col.entrySet()) {
+                        int colIndex = Integer.parseInt(colEntry.getKey());
+                        Object value = colEntry.getValue();
+                        failureList.add($.t("nmtam.matriximportapi.mydoservice.validtip", rowIndex,colIndex,value == null ? "" : value));
+                    }
+                }
+            }
         }
+        returnObj.put("failureReasonList", failureReasonList);
         returnObj.put("insert", insert);
         returnObj.put("update", update);
         returnObj.put("unExist", unExist);
         return returnObj;
-//        if (MatrixType.CUSTOM.getValue().equals(matrixVo.getType())) {
-//            JSONObject returnObj = new JSONObject();
-//            int update = 0, insert = 0, unExist = 0;
-//            MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
-//            //获取所有导入文件
-//            Map<String, MultipartFile> multipartFileMap = multipartRequest.getFileMap();
-//            if (multipartFileMap.isEmpty()) {
-//                throw new MatrixFileNotFoundException();
-//            }
-//            MultipartFile multipartFile;
-//            InputStream is;
-//            for (Map.Entry<String, MultipartFile> entry : multipartFileMap.entrySet()) {
-//                multipartFile = entry.getValue();
-//                is = multipartFile.getInputStream();
-//                String originalFilename = multipartFile.getOriginalFilename();
-//                if (StringUtils.isNotBlank(originalFilename) && originalFilename.contains(".")) {
-//                    originalFilename = originalFilename.substring(0, originalFilename.indexOf("."));
-//                }
-//                if (StringUtils.isNotBlank(originalFilename) && !originalFilename.equals(matrixVo.getName())) {
-//                    throw new MatrixNameDifferentImportFileNameException();
-//                }
-//
-//                List<MatrixAttributeVo> attributeVoList = attributeMapper.getMatrixAttributeByMatrixUuid(matrixVo.getUuid());
-//                if (CollectionUtils.isNotEmpty(attributeVoList)) {
-//                    Map<String, MatrixAttributeVo> headerMap = new HashMap<>();
-//                    for (MatrixAttributeVo attributeVo : attributeVoList) {
-//                        headerMap.put(attributeVo.getName(), attributeVo);
-//                    }
-//                    Workbook wb = WorkbookFactory.create(is);
-//                    Sheet sheet = wb.getSheetAt(0);
-//                    int rowNum = sheet.getLastRowNum();
-//                    //获取头栏位
-//                    Row headerRow = sheet.getRow(0);
-//                    int colNum = headerRow.getLastCellNum();
-//                    //attributeList 缺少uuid
-//                    if (colNum != attributeVoList.size() + 1) {
-//                        throw new MatrixHeaderMisMatchException(originalFilename);
-//                    }
-//                    //解析数据
-//                    for (int i = 1; i <= rowNum; i++) {
-//                        Row row = sheet.getRow(i);
-//                        boolean isNew = false;
-//                        MatrixColumnVo uuidColumn = null;
-//                        List<MatrixColumnVo> rowData = new ArrayList<>();
-//                        for (int j = 0; j < colNum; j++) {
-//                            Cell tbodycell = row.getCell(j);
-//                            String value = getCellValue(tbodycell);
-//                            String attributeUuid;
-//                            Cell theadCell = headerRow.getCell(j);
-//                            String columnName = theadCell.getStringCellValue();
-//                            if (("uuid").equals(columnName)) {
-//                                attributeUuid = "uuid";
-//                                if (StringUtils.isBlank(value) || dataMapper.getDynamicTableDataCountByUuid(value, matrixVo.getUuid(), TenantContext.get().getDataDbName()) == 0) {
-//                                    value = UuidUtil.randomUuid();
-//                                    isNew = true;
-//                                    rowData.add(new MatrixColumnVo(attributeUuid, value));
-//                                } else {
-//                                    uuidColumn = new MatrixColumnVo(attributeUuid, value);
-//                                }
-//                            } else {
-//                                MatrixAttributeVo attributeVo = headerMap.get(columnName);
-//                                if (attributeVo != null) {
-//                                    attributeUuid = attributeVo.getUuid();
-//                                    if (StringUtils.isNotBlank(attributeUuid)) {
-//                                        if (matrixService.matrixAttributeValueVerify(attributeVo, value)) {
-//                                            rowData.add(new MatrixColumnVo(attributeUuid, value));
-//                                        } else {
-//                                            throw new MatrixImportDataIllegalException(i + 1, j + 1, value);
-//                                        }
-//                                    }
-//                                }
-//                            }
-//                        }
-//                        if (isNew) {
-//                            dataMapper.insertDynamicTableData(rowData, matrixUuid, TenantContext.get().getDataDbName());
-//                            insert++;
-//                            update++;
-//                        } else {
-//                            dataMapper.updateDynamicTableDataByUuid(rowData, uuidColumn, matrixUuid, TenantContext.get().getDataDbName());
-//                        }
-//                    }
-//                } else {
-//                    throw new MatrixDataNotFoundException(originalFilename);
-//                }
-//            }
-//            returnObj.put("insert", insert);
-//            returnObj.put("update", update);
-//            returnObj.put("unExist", unExist);
-//            return returnObj;
-//        } else if (MatrixType.EXTERNAL.getValue().equals(matrixVo.getType())) {
-//            throw new MatrixExternalImportException();
-//        } else if (MatrixType.VIEW.getValue().equals(matrixVo.getType())) {
-//            throw new MatrixViewImportException();
-//        }
-//        return null;
     }
-
-//    private String getCellValue(Cell cell) {
-//        String value = "";
-//        if (cell != null) {
-//            if (cell.getCellType() != CellType.BLANK) {
-//                switch (cell.getCellType()) {
-//                    case NUMERIC:
-//                        if (DateUtil.isCellDateFormatted(cell)) {
-//                            value = formatter.format(cell.getDateCellValue());
-//                        } else {
-//                            value = String.valueOf(cell.getNumericCellValue());
-//                        }
-//                        break;
-//                    case BOOLEAN:
-//                        value = String.valueOf(cell.getBooleanCellValue());
-//                        break;
-//                    case FORMULA:
-//                        value = cell.getCellFormula();
-//                        break;
-//                    default:
-//                        value = cell.getStringCellValue();
-//                        break;
-//                }
-//            }
-//        }
-//        return value;
-//    }
 }
