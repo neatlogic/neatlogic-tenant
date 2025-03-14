3c0b065f9874d57ef5717af9d3ab417dc7953108
chenqw <gdnpc@163.com>
2025-03-14 16:55:27 +0800

    -[修改]邮件配置支持空用户和密码
    Signed-off-by: chenqw <gdnpc@163.com>

--- src/main/java/neatlogic/module/tenant/api/mailserver/MailServerSaveApi.java
+++ src/main/java/neatlogic/module/tenant/api/mailserver/MailServerSaveApi.java
@@ -15,6 +15,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 
 package neatlogic.module.tenant.api.mailserver;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import neatlogic.framework.auth.core.AuthAction;
 import neatlogic.framework.auth.label.NOTIFY_CONFIG_MODIFY;
 import neatlogic.framework.common.constvalue.ApiParamType;
@@ -25,7 +27,6 @@ import neatlogic.framework.restful.annotation.*;
 import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
 import neatlogic.framework.util.RegexUtils;
-import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -37,42 +38,42 @@ import javax.annotation.Resource;
 @OperationType(type = OperationTypeEnum.CREATE)
 public class MailServerSaveApi extends PrivateApiComponentBase {
 
-	@Resource
-	private NotifyConfigMapper notifyConfigMapper;
-	
-	@Override
-	public String getToken() {
-		return "mailserver/save";
-	}
+    @Resource
+    private NotifyConfigMapper notifyConfigMapper;
 
-	@Override
-	public String getName() {
-		return "nmtam.mailserversaveapi.getname";
-	}
+    @Override
+    public String getToken() {
+        return "mailserver/save";
+    }
 
-	@Override
-	public String getConfig() {
-		return null;
-	}
+    @Override
+    public String getName() {
+        return "nmtam.mailserversaveapi.getname";
+    }
 
-	@Input({
-			@Param(name = "uuid", type = ApiParamType.STRING, desc = "common.uuid"),
-			@Param(name = "name", type = ApiParamType.REGEX, rule = RegexUtils.NAME, isRequired= true, maxLength = 50, desc = "common.name"),
-			@Param(name = "host", type = ApiParamType.STRING, isRequired = true,  maxLength = 50, desc = "term.framework.smpthost"),
-			@Param(name = "port", type = ApiParamType.INTEGER, isRequired = true, desc = "term.framework.smptport"),
-			@Param(name = "userName", type = ApiParamType.STRING, isRequired = true,  maxLength = 50, desc = "common.username"),
-			@Param(name = "password", type = ApiParamType.STRING, isRequired = true,  maxLength = 50, desc = "common.password"),
-			@Param(name = "domain", type = ApiParamType.STRING,  maxLength = 50, desc = "term.framework.domain"),
-			@Param(name = "fromAddress", type = ApiParamType.STRING, isRequired = true,  maxLength = 50, desc = "common.mailaddress"),
-			@Param(name = "sslEnable", type = ApiParamType.ENUM, rule = "true,false", isRequired = true, maxLength = 50, desc = "term.framework.smptsslenable")
-	})
-	@Output({})
-	@Description(desc = "nmtam.mailserversaveapi.getname")
-	@Override
-	public Object myDoService(JSONObject jsonObj) throws Exception {
-		MailServerVo mailServerVo = jsonObj.toJavaObject(MailServerVo.class);
-		notifyConfigMapper.insertNotifyConfig(NotifyHandlerType.EMAIL.getValue(), JSONObject.toJSONString(mailServerVo));
-		return null;
-	}
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Input({
+            @Param(name = "uuid", type = ApiParamType.STRING, desc = "common.uuid"),
+            @Param(name = "name", type = ApiParamType.REGEX, rule = RegexUtils.NAME, isRequired = true, maxLength = 50, desc = "common.name"),
+            @Param(name = "host", type = ApiParamType.STRING, isRequired = true, maxLength = 50, desc = "term.framework.smpthost"),
+            @Param(name = "port", type = ApiParamType.INTEGER, isRequired = true, desc = "term.framework.smptport"),
+            @Param(name = "userName", type = ApiParamType.STRING, maxLength = 50, desc = "common.username"),
+            @Param(name = "password", type = ApiParamType.STRING, maxLength = 50, desc = "common.password"),
+            @Param(name = "domain", type = ApiParamType.STRING, maxLength = 50, desc = "term.framework.domain"),
+            @Param(name = "fromAddress", type = ApiParamType.STRING, isRequired = true, maxLength = 50, desc = "common.mailaddress"),
+            @Param(name = "sslEnable", type = ApiParamType.ENUM, rule = "true,false", isRequired = true, maxLength = 50, desc = "term.framework.smptsslenable")
+    })
+    @Output({})
+    @Description(desc = "nmtam.mailserversaveapi.getname")
+    @Override
+    public Object myDoService(JSONObject jsonObj) throws Exception {
+        MailServerVo mailServerVo = jsonObj.toJavaObject(MailServerVo.class);
+        notifyConfigMapper.insertNotifyConfig(NotifyHandlerType.EMAIL.getValue(), JSON.toJSONString(mailServerVo));
+        return null;
+    }
 
 }
