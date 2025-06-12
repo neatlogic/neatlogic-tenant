105e473b5a4b9710832a60be81396d6ba1f1b4b4
1437892690@qq.com <1437892690@qq.com>
2025-06-12 18:03:45 +0800

    [修复] 数据仓库第三方数据源导致java.lang.OutOfMemoryError: Metaspace
    关联 #[1442121129623552]数据仓库第三方数据源导致java.lang.OutOfMemoryError: Metaspace http://192.168.0.96:8090/demo/rdm.html#/bug-detail/939050947543040/939050947543057/1442121129623552

--- src/main/java/neatlogic/module/tenant/api/database/TestDataBaseApi.java
+++ src/main/java/neatlogic/module/tenant/api/database/TestDataBaseApi.java
@@ -21,16 +21,25 @@ import com.alibaba.fastjson.JSONObject;
 import neatlogic.framework.auth.core.AuthAction;
 import neatlogic.framework.auth.label.DATA_WAREHOUSE_MODIFY;
 import neatlogic.framework.common.constvalue.ApiParamType;
+import neatlogic.framework.datawarehouse.dao.mapper.DatabaseMapper;
 import neatlogic.framework.datawarehouse.dto.DatabaseVo;
-import neatlogic.framework.datawarehouse.service.DatabaseService;
+import neatlogic.framework.datawarehouse.exceptions.DatabaseConnectionFailedException;
+import neatlogic.framework.datawarehouse.exceptions.DatabaseNotFoundException;
+import neatlogic.framework.datawarehouse.utils.DriverHolder;
+import neatlogic.framework.file.dao.mapper.FileMapper;
+import neatlogic.framework.file.dto.FileVo;
 import neatlogic.framework.restful.annotation.*;
 import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.net.URLClassLoader;
-import java.sql.Connection;
+import java.sql.Driver;
+import java.util.List;
+import java.util.Properties;
 
 @Service
 @AuthAction(action = DATA_WAREHOUSE_MODIFY.class)
@@ -38,7 +47,10 @@ import java.sql.Connection;
 public class TestDataBaseApi extends PrivateApiComponentBase {
 
     @Resource
-    private DatabaseService databaseService;
+    private DatabaseMapper databaseMapper;
+
+    @Resource
+    private FileMapper fileMapper;
 
     @Override
     public String getName() {
@@ -55,15 +67,34 @@ public class TestDataBaseApi extends PrivateApiComponentBase {
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
         Long id = paramObj.getLong("id");
-        Connection conn = databaseService.getConnectionByDatabaseId(id);
-        if (conn != null) {
-            ClassLoader classLoader = conn.getClass().getClassLoader();
-            conn.close();
-            if (classLoader instanceof URLClassLoader) {
-                ((URLClassLoader) classLoader).close();
-            }
+        DatabaseVo databaseVo = databaseMapper.getDataBaseById(id);
+        if (databaseVo == null) {
+            throw new DatabaseNotFoundException(id);
         }
-        return null;
+        List<Long> fileIdList = databaseVo.getFileIdList();
+        if (CollectionUtils.isNotEmpty(fileIdList)) {
+            List<FileVo> fileList = fileMapper.getFileListByIdList(fileIdList);
+            databaseVo.setFileList(fileList);
+        } else {
+            throw new DatabaseConnectionFailedException(DatabaseConnectionFailedException.Type.FILE_ID_LIST_IS_EMPTY, databaseVo.getName());
+        }
+        Driver driver = DriverHolder.borrowDriver(databaseVo);
+        JSONObject config = databaseVo.getConfig();
+        if (MapUtils.isNotEmpty(config)) {
+            String user = config.getString("user");
+            String password = config.getString("password");
+            String url = config.getString("url");
+            Properties props = new Properties();
+            if (StringUtils.isNoneBlank(user)) {
+                props.put("user", user);
+            }
+            if (StringUtils.isNotBlank(password)) {
+                props.put("password", password);
+            }
+            return driver.connect(url, props);
+        } else {
+            throw new DatabaseConnectionFailedException(DatabaseConnectionFailedException.Type.CONFIG_IS_EMPTY, databaseVo.getName());
+        }
     }
 
     @Override
