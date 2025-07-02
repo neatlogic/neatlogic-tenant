16c7c14dd25355abf7d8eb31049dbcba5da30f81
lvzk <897706680@qq.com>
2025-07-02 15:26:32 +0800

    [功能]定时任务里面添加测试按钮 #[1452246112370688]定时任务里面添加测试按钮 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1452246112370688

--- src/main/java/neatlogic/module/tenant/api/scheduler/JobTestApi.java
+++ src/main/java/neatlogic/module/tenant/api/scheduler/JobTestApi.java
@@ -22,17 +22,20 @@ import neatlogic.framework.asynchronization.threadlocal.TenantContext;
 import neatlogic.framework.auth.core.AuthAction;
 import neatlogic.framework.auth.label.SCHEDULE_JOB_MODIFY;
 import neatlogic.framework.common.constvalue.ApiParamType;
+import neatlogic.framework.exception.core.ApiRuntimeException;
 import neatlogic.framework.restful.annotation.Input;
 import neatlogic.framework.restful.annotation.Param;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
 import neatlogic.framework.scheduler.core.IJob;
 import neatlogic.framework.scheduler.core.SchedulerManager;
 import neatlogic.framework.scheduler.dto.JobObject;
+import neatlogic.framework.scheduler.dto.JobVo;
 import neatlogic.framework.scheduler.exception.ScheduleHandlerNotFoundException;
-import org.apache.commons.lang3.StringUtils;
+import neatlogic.framework.scheduler.exception.ScheduleJobNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Objects;
 
 @AuthAction(action = SCHEDULE_JOB_MODIFY.class)
 @Service
@@ -46,28 +49,31 @@ public class JobTestApi extends PrivateApiComponentBase {
     }
 
     @Input({
-            @Param(name = "jobId", type = ApiParamType.STRING, desc = "nmtas.jobtestapi.input.param.desc.jobid", isRequired = true),
-            @Param(name = "jobGroup", type = ApiParamType.STRING, desc = "nmtas.jobtestapi.input.param.desc.jobgroup", isRequired = true),
-            @Param(name = "jobHandlerClassName", type = ApiParamType.STRING, desc = "nmtas.jobtestapi.input.param.desc.jobhandlerclassname", isRequired = true),
-            @Param(name = "type", type = ApiParamType.STRING, rule = "private,public", desc = "nmtas.jobtestapi.input.param.desc.type"),
+            @Param(name = "jobUuid", type = ApiParamType.STRING, desc = "nmtas.jobtestapi.input.param.desc.jobid", isRequired = true),
+            @Param(name = "jobHandlerClassName", type = ApiParamType.STRING, desc = "nmtas.jobtestapi.input.param.desc.jobhandlerclassname", isRequired = true)
     })
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
-        String jobId = paramObj.getString("jobId");
-        String jobGroup = paramObj.getString("jobGroup");
+        String jobUuid = paramObj.getString("jobUuid");
         String jobHandlerClassName = paramObj.getString("jobHandlerClassName");
-        String type = "private";
-        if(StringUtils.isNotBlank(paramObj.getString("type"))) {
-            type = paramObj.getString("type");
-        }
         IJob jobHandler = SchedulerManager.getHandler(jobHandlerClassName);
         if (jobHandler == null) {
             throw new ScheduleHandlerNotFoundException(jobHandlerClassName);
         }
         String tenantUuid = TenantContext.get().getTenantUuid();
-        JobObject jobObject = new JobObject.Builder("TEST@" + jobId + "@TEST", jobGroup, jobHandlerClassName, tenantUuid).withRepeatCount(1)
-                .setType(type)
-                .build();
+        JobVo jobVo = jobHandler.getJob(jobUuid);
+        if (jobVo == null) {
+            throw new ScheduleJobNotFoundException(jobUuid);
+        }
+        if (Objects.equals(jobVo.getIsActive(), 1)) {
+            throw new ApiRuntimeException("状态为‘禁用’，才能执行测试");
+        }
+        JobObject jobObject = new JobObject.Builder(jobVo.getUuid(), jobHandler.getGroupName(), jobHandler.getClassName(), tenantUuid)
+                .withRepeatCount(1)
+                .needAudit(1)
+                .withPropList(jobVo.getPropList())
+                .setIsTest(1)
+                .setType(jobHandler.getType()).build();
         schedulerManager.loadJob(jobObject);
         return null;
     }
