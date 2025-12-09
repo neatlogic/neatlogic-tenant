b4afe104ac5dc7e2c5600abfcaca659542409c38
linbangquan <1437892690@qq.com>
2025-12-09 03:13:31 +0000

    !113 [功能] 通知配置管理优化 * [功能] 通知配置管理优化

--- src/main/java/neatlogic/module/tenant/api/wechat/TestWechatApi.java
+++ src/main/java/neatlogic/module/tenant/api/wechat/TestWechatApi.java
@@ -16,26 +16,20 @@ import com.alibaba.fastjson.JSONObject;
 import neatlogic.framework.auth.core.AuthAction;
 import neatlogic.framework.auth.label.NOTIFY_CONFIG_MODIFY;
 import neatlogic.framework.common.constvalue.ApiParamType;
-import neatlogic.framework.dao.mapper.NotifyConfigMapper;
 import neatlogic.framework.dto.WechatVo;
-import neatlogic.framework.exception.wechat.WechatAuthenticationInformationNotFoundException;
-import neatlogic.framework.notify.core.NotifyHandlerType;
 import neatlogic.framework.restful.annotation.*;
 import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
 import neatlogic.framework.util.WechatUtil;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
 
 @Component
 @AuthAction(action = NOTIFY_CONFIG_MODIFY.class)
 @OperationType(type = OperationTypeEnum.SEARCH)
 public class TestWechatApi extends PrivateApiComponentBase {
 
-    @Resource
-    private NotifyConfigMapper notifyConfigMapper;
+//    @Resource
+//    private NotifyConfigMapper notifyConfigMapper;
 
     @Override
     public String getName() {
@@ -43,17 +37,21 @@ public class TestWechatApi extends PrivateApiComponentBase {
     }
 
     @Input({
+            @Param(name = "corpId", type = ApiParamType.STRING, isRequired = true, desc = "term.framework.corpid"),
+            @Param(name = "corpSecret", type = ApiParamType.STRING, isRequired = true, desc = "term.framework.corpsecret"),
+            @Param(name = "agentId", type = ApiParamType.STRING, isRequired = true, desc = "term.framework.agentid"),
             @Param(name = "toUser", type = ApiParamType.STRING, isRequired = true, desc = "nmtaw.testwechatapi.input.param.desc.touser")
     })
     @Output({})
     @Description(desc = "nmtaw.testwechatapi.getname")
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
-        String config = notifyConfigMapper.getConfigByType(NotifyHandlerType.WECHAT.getValue());
-        if (StringUtils.isBlank(config)) {
-            throw new WechatAuthenticationInformationNotFoundException();
-        }
-        WechatVo wechatVo = JSONObject.parseObject(config, WechatVo.class);
+//        String config = notifyConfigMapper.getConfigByType(NotifyHandlerType.WECHAT.getValue());
+//        if (StringUtils.isBlank(config)) {
+//            throw new WechatAuthenticationInformationNotFoundException();
+//        }
+//        WechatVo wechatVo = JSONObject.parseObject(config, WechatVo.class);
+        WechatVo wechatVo = paramObj.toJavaObject(WechatVo.class);
         WechatUtil.AccessToken accessToken = WechatUtil.getAccessToken(wechatVo.getCorpId(), wechatVo.getCorpSecret());
         String toUser = paramObj.getString("toUser");
         JSONObject data = WechatUtil.getTextCardMsg(
