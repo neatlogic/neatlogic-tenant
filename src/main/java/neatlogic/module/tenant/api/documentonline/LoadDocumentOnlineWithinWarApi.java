c956253abbf40e87cdadc2e07eac800b9e9b7073
1437892690@qq.com <1437892690@qq.com>
2025-12-12 18:59:39 +0800

    [功能] 帮助文档增加导入功能
    关联 #[1543410811371520]帮助文档增加导入功能 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1543410811371520

---
+++ src/main/java/neatlogic/module/tenant/api/documentonline/LoadDocumentOnlineWithinWarApi.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2025  TechSure Co., Ltd.  All Rights Reserved.
+ * This file is part of the NeatLogic software.
+ * Licensed under the NeatLogic Sustainable Use License (NSUL), Version 4.x – 2025.
+ * You may use this file only in compliance with the License.
+ * See the LICENSE file distributed with this work for the full license text.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+package neatlogic.module.tenant.api.documentonline;
+
+import com.alibaba.fastjson.JSONObject;
+import neatlogic.framework.auth.core.AuthAction;
+import neatlogic.framework.auth.label.DOCUMENTONLINE_CONFIG_MODIFY;
+import neatlogic.framework.documentonline.util.DocumentOnlineManager;
+import neatlogic.framework.restful.annotation.*;
+import neatlogic.framework.restful.constvalue.OperationTypeEnum;
+import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
+import org.springframework.stereotype.Service;
+
+@Service
+@AuthAction(action = DOCUMENTONLINE_CONFIG_MODIFY.class)
+@OperationType(type = OperationTypeEnum.UPDATE)
+public class LoadDocumentOnlineWithinWarApi extends PrivateApiComponentBase {
+
+    @Override
+    public String getName() {
+        return "nmtad.loaddocumentonlinewithinwarapi.getname";
+    }
+
+    @Input({})
+    @Output({})
+    @Description(desc = "nmtad.loaddocumentonlinewithinwarapi.getname")
+    @Override
+    public Object myDoService(JSONObject paramObj) throws Exception {
+        return DocumentOnlineManager.LoadDocumentsWithinWar();
+    }
+
+
+    @Override
+    public String getToken() {
+        return "documentonline/withinwar/load";
+    }
+}
