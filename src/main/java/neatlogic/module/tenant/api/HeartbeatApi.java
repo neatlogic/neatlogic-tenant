1130cd40c003b6ef4b898d92c527c346e0ea0bdc
lvzk <897706680@qq.com>
2025-12-23 14:36:03 +0800

    [功能]登录管理&密码重置 #[1577600328105991]登录管理&密码重置 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1577600328105991

---
+++ src/main/java/neatlogic/module/tenant/api/HeartbeatApi.java
@@ -0,0 +1,62 @@
+/*
+ *
+ * Copyright (C) 2025  TechSure Co., Ltd.  All Rights Reserved.
+ * This file is part of the NeatLogic software.
+ * Licensed under the NeatLogic Sustainable Use License (NSUL), Version 4.x – 2025.
+ * You may use this file only in compliance with the License.
+ * See the LICENSE file distributed with this work for the full license text.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ */
+
+package neatlogic.module.tenant.api;
+
+import com.alibaba.fastjson.JSONObject;
+import neatlogic.framework.auth.core.AuthAction;
+import neatlogic.framework.auth.label.NoAuth;
+import neatlogic.framework.common.constvalue.ApiParamType;
+import neatlogic.framework.dao.mapper.UserSessionMapper;
+import neatlogic.framework.restful.annotation.Description;
+import neatlogic.framework.restful.annotation.Input;
+import neatlogic.framework.restful.annotation.OperationType;
+import neatlogic.framework.restful.annotation.Param;
+import neatlogic.framework.restful.constvalue.OperationTypeEnum;
+import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+@AuthAction(action = NoAuth.class)
+@OperationType(type = OperationTypeEnum.UPDATE)
+public class HeartbeatApi extends PrivateApiComponentBase {
+    @Resource
+    private UserSessionMapper userSessionMapper;
+
+    @Override
+    public String getName() {
+        return "nmta.heartbeatapi.getname";
+    }
+
+    @Override
+    public String getToken() {
+        return "heartbeat";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Input({
+            @Param(name = "tokenHash", type = ApiParamType.STRING, isRequired = true, desc = "tokenHash")
+    })
+    @Description(desc = "nmta.heartbeatapi.getname")
+    @Override
+    public Object myDoService(JSONObject paramObj) throws Exception {
+        String tokenHash = paramObj.getString("tokenHash");
+        userSessionMapper.updateUserSession(tokenHash);
+        return null;
+    }
+}
