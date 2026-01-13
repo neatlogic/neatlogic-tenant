4df9845cff727a60f7c99ce96a4790df9514d0f4
lvzk <897706680@qq.com>
2026-01-13 10:27:07 +0800

    [功能]新增接口支持 basic 认证

--- src/main/java/neatlogic/module/tenant/api/apimanage/ApiManageGetApi.java
+++ src/main/java/neatlogic/module/tenant/api/apimanage/ApiManageGetApi.java
@@ -60,12 +60,15 @@ public class ApiManageGetApi extends PrivateApiComponentBase {
 	public Object myDoService(JSONObject jsonObj) throws Exception {
 		String token = jsonObj.getString("token");
 		ApiVo apiVo = ApiMapper.getApiByToken(token);
+        ApiVo api = PrivateApiComponentFactory.getApiByToken(token);
 		if(apiVo != null) {
+            if(api != null){
+                apiVo.setBasicSupport(api.getBasicSupport());
+            }
 			return apiVo;
 		}
-		apiVo = PrivateApiComponentFactory.getApiByToken(token);
-		if(apiVo != null) {
-			return apiVo;
+		if(api != null) {
+			return api;
 		}
 		throw new ApiNotFoundException(token);
 	}
