a62e67993d2cb396d1187dfd4011ac1b1875eeb0
1437892690@qq.com <1437892690@qq.com>
2026-03-20 17:48:27 +0800

    [功能] 矩阵属性数据查询-下拉接口优化
    关联 #[1645760953974787]矩阵属性数据查询-下拉接口优化 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1645760953974787

--- src/main/java/neatlogic/module/tenant/api/matrix/MatrixColumnDataSearchForSelectApi.java
+++ src/main/java/neatlogic/module/tenant/api/matrix/MatrixColumnDataSearchForSelectApi.java
@@ -34,13 +34,11 @@ import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
 import neatlogic.module.tenant.service.MatrixService;
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Service
 @AuthAction(action = NoAuth.class)
@@ -143,42 +141,6 @@ public class MatrixColumnDataSearchForSelectApi extends PrivateApiComponentBase 
             @Param(explode = BasePageVo.class)
     })
     @Description(desc = "矩阵属性数据查询-下拉级联接口")
-//    @Example(example = "" +
-//            "{" +
-//            "\"matrixUuid(矩阵uuid，必填)\": \"825e6ba09050406eb0de8c4bdcd4e27c\"," +
-//            "\"columnList(需要返回数据的字段列表，必填)\": [" +
-//            "\"92196814d8da4ad9bed63e1d650d7e98\"," +
-//            "\"a4e9978fd06d46d78b13f947a2b1b188\"," +
-//            "\"cf2c2677c18540a79e60cfd9d531b50c\"," +
-//            "\"b5d685e9e5fb4ce0baa0604de812e93b\"," +
-//            "\"6e4abb9b532b49139cec798a3828c7cd\"," +
-//            "\"3d2f5475138744938f0bb1da1f82002c\"," +
-//            "\"96d449a58b664a31b32bb1c28090aeee\"" +
-//            "]," +
-//            "\"searchColumnList(可搜索字段列表，选填)\": [" +
-//            "\"92196814d8da4ad9bed63e1d650d7e98\"," +
-//            "\"a4e9978fd06d46d78b13f947a2b1b188\"" +
-//            "\t]," +
-//            "\"currentPage\": 1," +
-//            "\"pageSize\": 10," +
-//            "\"sourceColumnList(过滤条件列表，选填)\": [" +
-//            "{" +
-//            "\"column(字段uuid，必填)\": \"a4e9978fd06d46d78b13f947a2b1b188\"," +
-//            "\"expression(过滤表达式，必填)\": \"like|notlike|equal|unequal|include|exclude|between|greater-than|less-than|is-null|match|is-not-null\"," +
-//            "\"valueList(过滤值列表，必填)\": [" +
-//            "\"1\"" +
-//            "]" +
-//            "}" +
-//            "]," +
-//            "\"filterList(联动过滤条件列表，选填)\": [" +
-//            "{" +
-//            "\"uuid(字段uuid，必填)\": \"92196814d8da4ad9bed63e1d650d7e98\"," +
-//            "\"valueList(过滤值列表，必填)\": [" +
-//            "\"2\"" +
-//            "]" +
-//            "}" +
-//            "]" +
-//            "}")
     @Override
     public Object myDoService(JSONObject jsonObj) throws Exception {
         String keyword = jsonObj.getString("keyword");
