4f8748a12541f6c3351c83e133615c2a4cf986e2
1437892690@qq.com <1437892690@qq.com>
2026-05-18 17:52:20 +0800

    [功能] sql监控增加统计指定接口拦截统计
    关联 #[1460056485232640]sql监控增加统计指定接口拦截统计 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1460056485232640

--- src/main/java/neatlogic/module/tenant/api/healthcheck/SearchSqlAuditApi.java
+++ src/main/java/neatlogic/module/tenant/api/healthcheck/SearchSqlAuditApi.java
@@ -2,7 +2,7 @@
  *
  * Copyright (C) 2025  TechSure Co., Ltd.  All Rights Reserved.
  * This file is part of the NeatLogic software.
- * Licensed under the NeatLogic Sustainable Use License (NSUL), Version 4.x – 2025.
+ * Licensed under the NeatLogic Sustainable Use License (NSUL), Version 4.x - 2025.
  * You may use this file only in compliance with the License.
  * See the LICENSE file distributed with this work for the full license text.
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,9 +19,14 @@ import neatlogic.framework.common.constvalue.ApiParamType;
 import neatlogic.framework.common.dto.BasePageVo;
 import neatlogic.framework.common.util.PageUtil;
 import neatlogic.framework.dao.plugin.SqlCostInterceptor;
+import neatlogic.framework.dto.healthcheck.RequestSqlAuditVo;
 import neatlogic.framework.dto.healthcheck.SqlAuditVo;
 import neatlogic.framework.healthcheck.SqlAuditManager;
-import neatlogic.framework.restful.annotation.*;
+import neatlogic.framework.restful.annotation.Description;
+import neatlogic.framework.restful.annotation.Input;
+import neatlogic.framework.restful.annotation.OperationType;
+import neatlogic.framework.restful.annotation.Output;
+import neatlogic.framework.restful.annotation.Param;
 import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
 import org.apache.commons.lang3.StringUtils;
@@ -30,12 +35,15 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
 @AuthAction(action = ADMIN.class)
 @OperationType(type = OperationTypeEnum.OPERATE)
 public class SearchSqlAuditApi extends PrivateApiComponentBase {
+
+    private static final int PAGE_SIZE = 20;
 
     @Override
     public String getToken() {
@@ -52,68 +60,97 @@ public class SearchSqlAuditApi extends PrivateApiComponentBase {
         return null;
     }
 
-
     @Input({@Param(name = "id", type = ApiParamType.STRING, desc = "sql语句id"),
-            @Param(name = "orderBy", type = ApiParamType.ENUM, rule = "timecost,runtime", desc = "排序，只支持timecost和rumtime"),
-            @Param(name = "currentPage", type = ApiParamType.INTEGER, desc = "当前页")
+            @Param(name = "url", type = ApiParamType.STRING, desc = "URL监控按请求地址过滤"),
+            @Param(name = "orderBy", type = ApiParamType.ENUM, rule = "timecost,runtime", desc = "排序，只支持timecost和runtime"),
+            @Param(name = "currentPage", type = ApiParamType.INTEGER, desc = "SQL ID监控当前页"),
+            @Param(name = "requestCurrentPage", type = ApiParamType.INTEGER, desc = "URL监控当前页")
     })
     @Output({
-            @Param(name = "tbodyList", explode = SqlAuditVo[].class),
-            @Param(explode = BasePageVo.class)
+            @Param(name = "sqlAuditData", explode = BasePageVo.class),
+            @Param(name = "requestSqlAuditData", explode = BasePageVo.class)
     })
     @Description(desc = "获取SQL耗时接口")
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
-        //复制一个新列表，不改变原来的列表
-        List<SqlAuditVo> sqlAuditList = new ArrayList<>(SqlAuditManager.getSqlAuditList());
         String orderBy = StringUtils.isNotBlank(paramObj.getString("orderBy")) ? paramObj.getString("orderBy") : "runtime";
-        String id = paramObj.getString("id");
-        int pageSize = 20;
-        int currentPage = paramObj.getIntValue("currentPage");
+        // SQL ID监控和URL监控分别分页，避免两个Tab切换时互相影响页码
+        JSONObject sqlAuditData = buildSqlAuditData(paramObj.getString("id"), orderBy, paramObj.getIntValue("currentPage"));
+        JSONObject requestSqlAuditData = buildRequestSqlAuditData(paramObj.getString("url"), orderBy, paramObj.getIntValue("requestCurrentPage"));
+        JSONObject returnObj = new JSONObject();
+        returnObj.put("sqlAuditData", sqlAuditData);
+        returnObj.put("requestSqlAuditData", requestSqlAuditData);
+        returnObj.put("maxTimeCost", sqlAuditData.getLongValue("maxTimeCost"));
+        returnObj.put("maxRequestTimeCost", requestSqlAuditData.getLongValue("maxRequestTimeCost"));
+        returnObj.put("sqlIdList", SqlCostInterceptor.SqlIdMap.getSqlIdList());
+        returnObj.put("urlList", SqlCostInterceptor.UrlMap.getUrlList());
+        return returnObj;
+    }
+
+    private JSONObject buildSqlAuditData(String id, String orderBy, int currentPage) {
+        // 复制新列表，避免前端分页和排序影响内存中的原始审计列表
+        List<SqlAuditVo> sqlAuditList = new ArrayList<>(SqlAuditManager.getSqlAuditList());
         if (StringUtils.isNotBlank(id)) {
-            sqlAuditList = sqlAuditList.stream().filter(d -> d.getId().toLowerCase(Locale.ROOT).contains(id.toLowerCase(Locale.ROOT))).collect(Collectors.toList());
+            sqlAuditList = sqlAuditList.stream().filter(d -> StringUtils.isNotBlank(d.getId()) && d.getId().toLowerCase(Locale.ROOT).contains(id.toLowerCase(Locale.ROOT))).collect(Collectors.toList());
         }
-        if (StringUtils.isNotBlank(orderBy)) {
-            if (orderBy.equals("timecost")) {
-                sqlAuditList = sqlAuditList.stream().sorted((o1, o2) -> o2.getTimeCost().compareTo(o1.getTimeCost())).collect(Collectors.toList());
-            } else {
-                sqlAuditList = sqlAuditList.stream().sorted((o1, o2) -> o2.getRunTime().compareTo(o1.getRunTime())).collect(Collectors.toList());
-            }
+        if (Objects.equals(orderBy, "timecost")) {
+            sqlAuditList = sqlAuditList.stream().sorted((o1, o2) -> o2.getTimeCost().compareTo(o1.getTimeCost())).collect(Collectors.toList());
+        } else {
+            sqlAuditList = sqlAuditList.stream().sorted((o1, o2) -> o2.getRunTime().compareTo(o1.getRunTime())).collect(Collectors.toList());
         }
         long maxTimeCost = 0;
         for (SqlAuditVo sqlAuditVo : sqlAuditList) {
-            if (maxTimeCost < sqlAuditVo.getTimeCost()) {
+            if (sqlAuditVo.getTimeCost() != null && maxTimeCost < sqlAuditVo.getTimeCost()) {
                 maxTimeCost = sqlAuditVo.getTimeCost();
             }
         }
-        int rowNum = sqlAuditList.size();
-        int pageCount = PageUtil.getPageCount(rowNum, pageSize);
+        JSONObject data = pageList(sqlAuditList, currentPage);
+        data.put("maxTimeCost", maxTimeCost);
+        return data;
+    }
+
+    private JSONObject buildRequestSqlAuditData(String url, String orderBy, int currentPage) {
+        // URL监控列表按一次HTTP请求一行展示，和SQL ID明细列表完全分开
+        List<RequestSqlAuditVo> requestSqlAuditList = new ArrayList<>(SqlAuditManager.getRequestSqlAuditList());
+        if (StringUtils.isNotBlank(url)) {
+            requestSqlAuditList = requestSqlAuditList.stream().filter(d -> StringUtils.isNotBlank(d.getUrl()) && d.getUrl().toLowerCase(Locale.ROOT).contains(url.toLowerCase(Locale.ROOT))).collect(Collectors.toList());
+        }
+        if (Objects.equals(orderBy, "timecost")) {
+            requestSqlAuditList = requestSqlAuditList.stream().sorted((o1, o2) -> Long.compare(o2.getTotalTimeCost(), o1.getTotalTimeCost())).collect(Collectors.toList());
+        } else {
+            requestSqlAuditList = requestSqlAuditList.stream().sorted((o1, o2) -> o2.getRunTime().compareTo(o1.getRunTime())).collect(Collectors.toList());
+        }
+        long maxRequestTimeCost = 0;
+        for (RequestSqlAuditVo requestSqlAuditVo : requestSqlAuditList) {
+            if (maxRequestTimeCost < requestSqlAuditVo.getTotalTimeCost()) {
+                maxRequestTimeCost = requestSqlAuditVo.getTotalTimeCost();
+            }
+        }
+        JSONObject data = pageList(requestSqlAuditList, currentPage);
+        data.put("maxRequestTimeCost", maxRequestTimeCost);
+        return data;
+    }
+
+    private JSONObject pageList(List<?> list, int currentPage) {
+        int rowNum = list.size();
+        int pageCount = PageUtil.getPageCount(rowNum, PAGE_SIZE);
+        // 空列表也返回第1页，避免前端表格收到currentPage=0
+        pageCount = Math.max(1, pageCount);
         currentPage = Math.max(1, currentPage);
         currentPage = Math.min(currentPage, pageCount);
+        List<?> tbodyList;
         try {
-            //因为数据可能在访问途中被清空掉，所以如果有异常直接返回空的列表
-            sqlAuditList = sqlAuditList.stream().skip((long) (currentPage - 1) * pageSize).limit(pageSize).collect(Collectors.toList());
+            // 审计数据可能在请求途中被清空，分页异常时直接返回空列表
+            tbodyList = list.stream().skip((long) (currentPage - 1) * PAGE_SIZE).limit(PAGE_SIZE).collect(Collectors.toList());
         } catch (Exception ex) {
-            sqlAuditList = new ArrayList<>();
+            tbodyList = new ArrayList<>();
         }
         JSONObject returnObj = new JSONObject();
         returnObj.put("pageCount", pageCount);
         returnObj.put("currentPage", currentPage);
-        returnObj.put("pageSize", pageSize);
+        returnObj.put("pageSize", PAGE_SIZE);
         returnObj.put("rowNum", rowNum);
-        returnObj.put("tbodyList", sqlAuditList);
-        returnObj.put("maxTimeCost", maxTimeCost);//用来给前端计算进度条
-        returnObj.put("sqlIdList", SqlCostInterceptor.SqlIdMap.getSqlIdList());
+        returnObj.put("tbodyList", tbodyList);
         return returnObj;
     }
-
-
-   /* public static void main(String[] a) {
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            list.add(Integer.toString(i));
-        }
-        list = list.stream().skip(1).limit(1).collect(Collectors.toList());
-        System.out.println(String.join(",", list));
-    }*/
 }
--- src/main/java/neatlogic/module/tenant/api/healthcheck/ToggleSqlInterceptorApi.java
+++ src/main/java/neatlogic/module/tenant/api/healthcheck/ToggleSqlInterceptorApi.java
@@ -2,7 +2,7 @@
  *
  * Copyright (C) 2025  TechSure Co., Ltd.  All Rights Reserved.
  * This file is part of the NeatLogic software.
- * Licensed under the NeatLogic Sustainable Use License (NSUL), Version 4.x – 2025.
+ * Licensed under the NeatLogic Sustainable Use License (NSUL), Version 4.x - 2025.
  * You may use this file only in compliance with the License.
  * See the LICENSE file distributed with this work for the full license text.
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
@@ -47,22 +47,40 @@ public class ToggleSqlInterceptorApi extends PrivateApiComponentBase {
         return null;
     }
 
-    @Input({@Param(name = "action", type = ApiParamType.ENUM, isRequired = true, rule = "insert,remove,clear", desc = "insert：激活追踪指定SQL，remove：取消追踪指定SQL，clear：取消追踪所有SQL"),
-            @Param(name = "id", type = ApiParamType.STRING, desc = "mapper配置文件中的sql id")})
-    @Description(desc = "打开指定SQL日志，在标准输出中能查看最终执行SQL和执行时间")
+    @Input({@Param(name = "action", type = ApiParamType.ENUM, isRequired = true, rule = "insert,remove,clear", desc = "insert：激活追踪，remove：取消追踪，clear：取消全部追踪"),
+            @Param(name = "id", type = ApiParamType.STRING, desc = "mapper配置文件中的sql id"),
+            @Param(name = "url", type = ApiParamType.STRING, desc = "需要按HTTP请求统计SQL的URL，*代表全部URL")})
+    @Description(desc = "打开指定SQL或URL监控，统计最终执行SQL和执行时间")
     @Override
     public Object myDoService(JSONObject jsonObj) throws Exception {
         String action = jsonObj.getString("action");
         String id = jsonObj.getString("id");
+        String url = jsonObj.getString("url");
         if (StringUtils.isNotBlank(action)) {
             if (action.equalsIgnoreCase("clear")) {
+                // 清空时同时清理sqlId和URL两类监控配置及两类审计数据
                 SqlCostInterceptor.SqlIdMap.clear();
+                SqlCostInterceptor.UrlMap.clear();
                 SqlAuditManager.clearSqlAudit();
-            } else if (action.equalsIgnoreCase("insert") && StringUtils.isNotBlank(id)) {
-                SqlCostInterceptor.SqlIdMap.addId(id);
-            } else if (action.equalsIgnoreCase("remove") && StringUtils.isNotBlank(id)) {
-                SqlCostInterceptor.SqlIdMap.removeId(id);
-                SqlAuditManager.removeSqlAudit(id);
+                SqlAuditManager.clearRequestSqlAudit();
+            } else if (action.equalsIgnoreCase("insert")) {
+                // 添加监控时允许sql id和url同时填写，分别进入不同监控集合
+                if (StringUtils.isNotBlank(id)) {
+                    SqlCostInterceptor.SqlIdMap.addId(id);
+                }
+                if (StringUtils.isNotBlank(url)) {
+                    SqlCostInterceptor.UrlMap.addUrl(url);
+                }
+            } else if (action.equalsIgnoreCase("remove")) {
+                // 删除监控时分别清理sql id和url对应的历史审计记录
+                if (StringUtils.isNotBlank(id)) {
+                    SqlCostInterceptor.SqlIdMap.removeId(id);
+                    SqlAuditManager.removeSqlAudit(id);
+                }
+                if (StringUtils.isNotBlank(url)) {
+                    SqlCostInterceptor.UrlMap.removeUrl(url);
+                    SqlAuditManager.removeRequestSqlAudit(url);
+                }
             }
         }
         return null;
