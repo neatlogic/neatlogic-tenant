98e6babfb719e42f974a6db872847cc676df6dec
chenqw <gdnpc@163.com>
2026-05-27 15:24:54 +0800

    adjust
    Signed-off-by: chenqw <gdnpc@163.com>

--- src/main/java/neatlogic/module/tenant/api/apimanage/ApiManageGetApi.java
+++ src/main/java/neatlogic/module/tenant/api/apimanage/ApiManageGetApi.java
@@ -24,15 +24,16 @@ import neatlogic.framework.restful.core.privateapi.PrivateApiComponentFactory;
 import neatlogic.framework.restful.dao.mapper.ApiMapper;
 import neatlogic.framework.restful.dto.ApiHandlerVo;
 import neatlogic.framework.restful.dto.ApiVo;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @Service
 @AuthAction(action = INTERFACE_MODIFY.class)
 @OperationType(type = OperationTypeEnum.SEARCH)
 public class ApiManageGetApi extends PrivateApiComponentBase {
 
-	@Autowired
+	@Resource
 	private ApiMapper ApiMapper;
 	
 	@Override
