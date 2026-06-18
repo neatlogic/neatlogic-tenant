c9d593afc8461b5386cc0bfdb37fde66838d9081
1437892690@qq.com <1437892690@qq.com>
2026-06-18 16:53:52 +0800

    [功能] 登录记录优化
    关联 #[1709466677149701]登录记录优化 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1709466677149701

--- src/main/java/neatlogic/module/tenant/api/featureusageaudit/SearchFeatureApi.java
+++ src/main/java/neatlogic/module/tenant/api/featureusageaudit/SearchFeatureApi.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2025  TechSure Co., Ltd.  All Rights Reserved.
  * This file is part of the NeatLogic software.
- * Licensed under the NeatLogic Sustainable Use License (NSUL), Version 4.x – 2025.
+ * Licensed under the NeatLogic Sustainable Use License (NSUL), Version 4.x - 2025.
  * You may use this file only in compliance with the License.
  * See the LICENSE file distributed with this work for the full license text.
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,10 +14,15 @@ import com.alibaba.fastjson.JSONObject;
 import neatlogic.framework.auth.core.AuthAction;
 import neatlogic.framework.auth.label.NoAuth;
 import neatlogic.framework.common.constvalue.ApiParamType;
+import neatlogic.framework.common.constvalue.GroupSearch;
 import neatlogic.framework.dao.mapper.FeatureUsageAuditMapper;
 import neatlogic.framework.dto.featureusageaudit.FeatureUsageAuditSearchVo;
 import neatlogic.framework.dto.featureusageaudit.FeatureUsageAuditVo;
-import neatlogic.framework.restful.annotation.*;
+import neatlogic.framework.restful.annotation.Description;
+import neatlogic.framework.restful.annotation.Input;
+import neatlogic.framework.restful.annotation.OperationType;
+import neatlogic.framework.restful.annotation.Output;
+import neatlogic.framework.restful.annotation.Param;
 import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
 import neatlogic.framework.util.TableResultUtil;
@@ -49,16 +54,16 @@ public class SearchFeatureApi extends PrivateApiComponentBase {
     }
 
     @Input({
-            @Param(name = "userUuid", type = ApiParamType.STRING, isRequired = false, desc = "用户UUID"),
-            @Param(name = "moduleGroup", type = ApiParamType.STRING, isRequired = false, desc = "模块组"),
-            @Param(name = "menuPath", type = ApiParamType.STRING, desc = "menu path"),
+            @Param(name = "userUuid", type = ApiParamType.STRING, desc = "用户UUID"),
+            @Param(name = "moduleGroup", type = ApiParamType.STRING, desc = "模块"),
+            @Param(name = "menuPath", type = ApiParamType.STRING, desc = "菜单路径"),
             @Param(name = "keyword", type = ApiParamType.STRING, desc = "common.keyword"),
             @Param(name = "currentPage", type = ApiParamType.INTEGER, desc = "common.currentpage"),
             @Param(name = "pageSize", type = ApiParamType.INTEGER, desc = "common.pagesize"),
             @Param(name = "timeRange", type = ApiParamType.INTEGER, desc = "common.duration"),
             @Param(name = "timeUnit", type = ApiParamType.STRING, desc = "common.timeunit"),
-            @Param(name = "startTime", type = ApiParamType.LONG, isRequired = true, desc = "common.starttime"),
-            @Param(name = "endTime", type = ApiParamType.LONG, isRequired = true, desc = "common.endtime"),
+            @Param(name = "startTime", type = ApiParamType.LONG, desc = "common.starttime"),
+            @Param(name = "endTime", type = ApiParamType.LONG, desc = "common.endtime"),
     })
     @Output({
             @Param(name = "tbodyList", explode = FeatureUsageAuditVo[].class, desc = "common.tbodylist")
@@ -67,7 +72,12 @@ public class SearchFeatureApi extends PrivateApiComponentBase {
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
         FeatureUsageAuditSearchVo searchVo = paramObj.toJavaObject(FeatureUsageAuditSearchVo.class);
-        //将时间范围转为 开始时间、结束时间
+        String userUuid = paramObj.getString("userUuid");
+        if (StringUtils.isNotBlank(userUuid) && userUuid.startsWith(GroupSearch.USER.getValuePlugin())) {
+            userUuid = GroupSearch.removePrefix(userUuid);
+            searchVo.setUserUuid(userUuid);
+        }
+        // 将相对时间范围转换为开始时间和结束时间，供统计查询统一使用。
         if (searchVo.getStartTime() == null && searchVo.getEndTime() == null) {
             Integer timeRange = paramObj.getInteger("timeRange");
             String timeUnit = paramObj.getString("timeUnit");
