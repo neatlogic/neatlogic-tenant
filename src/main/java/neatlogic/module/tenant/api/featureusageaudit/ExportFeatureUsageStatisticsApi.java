9c5ad5e09fa649c858c889e2c8c28f93e737240d
linbangquan <1437892690@qq.com>
2026-06-25 06:57:43 +0000

    !139 [功能] 登录记录优化
    * [功能] 登录记录优化
    * [功能] 登录记录优化
    * [功能] 登录记录优化
    * [功能] 登录记录优化
    * [功能] 登录记录优化
    * [功能] 登录记录优化
    * [功能] 登录记录优化
    * [功能] 登录记录优化
    * [功能] 登录记录优化
    * [功能] 登录记录优化
    * [功能] 登录记录优化
    * [功能] 登录记录优化
    * [功能] 登录记录优化
    * [功能] 登录记录优化
    * [功能] 登录记录优化
    * [功能] 登录记录优化
    * [功能] 登录记录优化
    * [功能] 登录记录优化
    * [功能] 登录记录优化
    * [功能] 登录记录优化

---
+++ src/main/java/neatlogic/module/tenant/api/featureusageaudit/ExportFeatureUsageStatisticsApi.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright (C) 2025  TechSure Co., Ltd.  All Rights Reserved.
+ * This file is part of the NeatLogic software.
+ * Licensed under the NeatLogic Sustainable Use License (NSUL), Version 4.x - 2025.
+ * You may use this file only in compliance with the License.
+ * See the LICENSE file distributed with this work for the full license text.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+package neatlogic.module.tenant.api.featureusageaudit;
+
+import com.alibaba.fastjson.JSONObject;
+import neatlogic.framework.auth.core.AuthAction;
+import neatlogic.framework.auth.label.NoAuth;
+import neatlogic.framework.auth.label.USER_MODIFY;
+import neatlogic.framework.common.constvalue.ApiParamType;
+import neatlogic.framework.common.constvalue.GroupSearch;
+import neatlogic.framework.common.constvalue.MimeType;
+import neatlogic.framework.common.util.ModuleUtil;
+import neatlogic.framework.dao.mapper.FeatureUsageAuditMapper;
+import neatlogic.framework.dto.featureusageaudit.FeatureUsageAuditSearchVo;
+import neatlogic.framework.dto.featureusageaudit.FeatureUsageAuditVo;
+import neatlogic.framework.dto.module.ModuleGroupVo;
+import neatlogic.framework.restful.annotation.*;
+import neatlogic.framework.restful.constvalue.OperationTypeEnum;
+import neatlogic.framework.restful.core.privateapi.binarystream.PrivateBinaryStreamApiComponentBase;
+import neatlogic.framework.util.FileUtil;
+import neatlogic.framework.util.TimeUtil;
+import neatlogic.framework.util.excel.ExcelBuilder;
+import neatlogic.framework.util.excel.SheetBuilder;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+@Component
+@AuthAction(action = USER_MODIFY.class)
+@OperationType(type = OperationTypeEnum.SEARCH)
+public class ExportFeatureUsageStatisticsApi extends PrivateBinaryStreamApiComponentBase {
+
+    private static final int EXPORT_PAGE_SIZE = 100;
+    private static final List<String> HEADER_LIST = Arrays.asList("模块", "功能名称", "使用时长", "使用次数");
+    private static final List<String> COLUMN_LIST = Arrays.asList("moduleGroupName", "featureName", "duration", "usedCount");
+
+    @Resource
+    private FeatureUsageAuditMapper featureUsageAuditMapper;
+
+    @Override
+    public String getToken() {
+        return "feature/usage/statistics/export";
+    }
+
+    @Override
+    public String getName() {
+        return "nmtaf.exportfeatureusagestatisticsapi.getname";
+    }
+
+    @Input({
+            @Param(name = "userUuid", type = ApiParamType.STRING, desc = "common.useruuid"),
+            @Param(name = "moduleGroupList", type = ApiParamType.JSONARRAY, desc = "common.modulegroup"),
+            @Param(name = "featureNameList", type = ApiParamType.JSONARRAY, desc = "common.featurename"),
+            @Param(name = "keyword", type = ApiParamType.STRING, desc = "common.keyword"),
+            @Param(name = "timeRange", type = ApiParamType.INTEGER, desc = "common.timerange"),
+            @Param(name = "timeUnit", type = ApiParamType.STRING, desc = "common.timeunit"),
+            @Param(name = "startTime", type = ApiParamType.LONG, desc = "common.starttime"),
+            @Param(name = "endTime", type = ApiParamType.LONG, desc = "common.endtime"),
+    })
+    @Output({})
+    @Description(desc = "nmtaf.exportfeatureusagestatisticsapi.getname")
+    @Override
+    public Object myDoService(JSONObject paramObj, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        FeatureUsageAuditSearchVo searchVo = paramObj.toJavaObject(FeatureUsageAuditSearchVo.class);
+        buildSearchParam(searchVo, paramObj);
+
+        response.setContentType(MimeType.XLSX.getValue() + ";charset=utf-8");
+        String fileName = "功能使用统计" + new SimpleDateFormat("yyyyMMddHHmm").format(new Date()) + ".xlsx";
+        response.setHeader("Content-Disposition", " attachment; filename=\"" + FileUtil.getEncodedFileName(fileName) + "\"");
+        ExcelBuilder builder = new ExcelBuilder(SXSSFWorkbook.class)
+                .withBorderColor(HSSFColor.HSSFColorPredefined.GREY_40_PERCENT)
+                .withHeadFontColor(HSSFColor.HSSFColorPredefined.WHITE)
+                .withHeadBgColor(HSSFColor.HSSFColorPredefined.DARK_BLUE)
+                .withColumnWidth(24);
+        SheetBuilder sheetBuilder = builder.addSheet("sheet1")
+                .withHeaderList(HEADER_LIST)
+                .withColumnList(COLUMN_LIST);
+        try (Workbook workbook = builder.build();
+             ServletOutputStream os = response.getOutputStream()) {
+            writeData(sheetBuilder, searchVo);
+            workbook.write(os);
+            if (workbook instanceof SXSSFWorkbook) {
+                ((SXSSFWorkbook) workbook).dispose();
+            }
+        }
+        return null;
+    }
+
+    private void buildSearchParam(FeatureUsageAuditSearchVo searchVo, JSONObject paramObj) {
+        String userUuid = paramObj.getString("userUuid");
+        if (StringUtils.isNotBlank(userUuid) && userUuid.startsWith(GroupSearch.USER.getValuePlugin())) {
+            searchVo.setUserUuid(GroupSearch.removePrefix(userUuid));
+        }
+        // 将相对时间范围转换为开始时间和结束时间，导出与列表查询使用同一套时间条件。
+        if (searchVo.getStartTime() == null && searchVo.getEndTime() == null) {
+            Integer timeRange = paramObj.getInteger("timeRange");
+            String timeUnit = paramObj.getString("timeUnit");
+            if (timeRange != null && StringUtils.isNotBlank(timeUnit)) {
+                searchVo.setStartTime(TimeUtil.recentTimeTransfer(timeRange, timeUnit));
+                searchVo.setEndTime(new Date());
+            }
+        }
+        searchVo.setCurrentPage(1);
+        searchVo.setPageSize(EXPORT_PAGE_SIZE);
+    }
+
+    private void writeData(SheetBuilder sheetBuilder, FeatureUsageAuditSearchVo searchVo) {
+        int rowNum = featureUsageAuditMapper.getFeatureCount(searchVo);
+        if (rowNum <= 0) {
+            return;
+        }
+        searchVo.setRowNum(rowNum);
+        Integer pageCount = searchVo.getPageCount();
+        for (int currentPage = 1; currentPage <= pageCount; currentPage++) {
+            searchVo.setCurrentPage(currentPage);
+            List<FeatureUsageAuditVo> featureUsageAuditList = featureUsageAuditMapper.getFeatureList(searchVo);
+            for (FeatureUsageAuditVo featureUsageAuditVo : featureUsageAuditList) {
+                Map<String, Object> dataMap = new LinkedHashMap<>();
+                dataMap.put("moduleGroupName", getModuleGroupName(featureUsageAuditVo.getModuleGroup()));
+                dataMap.put("featureName", StringUtils.defaultString(featureUsageAuditVo.getFeatureName()));
+                dataMap.put("duration", getDurationText(featureUsageAuditVo.getDuration()));
+                dataMap.put("usedCount", featureUsageAuditVo.getUsedCount() == null ? 0 : featureUsageAuditVo.getUsedCount());
+                sheetBuilder.addData(dataMap);
+            }
+        }
+    }
+
+    private String getModuleGroupName(String moduleGroup) {
+        ModuleGroupVo groupVo = ModuleUtil.getModuleGroup(moduleGroup);
+        if (groupVo != null) {
+            return groupVo.getGroupName();
+        }
+        return StringUtils.defaultString(moduleGroup);
+    }
+
+    private String getDurationText(Long duration) {
+        if (duration == null) {
+            return StringUtils.EMPTY;
+        }
+        return TimeUtil.millisecondsTransferMaxTimeUnit(duration);
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+}
---
+++ src/main/java/neatlogic/module/tenant/api/featureusageaudit/SaveFeatureUsageAuditApi.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright (C) 2025  TechSure Co., Ltd.  All Rights Reserved.
+ * This file is part of the NeatLogic software.
+ * Licensed under the NeatLogic Sustainable Use License (NSUL), Version 4.x – 2025.
+ * You may use this file only in compliance with the License.
+ * See the LICENSE file distributed with this work for the full license text.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+package neatlogic.module.tenant.api.featureusageaudit;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import neatlogic.framework.asynchronization.threadlocal.UserContext;
+import neatlogic.framework.auth.core.AuthAction;
+import neatlogic.framework.auth.label.NoAuth;
+import neatlogic.framework.common.constvalue.ApiParamType;
+import neatlogic.framework.dao.mapper.LoginMapper;
+import neatlogic.framework.dto.featureusageaudit.FeatureUsageAuditVo;
+import neatlogic.framework.dto.loginaudit.LoginAuditVo;
+import neatlogic.framework.exception.type.ParamInvalidException;
+import neatlogic.framework.restful.annotation.Description;
+import neatlogic.framework.restful.annotation.Input;
+import neatlogic.framework.restful.annotation.OperationType;
+import neatlogic.framework.restful.annotation.Output;
+import neatlogic.framework.restful.annotation.Param;
+import neatlogic.framework.restful.constvalue.OperationTypeEnum;
+import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
+import neatlogic.framework.util.SnowflakeUtil;
+import neatlogic.module.tenant.service.featureusage.FeatureUsageService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+
+@Service
+@AuthAction(action = NoAuth.class)
+@OperationType(type = OperationTypeEnum.CREATE)
+public class SaveFeatureUsageAuditApi extends PrivateApiComponentBase {
+
+    private static final long MAX_DURATION = TimeUnit.DAYS.toMillis(1);
+
+    @Resource
+    private FeatureUsageService featureUsageService;
+
+    @Resource
+    private LoginMapper loginMapper;
+
+    @Override
+    public String getToken() {
+        return "feature/usage/audit/save";
+    }
+
+    @Override
+    public String getName() {
+        return "nmtaf.savefeatureusageauditapi.getname";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Input({
+            @Param(name = "moduleGroup", type = ApiParamType.STRING, isRequired = true, desc = "common.modulegroup"),
+            @Param(name = "featurePath", type = ApiParamType.STRING, isRequired = true, desc = "common.featurepath"),
+            @Param(name = "featureName", type = ApiParamType.STRING, isRequired = true, desc = "common.featurename"),
+            @Param(name = "startTime", type = ApiParamType.LONG, isRequired = true, desc = "common.starttime"),
+            @Param(name = "endTime", type = ApiParamType.LONG, isRequired = true, desc = "common.endtime"),
+            @Param(name = "duration", type = ApiParamType.LONG, isRequired = true, desc = "common.duration"),
+    })
+    @Output({
+            @Param(name = "id", type = ApiParamType.LONG, desc = "common.id")
+    })
+    @Description(desc = "nmtaf.savefeatureusageauditapi.getname")
+    @Override
+    public Object myDoService(JSONObject jsonObj) throws Exception {
+        JSONObject resultObj = new JSONObject();
+        String userUuid = UserContext.get().getUserUuid();
+        if (StringUtils.isNotBlank(userUuid)) {
+            FeatureUsageAuditVo auditVo = JSON.toJavaObject(jsonObj, FeatureUsageAuditVo.class);
+            validate(auditVo);
+            auditVo.setUserUuid(userUuid);
+            auditVo.setId(SnowflakeUtil.uniqueLong());
+            LoginAuditVo lastLoginAudit = loginMapper.getLastLoginAuditByUserUuid(userUuid);
+            if (lastLoginAudit != null) {
+                auditVo.setLoginAuditId(lastLoginAudit.getId());
+            } else {
+                auditVo.setLoginAuditId(-1L);
+            }
+            featureUsageService.addFeatureUsageAuditVo(auditVo);
+            resultObj.put("id", auditVo.getId());
+        }
+        return resultObj;
+    }
+
+    private void validate(FeatureUsageAuditVo auditVo) {
+        if (StringUtils.isBlank(auditVo.getModuleGroup())) {
+            throw new ParamInvalidException("moduleGroup", auditVo.getModuleGroup());
+        }
+        if (StringUtils.isBlank(auditVo.getFeaturePath())) {
+            throw new ParamInvalidException("featurePath", auditVo.getFeaturePath());
+        }
+        if (StringUtils.isBlank(auditVo.getFeatureName())) {
+            throw new ParamInvalidException("featureName", auditVo.getFeatureName());
+        }
+        if (auditVo.getStartTime() == null) {
+            throw new ParamInvalidException("startTime", null);
+        }
+        if (auditVo.getEndTime() == null) {
+            throw new ParamInvalidException("endTime", null);
+        }
+        Long duration = auditVo.getDuration();
+        if (duration == null || duration <= 0 || duration > MAX_DURATION) {
+            throw new ParamInvalidException("duration", String.valueOf(duration));
+        }
+        if (auditVo.getEndTime().before(auditVo.getStartTime())) {
+            throw new ParamInvalidException("endTime", String.valueOf(auditVo.getEndTime().getTime()));
+        }
+    }
+}
