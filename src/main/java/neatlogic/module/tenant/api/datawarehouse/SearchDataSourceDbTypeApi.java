017a887542760db650008add3ebcd950d5dfc6e5
1437892690@qq.com <1437892690@qq.com>
2026-08-27 16:51:33 +0800

    [功能] 将数据仓库数据源移到commercial
    关联 #[1760227318562821]将数据仓库数据源移到commercial http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1760227318562821

--- src/main/java/neatlogic/module/tenant/api/datawarehouse/SearchDataSourceDbTypeApi.java
+++ src/main/java/neatlogic/module/tenant/api/datawarehouse/SearchDataSourceDbTypeApi.java
@@ -15,16 +15,14 @@ import com.alibaba.fastjson.JSONObject;
 import neatlogic.framework.auth.core.AuthAction;
 import neatlogic.framework.auth.label.DATA_WAREHOUSE_BASE;
 import neatlogic.framework.common.constvalue.ApiParamType;
+import neatlogic.framework.datawarehouse.core.DataSourceServiceHandlerFactory;
+import neatlogic.framework.datawarehouse.core.IDataSourceServiceHandler;
 import neatlogic.framework.datawarehouse.dao.mapper.DatabaseMapper;
-import neatlogic.framework.datawarehouse.dto.DatabaseVo;
 import neatlogic.framework.integration.dao.mapper.IntegrationMapper;
-import neatlogic.framework.integration.dto.IntegrationVo;
 import neatlogic.framework.restful.annotation.*;
 import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
-import neatlogic.framework.util.$;
-import neatlogic.module.framework.datawarehouse.integration.handler.DataWareHouseIntegrationHandler;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.collections4.MapUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -61,57 +59,64 @@ public class SearchDataSourceDbTypeApi extends PrivateApiComponentBase {
         String keyword = paramObj.getString("keyword");
         JSONObject resultObj = new JSONObject();
         JSONArray tbodyList = new JSONArray();
-        JSONArray mysqlChildren = new JSONArray();
-        if (StringUtils.isBlank(keyword) || MYSQL.contains(keyword.toLowerCase())) {
-            mysqlChildren.fluentAdd(new JSONObject().fluentPut("value", MYSQL).fluentPut("text", MYSQL));
+        List<IDataSourceServiceHandler> handlerList = DataSourceServiceHandlerFactory.getHandlerList();
+        for (IDataSourceServiceHandler handler : handlerList) {
+            JSONObject dataSourceTypeGroup = handler.getDataSourceTypeGroup(keyword);
+            if (MapUtils.isNotEmpty(dataSourceTypeGroup)) {
+                tbodyList.add(dataSourceTypeGroup);
+            }
         }
-        tbodyList.add(new JSONObject()
-                .fluentPut("value", MYSQL.toUpperCase()).fluentPut("text", MYSQL)
-                .fluentPut("children", mysqlChildren )
-        );
-        JSONArray mongodbChildren = new JSONArray();
-        if (StringUtils.isBlank(keyword) || MONGODB.contains(keyword.toLowerCase())) {
-            mongodbChildren.fluentAdd(new JSONObject().fluentPut("value", MONGODB).fluentPut("text", MONGODB));
-        }
-        tbodyList.add(new JSONObject()
-                .fluentPut("value", MONGODB.toUpperCase()).fluentPut("text", MONGODB)
-                .fluentPut("children", mongodbChildren)
-        );
-        JSONArray elasticsearchChildren = new JSONArray();
-        if (StringUtils.isBlank(keyword) || ELASTICSEARCH.contains(keyword.toLowerCase())) {
-            elasticsearchChildren.fluentAdd(new JSONObject().fluentPut("value", ELASTICSEARCH).fluentPut("text", ELASTICSEARCH));
-        }
-        tbodyList.add(new JSONObject()
-                .fluentPut("value", ELASTICSEARCH.toUpperCase()).fluentPut("text", ELASTICSEARCH)
-                .fluentPut("children", elasticsearchChildren)
-        );
-        JSONArray databaseChildren = new JSONArray();
-        DatabaseVo searchVo = new DatabaseVo();
-        searchVo.setKeyword(keyword);
-        searchVo.setCurrentPage(1);
-        searchVo.setPageSize(500);
-        List<DatabaseVo> dataBaseList = databaseMapper.getDataBaseList(searchVo);
-        for (DatabaseVo databaseVo : dataBaseList) {
-            databaseChildren.add(new JSONObject().fluentPut("value", databaseVo.getType() + "-" + databaseVo.getId()).fluentPut("text", databaseVo.getName()));
-        }
-        tbodyList.add(new JSONObject()
-                .fluentPut("value", "database").fluentPut("text", $.t("common.datasource"))
-                .fluentPut("children", databaseChildren));
+//        JSONArray mysqlChildren = new JSONArray();
+//        if (StringUtils.isBlank(keyword) || MYSQL.contains(keyword.toLowerCase())) {
+//            mysqlChildren.fluentAdd(new JSONObject().fluentPut("value", MYSQL).fluentPut("text", MYSQL));
+//        }
+//        tbodyList.add(new JSONObject()
+//                .fluentPut("value", MYSQL.toUpperCase()).fluentPut("text", MYSQL)
+//                .fluentPut("children", mysqlChildren )
+//        );
+//        JSONArray mongodbChildren = new JSONArray();
+//        if (StringUtils.isBlank(keyword) || MONGODB.contains(keyword.toLowerCase())) {
+//            mongodbChildren.fluentAdd(new JSONObject().fluentPut("value", MONGODB).fluentPut("text", MONGODB));
+//        }
+//        tbodyList.add(new JSONObject()
+//                .fluentPut("value", MONGODB.toUpperCase()).fluentPut("text", MONGODB)
+//                .fluentPut("children", mongodbChildren)
+//        );
+//        JSONArray elasticsearchChildren = new JSONArray();
+//        if (StringUtils.isBlank(keyword) || ELASTICSEARCH.contains(keyword.toLowerCase())) {
+//            elasticsearchChildren.fluentAdd(new JSONObject().fluentPut("value", ELASTICSEARCH).fluentPut("text", ELASTICSEARCH));
+//        }
+//        tbodyList.add(new JSONObject()
+//                .fluentPut("value", ELASTICSEARCH.toUpperCase()).fluentPut("text", ELASTICSEARCH)
+//                .fluentPut("children", elasticsearchChildren)
+//        );
+//        JSONArray databaseChildren = new JSONArray();
+//        DatabaseVo searchVo = new DatabaseVo();
+//        searchVo.setKeyword(keyword);
+//        searchVo.setCurrentPage(1);
+//        searchVo.setPageSize(500);
+//        List<DatabaseVo> dataBaseList = databaseMapper.getDataBaseList(searchVo);
+//        for (DatabaseVo databaseVo : dataBaseList) {
+//            databaseChildren.add(new JSONObject().fluentPut("value", databaseVo.getType() + "-" + databaseVo.getId()).fluentPut("text", databaseVo.getName()));
+//        }
+//        tbodyList.add(new JSONObject()
+//                .fluentPut("value", "database").fluentPut("text", $.t("common.datasource"))
+//                .fluentPut("children", databaseChildren));
 
-        JSONArray integrationChildren = new JSONArray();
-        IntegrationVo integrationVo = new IntegrationVo();
-        integrationVo.setKeyword(keyword);
-        integrationVo.setCurrentPage(1);
-        integrationVo.setPageSize(500);
-        integrationVo.setHandler(DataWareHouseIntegrationHandler.class.getSimpleName());
-        integrationVo.setIsActive(1);
-        List<IntegrationVo> integrationList = integrationMapper.searchIntegration(integrationVo);
-        for (IntegrationVo integration : integrationList) {
-            integrationChildren.add(new JSONObject().fluentPut("value", "integration-" + integration.getUuid()).fluentPut("text", integration.getName()));
-        }
-        tbodyList.add(new JSONObject()
-                .fluentPut("value", "integration").fluentPut("text", $.t("term.framework.integration"))
-                .fluentPut("children", integrationChildren));
+//        JSONArray integrationChildren = new JSONArray();
+//        IntegrationVo integrationVo = new IntegrationVo();
+//        integrationVo.setKeyword(keyword);
+//        integrationVo.setCurrentPage(1);
+//        integrationVo.setPageSize(500);
+//        integrationVo.setHandler(DataWareHouseIntegrationHandler.class.getSimpleName());
+//        integrationVo.setIsActive(1);
+//        List<IntegrationVo> integrationList = integrationMapper.searchIntegration(integrationVo);
+//        for (IntegrationVo integration : integrationList) {
+//            integrationChildren.add(new JSONObject().fluentPut("value", "integration-" + integration.getUuid()).fluentPut("text", integration.getName()));
+//        }
+//        tbodyList.add(new JSONObject()
+//                .fluentPut("value", "integration").fluentPut("text", $.t("term.framework.integration"))
+//                .fluentPut("children", integrationChildren));
         resultObj.put("tbodyList", tbodyList);
         return resultObj;
     }
